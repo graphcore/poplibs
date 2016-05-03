c926b959d94baf8cf412eef0bdb6d5753fa7f38e
David Lacey <david.lacey@xmos.com>
2016-05-03 16:22:33 +0100

    Update neural net convolution vertices to have a more accurate cycle estimate
    Reviewers: richard
    
    Reviewed By: richard
    
    Differential Revision: https://phabricator.sourcevertex.net/D268

--- PerformanceEstimation.hpp
+++ PerformanceEstimation.hpp
@@ -18,15 +18,21 @@ getConvPartialCycleEstimate(bool isFloat, unsigned inChansPerGroup,
 {
   unsigned vertexOverhead = 5;
   if (inChansPerGroup == 4 && !isFloat && stride == 1) {
-    unsigned innerLoopCycles = outputWidth;
+    // The 6 cycle overhead is estimated as 2 cycles to flush to convolution
+    // instruction pipeline and 4 cycles to load the weights.
+    unsigned innerLoopCycles = 6 + outputWidth;
     // A nx1 convolution must be computed as the sum of 3x1 convolutions.
     // The number of passes per row is the number of 3x1 convolutions
     // required.
     unsigned passesPerRow = (kernelSize + 2) / 3;
-    // The 1 here represents loop overheads. TODO take into account
-    // the time to load weights at the beginning of the row and flush the
-    // accumulator values out at the end of the row.
-    return vertexOverhead + numInRows * passesPerRow * (1 + innerLoopCycles);
+    unsigned outerLoopCycles;
+    if (passesPerRow > 1) {
+      unsigned middleLoopCycles = 1 + passesPerRow * innerLoopCycles;
+      outerLoopCycles = 1 + numInRows * middleLoopCycles;
+    } else {
+      outerLoopCycles = 1 + numInRows * innerLoopCycles;
+    }
+    return vertexOverhead + outerLoopCycles;
   }
   return vertexOverhead +
          outputWidth *
