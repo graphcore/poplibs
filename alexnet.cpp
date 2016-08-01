c6056405afd77e501d8186a1ed73e50bf6d05806
Richard Osborne <richard@graphcore.ai>
2016-08-01 11:01:09 +0100

    Pad the input depth of the first layer to 4.
    Summary:
    This provides a significant speed up as it lets us use the convolution
    instruction for these layers.
    
    Depends on D549
    Fixes T291
    
    Reviewers: davel
    
    Reviewed By: davel
    
    Maniphest Tasks: T291
    
    Differential Revision: https://phabricator.sourcevertex.net/D550

--- alexnet.cpp
+++ alexnet.cpp
@@ -3,8 +3,8 @@
 
 int main(int argc, char **argv) {
   DataSet IMAGENET;
-  IMAGENET.dataSize = 224*224*3;
-  IMAGENET.dim = std::vector<std::size_t>{224,224,3};
+  IMAGENET.dataSize = 224*224*4;
+  IMAGENET.dim = std::vector<std::size_t>{224,224,4};
   IMAGENET.numTraining = 1000;
   IMAGENET.numTest = 1000;
   IMAGENET.testLabels =
