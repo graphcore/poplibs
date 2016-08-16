69895d5c93219604d0deec861dab6de8c5b3fbd1
Eddy Andrews <Eddy.Andrews@graphcore.ai>
2016-08-16 15:23:54 +0100

    Initial maxpool handling nonoverlapping, nonpadding pool layers
    Summary:
    Maxpool for simple layers. Also makes both fwd and bwd passes schedule x as the
    inner dimmension
    
    Test Plan:
    tested on modified alexnet and conv_mnist, training conv_mnist in non-IPU mode
    gives unchanged convergence
    
    Reviewers: davel
    
    Reviewed By: davel
    
    Differential Revision: https://phabricator.sourcevertex.net/D601

--- MaxPool.cpp
+++ MaxPool.cpp
@@ -64,11 +64,13 @@ maxPool(Graph &graph, IPUModelEngineBuilder::TileMapping &mapping,
   const auto dataPathWidth = deviceInfo.dataPathWidth;
   const auto layerName = "MaxPool" + std::to_string(kernelSize) + "x" +
                           std::to_string(kernelSize);
-  unsigned prevChanGroups = in.dim(0);
-  unsigned numChannels = in.dim(0) * in.dim(3);
-  unsigned numChanGroups = out.dim(0);
-  unsigned prevChansPerGroup = numChannels / prevChanGroups;
-  unsigned chansPerGroup = numChannels / numChanGroups;
+  const auto prevNumChanGroups = in.dim(0);
+  const auto prevChansPerGroup = in.dim(3);
+  const auto prevNumChannels = prevNumChanGroups * prevChansPerGroup;
+  const auto numChanGroups = out.dim(0);
+  const auto chansPerGroup = out.dim(3);
+  const auto numChannels = numChanGroups * chansPerGroup;
+  assert(numChannels == prevNumChannels);
   unsigned inDimY = in.dim(1), inDimX = in.dim(2);
   unsigned outDimY, outDimX;
   std::tie(outDimY, outDimX) = getOutputDim(inDimY, inDimX,
@@ -78,8 +80,10 @@ maxPool(Graph &graph, IPUModelEngineBuilder::TileMapping &mapping,
   const auto chunkSize = gcd<unsigned>(prevChansPerGroup, chansPerGroup);
   const auto chunksPerChanGroup = chansPerGroup / chunkSize;
   ComputeSet fwd = graph.createComputeSet(layerName + ".fwd");
+  //mapping over this MaxPool layer's outputs
   const auto outMapping = computeActivationsMapping(out, deviceInfo);
   const auto numTiles = deviceInfo.getNumIPUs() * deviceInfo.getTilesPerIPU();
+
   for (unsigned tile = 0; tile != numTiles; ++tile) {
     const auto tileOutBegin = outMapping[tile];
     const auto tileOutEnd = outMapping[tile + 1];
@@ -91,8 +95,8 @@ maxPool(Graph &graph, IPUModelEngineBuilder::TileMapping &mapping,
     if (tileNumGroups == 0)
       continue;
     for (unsigned i = tileGroupBegin; i != tileGroupEnd; ++i) {
-      unsigned y = i % outDimY;
-      unsigned x = (i / outDimY) % outDimX;
+      unsigned x = i % outDimX;
+      unsigned y = (i / outDimX) % outDimY;
       unsigned chanGroup = i / (outDimY * outDimX);
       unsigned inYBegin, inYEnd;
       std::tie(inYBegin, inYEnd) = getInputRange(y, stride, kernelSize,
@@ -109,18 +113,21 @@ maxPool(Graph &graph, IPUModelEngineBuilder::TileMapping &mapping,
       mapping.setMapping(v, tile);
       graph.setFieldSize(v["activationIn"],
                          chunksPerChanGroup * inYSize * inXSize);
+      unsigned chunkIndex = 0;
+
       for (unsigned j = 0; j != chunksPerChanGroup; ++j) {
         unsigned chan = chanGroup * chansPerGroup + j * chunksPerChanGroup;
         unsigned prevChanGroup = chan / prevChansPerGroup;
         unsigned prevChanInGroup = chan % prevChansPerGroup;
+
         for (unsigned inY = inYBegin; inY != inYEnd; ++inY) {
           for (unsigned inX = inXBegin; inX != inXEnd; ++inX) {
             Tensor inWindow =
                 in[prevChanGroup][inY][inX].slice(prevChanInGroup,
                                                   prevChanInGroup + chunkSize);
-            const auto chunkIndex =
-                (inX - inXBegin) + inXSize * ((inY - inYBegin) + inYSize * j);
+
             graph.connect(inWindow, v["activationIn"][chunkIndex]);
+            chunkIndex++;
           }
         }
       }
@@ -135,45 +142,109 @@ maxPoolBackward(Graph &graph, IPUModelEngineBuilder::TileMapping &mapping,
                 unsigned kernelSize, unsigned stride, unsigned padding,
                 std::string dType, Tensor actIn, Tensor actOut,
                 Tensor deltasIn, Tensor deltasOut) {
-  // Currently this code does not target the IPU model (no tile mapping)
-  // but is just used for the purely functional tests
-  if (deviceInfo.isIPU)
-    return Sequence();
-  const auto numChanGroups = deltasIn.dim(0);
-  const auto chansPerGroup = deltasIn.dim(3);
-  const auto numChannels = numChanGroups * chansPerGroup;
+  // actIn is from the previous layer
+  // actOut went to the next layer
+  // deltasIn is from the next layer
+  // deltasOut is to the previous layer
+  assert(actIn.dim(1) == deltasOut.dim(1));
+  assert(actIn.dim(2) == deltasOut.dim(2));
+  assert(actOut.dim(1) == deltasIn.dim(1));
+  assert(actOut.dim(2) == deltasIn.dim(2));
+
+  // "prev" refers to the layer nearer the input
+  // "next" refers to the layer nearer the output
   const auto layerName = "MaxPool" + std::to_string(kernelSize) + "x" +
                           std::to_string(kernelSize);
-  auto bwdCS = graph.createComputeSet(layerName + ".bwd");
-  const auto yDimOut = deltasIn.dim(1);
-  const auto xDimOut = deltasIn.dim(2);
-  const auto xDim = actIn.dim(2);
-  const auto yDim = actIn.dim(1);
-  if (padding != 0 || xDim % stride != 0 || yDim % stride != 0 ||
-      kernelSize != stride) {
-    throw net_creation_error("padding, overlapped pooling or pooling that "
-                             "doesn't exactly divide "
-                             "input not implemented yet");
+  const auto nextNumChanGroups = actOut.dim(0);
+  const auto nextChansPerGroup = actOut.dim(3);
+  const auto nextNumChannels = nextNumChanGroups * nextChansPerGroup;
+  const auto prevNumChanGroups = actIn.dim(0);
+  const auto prevChansPerGroup = actIn.dim(3);
+  const auto prevNumChannels = prevNumChanGroups * prevChansPerGroup;
+  //MaxPool so no change in channel dimension
+  assert(nextNumChannels == prevNumChannels);
+
+  const auto yDimPrev = actIn.dim(1);
+  const auto xDimPrev = actIn.dim(2);
+  const auto yDimNext = actOut.dim(1);
+  const auto xDimNext = actOut.dim(2);
+  unsigned calcNextX, calcNextY;
+
+  if (deviceInfo.isIPU
+      && (padding != 0 || xDimPrev % stride != 0 || yDimPrev % stride != 0 ||
+          kernelSize != stride)) {
+    std::cerr << "WARNING: padding, overlapped pooling or pooling that"
+                 " doesn't exactly divide\n"
+                 "input not implemented yet, skipping tile mapping for"
+                 " this layer\n";
+    return Sequence();
   }
-  for (unsigned xIn = 0; xIn < xDim; ++xIn) {
-    const auto xOut = xIn / stride;
-    assert(xOut < xDimOut);
-    for (unsigned yIn = 0; yIn < yDim; ++yIn) {
-      const auto yOut = yIn / stride;
-      assert(yOut < yDimOut);
-      for (unsigned chan = 0; chan < numChannels; ++chan) {
-        graph.addVertex(bwdCS, templateVertex("MaxPoolingBwd", dType),
-          { {"actOut", actOut[chan / actOut.dim(3)][yOut][xOut]
-                             [chan % actOut.dim(3)]},
-            {"actIn", actIn[chan / actIn.dim(3)][yIn][xIn]
-                           [chan % actIn.dim(3)]},
-            {"errIn", deltasIn[chan / deltasIn.dim(3)][yOut][xOut]
-                              [chan % deltasIn.dim(3)]},
-            {"errOut", deltasOut[chan / deltasOut.dim(3)][yIn][xIn]
-                                [chan % deltasOut.dim(3)]} });
+  std::tie(calcNextY, calcNextX) = getOutputDim(yDimPrev, xDimPrev,
+                                              kernelSize, stride, padding);
+  assert(calcNextY == yDimNext);
+  assert(calcNextX == xDimNext);
+  // The input and output tensors may have different group sizes
+  const auto prevChunkSize = gcd<unsigned>(actOut.dim(3), deltasOut.dim(3));
+  const auto nextChunkSize = gcd<unsigned>(actIn.dim(3), deltasIn.dim(3));
+  const auto chunkSize = gcd<unsigned>(prevChunkSize, nextChunkSize);
+  const auto nextChunksPerChanGroup = nextChansPerGroup / chunkSize;
+
+  auto bwdCS = graph.createComputeSet(layerName + ".bwd");
+  // map over the Pool kernels - all will be mapped to the same tile
+  const auto nextMapping = computeActivationsMapping(actOut, deviceInfo);
+  const auto numTiles = deviceInfo.getNumIPUs() * deviceInfo.getTilesPerIPU();
+
+  for (auto tile = 0; tile != numTiles; ++tile) {
+    const auto tileBegin = nextMapping[tile];
+    const auto tileEnd = nextMapping[tile + 1];
+    assert(tileBegin % nextChansPerGroup == 0);
+    assert(tileEnd % nextChansPerGroup == 0);
+    const auto tileGroupBegin = tileBegin / nextChansPerGroup;
+    const auto tileGroupEnd = tileEnd / nextChansPerGroup;
+    const auto tileNumGroups = tileGroupEnd - tileGroupBegin;
+    if (tileNumGroups == 0)
+      continue;
+    for (unsigned i = tileGroupBegin; i != tileGroupEnd; ++i) {
+      unsigned xNext = i % xDimNext;
+      unsigned yNext = (i / xDimNext) % yDimNext;
+      unsigned chanGroupNext = i / (yDimNext * xDimNext);
+      unsigned prevYBegin, prevYEnd;
+      std::tie(prevYBegin, prevYEnd) = getInputRange(yNext, stride, kernelSize,
+                                                     padding, yDimPrev, true);
+      const auto prevYSize = prevYEnd - prevYBegin;
+      unsigned prevXBegin, prevXEnd;
+      std::tie(prevXBegin, prevXEnd) = getInputRange(xNext, stride, kernelSize,
+                                                     padding, xDimPrev, true);
+      const auto prevXSize = prevXEnd - prevXBegin;
+      for (unsigned chunk = 0; chunk != nextChunksPerChanGroup; chunk++) {
+        // Generate one vertex for each individual output pixel
+        unsigned baseChan = chanGroupNext * nextChansPerGroup + chunk * chunkSize;
+        unsigned prevChanGroup = baseChan / prevChansPerGroup;
+        unsigned prevChanInGroup = baseChan % prevChansPerGroup;
+        for (auto yPrev = prevYBegin; yPrev != prevYEnd; ++yPrev) {
+          for (auto xPrev = prevXBegin; xPrev != prevXEnd; ++xPrev) {
+            for (auto chan = baseChan; chan != baseChan + chunkSize; ++chan) {
+              // add a new vertex for every input activation
+              auto v = graph.addVertex(bwdCS, templateVertex("MaxPoolingBwd", dType),
+                {
+                  {"actOut", actOut[chan / actOut.dim(3)][yNext][xNext]
+                                   [chan % actOut.dim(3)]},
+                  {"actIn", actIn[chan / actIn.dim(3)][yPrev][xPrev]
+                                 [chan % actIn.dim(3)]},
+                  {"errIn", deltasIn[chan / deltasIn.dim(3)][yNext][xNext]
+                                    [chan % deltasIn.dim(3)]},
+                  {"errOut", deltasOut[chan / deltasOut.dim(3)][yPrev][xPrev]
+                                      [chan % deltasOut.dim(3)]}
+                });
+              mapping.setMapping(v, tile);
+
+            }
+          }
+        }
       }
     }
   }
+
   return Execute(bwdCS);
 }
 
