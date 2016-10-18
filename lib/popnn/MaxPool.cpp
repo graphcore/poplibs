graphcore/poplibs

commit 0378a125a74ab70975011f81154c2442cab07d5f

Simple changes to fix warnings generated by -Wall
Summary:
These are the easy fixes. There are a couple of less trivial ones to follow
later

Test Plan: default testing via buildbot

Reviewers: richard

Reviewed By: richard

Differential Revision: https://phabricator.sourcevertex.net/D802

lib/popnn/MaxPool.cpp
@@ -74,7 +74,8 @@ maxPool(Graph &graph,
   const auto numChanGroups = out.dim(1);
   const auto chansPerGroup = out.dim(4);
   const auto numChannels = numChanGroups * chansPerGroup;
-  assert(numChannels == prevNumChannels);
+  if (numChannels != prevNumChannels)
+    assert(!"maxPool's input and output numChannels differ");
   unsigned inDimY = in.dim(2), inDimX = in.dim(3);
   unsigned outDimY, outDimX;
   std::tie(outDimY, outDimX) = getOutputDim(inDimY, inDimX,
@@ -181,7 +182,8 @@ maxPoolBackward(Graph &graph,
   const auto prevChansPerGroup = actIn0.dim(3);
   const auto prevNumChannels = prevNumChanGroups * prevChansPerGroup;
   //MaxPool so no change in channel dimension
-  assert(nextNumChannels == prevNumChannels);
+  if(nextNumChannels == prevNumChannels)
+    assert(!"maxPoolBackwards: prev and next NumChannels must match");
 
   const auto yDimPrev = deltasOut0.dim(1);
   const auto xDimPrev = deltasOut0.dim(2);

benchmarks/single_conv_layer.cpp
@@ -16,6 +16,7 @@
 #include <popnn/NonLinearity.hpp>
 #include <popnn_ref/Convolution.hpp>
 #include <popnn_ref/NonLinearity.hpp>
+#include <popnn/Compiler.hpp>
 #include <random>
 
 using namespace poplar;
@@ -369,6 +370,7 @@ const char *asString(const FPDataType &type) {
   case FPDataType::HALF: return "half";
   case FPDataType::FLOAT: return "float";
   }
+  POPNN_UNREACHABLE();
 }
 
 inline std::ostream &operator<<(std::ostream &os, const FPDataType &type) {
@@ -383,7 +385,7 @@ inline std::istream &operator>>(std::istream &in, FPDataType &type) {
   else if (token == "float")
     type = FPDataType::FLOAT;
   else
-    throw std::runtime_error("Invalid data type name");
+    POPNN_UNREACHABLE();
   return in;
 }
 
@@ -393,6 +395,7 @@ const char *asString(const NonLinearityType &type) {
   case NON_LINEARITY_RELU: return "relu";
   case NON_LINEARITY_SIGMOID: return "sigmoid";
   }
+  POPNN_UNREACHABLE();
 }
 
 inline std::ostream &operator<<(std::ostream &os,
@@ -410,7 +413,7 @@ inline std::istream &operator>>(std::istream &in, NonLinearityType &type) {
   else if (token == "sigmoid")
     type = NON_LINEARITY_SIGMOID;
   else
-    throw std::runtime_error("Invalid non-linearity type");
+    POPNN_UNREACHABLE();
   return in;
 }
 

include/popnn/Compiler.hpp
@@ -1,0 +1,10 @@
+#ifndef __Compiler_hpp__
+#define __Compiler_hpp__
+
+#ifdef NDEBUG
+#define POPNN_UNREACHABLE() __builtin_unreachable()
+#else
+#define POPNN_UNREACHABLE() __builtin_trap()
+#endif
+
+#endif // _Compiler_hpp__

lib/popnn/ConvUtil.cpp
@@ -9,7 +9,7 @@ getInputIndex(unsigned outputIndex, unsigned stride, unsigned kernelSize,
     const auto start = static_cast<int>(outputIndex * stride) -
                        static_cast<int>(padding);
     auto inputIndex = start + static_cast<int>(kernelIndex);
-    if (inputIndex < 0 || inputIndex >= inputSize)
+    if (inputIndex < 0 || static_cast<unsigned>(inputIndex) >= inputSize)
       return ~0U;
     return inputIndex;
   } else {

lib/popnn/Convolution.cpp
@@ -283,7 +283,7 @@ createConvPartial1x1OutVertex(Graph &graph,
                               const Tensor &in, const Tensor &weights,
                               const Tensor &out, bool forward) {
   assert(kernelSizeY == 1 && kernelSizeX == 1);
-  assert(forward || stride == 1);
+  assert(forward || (strideY == 1 && strideX == 1));
   const auto inDimY = in.dim(1);
   const auto inDimX = in.dim(2);
   const auto inChansPerGroup = static_cast<unsigned>(in.dim(3));
@@ -552,7 +552,8 @@ createConvPartialDotProductVertex(Graph &graph,
   const auto inDimY = in.dim(1);
   const auto inDimX = in.dim(2);
   const auto partialType = partition.getPartialType();
-  assert(outChansPerGroup == 1);
+  if (outChansPerGroup != 1)
+    assert(!"outChansPerGroup must be 1");
   assert(outYEnd - outYBegin == 1);
   const auto y = outYBegin;
   unsigned inYBegin, inYEnd, inXBegin, inXEnd;

lib/popnn/FullyConnected.cpp
@@ -38,8 +38,7 @@ fullyConnected(Graph &graph,
   const auto numIPUs = deviceInfo.numIPUs;
   const auto tilesPerIPU = deviceInfo.tilesPerIPU;
   const auto &activationsOutMapping = plan.outputMapping;
-  bool isFloat = dType == "float";
-  assert(isFloat || dType == "half");
+  assert(dType == "float" || dType == "half");
   const auto &ipuPartition = plan.ipuPartition;
   auto prog = Sequence();
   // Iterate through the batch creating new compute sets to add to the

lib/popnn/PerformanceEstimation.hpp
@@ -134,26 +134,28 @@ getWeightGradCalcCycles(unsigned numOutRows, unsigned numInRows,
                         unsigned xpadding, unsigned ypadding,
                         unsigned vectorWidth) {
   std::uint64_t cycles = 0;
-  for (int wy = 0; wy < kernelSizeY; ++wy) {
+  for (unsigned wy = 0; wy < kernelSizeY; ++wy) {
     cycles += 2;
-    for (int wx = 0; wx < kernelSizeX; ++wx) {
+    for (unsigned wx = 0; wx < kernelSizeX; ++wx) {
       cycles += 5;
       for (unsigned outChan = 0; outChan < outChansPerGroup; ++outChan) {
         cycles += 1;
-        int inRow = wy - static_cast<int>(ypadding);
+        int inRowInt = wy - static_cast<int>(ypadding);
         unsigned outRow = 0;
-        while (inRow < 0) {
-          inRow += strideY;
+        while (inRowInt < 0) {
+          inRowInt += strideY;
           outRow += 1;
         }
+        unsigned inRow = inRowInt;
         while (outRow < numOutRows && inRow < numInRows) {
           cycles += 1;
-          int inCol = wx - static_cast<int>(xpadding);
+          int inColInt = wx - static_cast<int>(xpadding);
           unsigned outCol = 0;
-          while (inCol < 0) {
-            inCol += strideX;
+          while (inColInt < 0) {
+            inColInt += strideX;
             outCol += 1;
           }
+          unsigned inCol = inColInt;
           while (outCol < outputWidth && inCol < inputWidth) {
             cycles += 2 * (inChansPerGroup + vectorWidth - 1) / vectorWidth;
             outCol += 1;

lib/popnn/Winograd.cpp
@@ -484,8 +484,6 @@ uint64_t WgdTilePartition::tilePartition(
         /* compute accumulate cost: all the data is local to a tile and hence
          * needn't be exchanged
          */
-        /* size in bytes of accumulated output (or partials) */
-        const unsigned accAtomSize = isFloat ? 4 : 2;
         Cost ecAcc = 0;
         const auto weightsPerConvUnit =
                                 deviceInfo.getWeightsPerConvUnit(isFloat);
@@ -667,7 +665,6 @@ static void wgdMapWeights(
   const auto &deviceInfo = graph.getDevice().getDeviceInfo();
 
   const unsigned numTiles = deviceInfo.getNumTiles();
-  const unsigned numWorkers = deviceInfo.numWorkerContexts;
 
   assert(tp.zic % WgdTilePartition::kUnitSize == 0);
 

commit b00bfd8887a3e7646d8e761f46529b345040a2c8

Correct sense of MaxPool numChan check
Was broken by commit 0378a125a74ab70975011f81154c2442cab07d5f

lib/popnn/MaxPool.cpp
@@ -182,7 +182,7 @@ maxPoolBackward(Graph &graph,
   const auto prevChansPerGroup = actIn0.dim(3);
   const auto prevNumChannels = prevNumChanGroups * prevChansPerGroup;
   //MaxPool so no change in channel dimension
-  if(nextNumChannels == prevNumChannels)
+  if(nextNumChannels != prevNumChannels)
     assert(!"maxPoolBackwards: prev and next NumChannels must match");
 
   const auto yDimPrev = deltasOut0.dim(1);