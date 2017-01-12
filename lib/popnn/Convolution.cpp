graphcore/poplibs

commit 68f463830c468d8cdf320330766496a7652ab34f

Rename Tensor getDimensionality() and dims()
Summary:
Rename getDimensionality()->rank() and dims()->shape()
Depends on poplar change D1097

Reviewers: davel

Reviewed By: davel

Differential Revision: https://phabricator.sourcevertex.net/D1098

lib/popnn/Convolution.cpp
@@ -202,7 +202,7 @@ calculateWeightMapping(Tensor w,
       // Group weights that are accessed contiguously by tiles within this
       // loop body.
       std::vector<std::pair<unsigned, unsigned>> sharedWeights;
-      addWeightRegions(sharedWeights, w.dims(), outZGroupBegin, outZGroupEnd,
+      addWeightRegions(sharedWeights, w.shape(), outZGroupBegin, outZGroupEnd,
                        inZGroupBegin, inZGroupEnd);
       mergeAdjacentRegions(sharedWeights);
       // Spread groups of weights equally across the tiles that read them.
@@ -909,7 +909,7 @@ reduce(Graph &graph,
     return partials[0];
   }
   Tensor reduced = graph.addTensor(reducedType,
-                                   partials[0].dims(), "reduced");
+                                   partials[0].shape(), "reduced");
   applyTensorMapping(graph, reduced, reducedMapping);
   ::reduce(graph, partials, reduced, reducedMapping, reduceCS);
   return reduced;
@@ -935,7 +935,7 @@ computeReducedMapping(const poplar::Graph &graph,
   const auto numTiles = activationsMapping.size() - 1;
   std::vector<std::vector<std::pair<unsigned, unsigned>>>
       reducedMapping(numTiles);
-  assert(activations.getDimensionality() == 4);
+  assert(activations.rank() == 4);
   const auto dimY = activations.dim(1);
   const auto dimX = activations.dim(2);
   const auto fieldSize = dimY * dimX;
@@ -1441,9 +1441,9 @@ matrixMultiplyByConvInstruction(Graph &graph, const Plan &plan,
                                 const std::vector<unsigned> &cTileMapping,
                                 const std::string &debugPrefix) {
   const auto dType = graph.getTensorElementType(a);
-  assert(a.getDimensionality() == 4);
-  assert(b.getDimensionality() == 3);
-  assert(c.getDimensionality() == 3);
+  assert(a.rank() == 4);
+  assert(b.rank() == 3);
+  assert(c.rank() == 3);
   assert(a.dim(0) == c.dim(0));
   assert(a.dim(1) == b.dim(0));
   assert(a.dim(2) == c.dim(2));
@@ -2277,8 +2277,8 @@ convolutionWeightUpdateAop(Graph &graph,
                                                    izg, ox, oy, ozg,
                                                    plan,
                                                    isMultiIPU);
-            addWeightDeltaPartialRegions(partialsMapping[tile], partials.dims(),
-                                         b, oy, ox,
+            addWeightDeltaPartialRegions(partialsMapping[tile],
+                                         partials.shape(), b, oy, ox,
                                          outZGroupBegin, outZGroupEnd,
                                          inZGroupBegin, inZGroupEnd);
             calcPartialWeightGradsAop(graph, tile,
@@ -2308,13 +2308,13 @@ convolutionWeightUpdateAop(Graph &graph,
     weightDeltas = partials[0][0][0];
   } else {
     auto reduceCS = graph.createComputeSet(layerName + "/Reduce");
-    weightDeltas = graph.addTensor(dType, weights.dims(),
+    weightDeltas = graph.addTensor(partialsType, weights.shape(),
                                    layerName + "/WeightDeltas");
     const auto
       weightDeltaMapping = calculateWeightMapping(weights, graph, plan,
                                                   batchSize);
     applyTensorMapping(graph, weightDeltas, weightDeltaMapping);
-    auto flatPartialsDims = weightDeltas.dims();
+    auto flatPartialsDims = weightDeltas.shape();
     flatPartialsDims.insert(flatPartialsDims.begin(), numPartials);
     auto flatPartials = partials.reshape(flatPartialsDims);
     ::reduce(graph, flatPartials, weightDeltas, weightDeltaMapping, reduceCS);

lib/popnn/ActivationMapping.cpp
@@ -17,12 +17,12 @@ computeActivationsMapping(const poplar::Graph &graph, poplar::Tensor act,
   const auto numTiles = graph.getDevice().getDeviceInfo().getNumTiles();
   const auto numActivations = act.numElements();
   unsigned chansPerGroup, beginTile, endTile;
-  if (act.getDimensionality() == 1) {
+  if (act.rank() == 1) {
     chansPerGroup = 1;
     beginTile = 0;
     endTile = numTiles;
   } else {
-    assert(act.getDimensionality() == 4);
+    assert(act.rank() == 4);
     chansPerGroup = act.dim(3);
     const auto batchElemsPerTile = (batchSize + numTiles - 1) / numTiles;
     const auto numBatchGroups =

lib/popnn/Cast.cpp
@@ -39,7 +39,7 @@ cast(poplar::Graph &graph,
      > &mapping,
      poplar::Tensor src, poplar::Tensor dst,
      poplar::ComputeSet cs) {
-  assert(src.dims() == dst.dims());
+  assert(src.shape() == dst.shape());
   src = src.flatten();
   dst = dst.flatten();
   const auto srcType = graph.getTensorElementType(src);

lib/popnn/Net.cpp
@@ -465,18 +465,19 @@ outputResidualDescription(const ResidualLayer &rLayer,
   std::cout << "   -- Residual layer:\n";
   for (auto inputOffset : rLayer.resIndex) {
     auto idx = thisLayer - inputOffset + 1;
-    assert(acts[idx].getDimensionality() == 5);
-    const auto &dims = acts[idx].dims();
+    assert(acts[idx].rank() == 5);
+    const auto &shape = acts[idx].shape();
     std::cout << "        Input(-"<<inputOffset<<"):  "
-              << dims[2] << "x" << dims[3] <<"x" << dims[1] * dims[4] << "\n";
+              << shape[2] << "x" << shape[3] <<"x" << shape[1] * shape[4]
+              << "\n";
   }
 
-  const auto &dims = acts[thisLayer - rLayer.resIndex[0] + 1].dims();
-  auto flops = residual::getNumberOfAdds(dims[2], dims[3],
-                                         dims[1] * dims[4],
+  const auto &shape = acts[thisLayer - rLayer.resIndex[0] + 1].shape();
+  auto flops = residual::getNumberOfAdds(shape[2], shape[3],
+                                         shape[1] * shape[4],
                                          forwardOnly);
   std::cout << "        Output:     "
-            << dims[2] << "x" << dims[3] <<"x" << dims[1] * dims[4] << "\n"
+            << shape[2] << "x" << shape[3] <<"x" << shape[1] * shape[4] << "\n"
             << "        FLOPs:      " << flops << "\n";
 }
 
@@ -591,30 +592,30 @@ Net::createResidualLayerFwd(unsigned i,
   unsigned numChannels, outDimY, outDimX;
   //The output will be the same batch/y/x dimensions as the first input with
   //channel grouping chosen to match the following layer
-  const auto &in0Dims = acts[i - residualLayer.resIndex[0] + 1].dims();
-  numChannels = in0Dims[1] * in0Dims[4];
-  outDimY = in0Dims[2];
-  outDimX = in0Dims[3];
+  const auto &in0Shape = acts[i - residualLayer.resIndex[0] + 1].shape();
+  numChannels = in0Shape[1] * in0Shape[4];
+  outDimY = in0Shape[2];
+  outDimX = in0Shape[3];
   auto outChansPerGroup = getRequiredChansPerGroupFwd(i + 1, outDimY, outDimX,
                                                       numChannels);
   if (outChansPerGroup == 0) {
-    outChansPerGroup = in0Dims[4];
+    outChansPerGroup = in0Shape[4];
   }
 
   acts[i + 1] = graph->addTensor(dType,
-                                 {in0Dims[0], numChannels / outChansPerGroup,
+                                 {in0Shape[0], numChannels / outChansPerGroup,
                                    outDimY, outDimX, outChansPerGroup},
                                  "activations." + std::to_string(i));
   mapActivations(*graph, acts[i + 1]);
   Tensor in0 =
     residual::arrangeResidualInput(*graph,
                                    acts[i - residualLayer.resIndex[0] + 1],
-                                   acts[i + 1].dims(), dType,
+                                   acts[i + 1].shape(), dType,
                                    residualLayer.resMethod);
   Tensor in1 =
     residual::arrangeResidualInput(*graph,
                                    acts[i - residualLayer.resIndex[1] + 1],
-                                   acts[i + 1].dims(), dType,
+                                   acts[i + 1].shape(), dType,
                                    residualLayer.resMethod);
   switch (residualLayer.resMethod) {
   case RESIDUAL_PAD:
@@ -625,15 +626,15 @@ Net::createResidualLayerFwd(unsigned i,
                                in1,
                                acts[i + 1],
                                debugPrefix);
-      auto outDims = acts[i].dims();
-      numFlops += outDims[0] *
-        residual::getNumberOfAdds(outDims[2], outDims[3],
-                                  outDims[1] * outDims[4],
+      auto outShape = acts[i].shape();
+      numFlops += outShape[0] *
+        residual::getNumberOfAdds(outShape[2], outShape[3],
+                                  outShape[1] * outShape[4],
                                   netType == TestOnlyNet);
       perfectCycleTime +=
         residual::getPerfectCycleCount(*graph, dType,
-                                       outDims[0], outDims[2], outDims[3],
-                                       outDims[1] * outDims[4],
+                                       outShape[0], outShape[2], outShape[3],
+                                       outShape[1] * outShape[4],
                                        netType == TestOnlyNet);
       return fwdProg;
     }
@@ -775,7 +776,7 @@ Program Net::createConvLayerBwd(unsigned i,
   auto biases = params[i][1];
   Tensor zDeltas = deltas[i + 1];
   if (nonLinearityType != NON_LINEARITY_NONE) {
-    zDeltas = graph->addTensor(dType, deltas[i + 1].dims(),
+    zDeltas = graph->addTensor(dType, deltas[i + 1].shape(),
                                       "zDeltas");
     mapActivations(*graph, zDeltas);
     prog.add(bwdNonLinearity(*graph, acts[i + 1], deltas[i + 1], zDeltas,
@@ -1059,11 +1060,11 @@ void Net::initialize(DataSet &dataSet, LossType lossType) {
   graph->setTileMapping(numCorrect, 0);
   Tensor loss = graph->addTensor(dType, {batchSize}, "loss");
   graph->setTileMapping(loss, 0);
-  deltas[layers.size()] = graph->addTensor(dType, lastAct.dims(), "deltas");
+  deltas[layers.size()] = graph->addTensor(dType, lastAct.shape(), "deltas");
   mapActivations(*graph, deltas[layers.size()]);
   lastAct = lastAct.reshape({batchSize, lastAct.numElements() / batchSize});
   auto firstDeltas = deltas[layers.size()];
-  firstDeltas = firstDeltas.reshape(lastAct.dims());
+  firstDeltas = firstDeltas.reshape(lastAct.shape());
   auto calcLossProg = calcLoss(*graph,
                                lastAct,
                                expected,
@@ -1087,7 +1088,7 @@ void Net::initialize(DataSet &dataSet, LossType lossType) {
       }
 
       if (backwardPassRequired) {
-        if (acts[i].dims().size() == 5) {
+        if (acts[i].rank() == 5) {
           auto numChannels = acts[i].dim(1) * acts[i].dim(4);
           auto chansPerGroup = getRequiredChansPerGroupBwd(i - 1);
           if (chansPerGroup == 0)
@@ -1100,15 +1101,15 @@ void Net::initialize(DataSet &dataSet, LossType lossType) {
                                                acts[i].dim(3),
                                                chansPerGroup} , "deltas");
          } else {
-           assert(acts[i].dims().size() == 2);
-           deltas[i] = graph->addTensor(dType, acts[i].dims(), "deltas");
+           assert(acts[i].rank() == 2);
+           deltas[i] = graph->addTensor(dType, acts[i].shape(), "deltas");
         }
         mapActivations(*graph, deltas[i]);
       }
 
       const auto *layer = layers[i].get();
       if (const auto *fc = dynamic_cast<const FullyConnectedLayer *>(layer)) {
-        Tensor zDeltas = graph->addTensor(dType, deltas[i + 1].dims(),
+        Tensor zDeltas = graph->addTensor(dType, deltas[i + 1].shape(),
                                           "zDeltas");
         mapActivations(*graph, zDeltas);
         auto weights = params[i][0];

lib/popnn/NonLinearity.cpp
@@ -31,12 +31,12 @@ bwdNonLinearity(Graph &graph,
     Tensor regroupedActs;
     // TODO: This could possible be made more efficient by merging the
     // regrouping with the calculation of the non linearity derivative.
-    if (activations.getDimensionality() == 1 ||
+    if (activations.rank() == 1 ||
         activations.dim(3) == zDeltas.dim(3)) {
       regroupedActs = activations;
     } else {
       auto dType = graph.getTensorElementType(activations);
-      regroupedActs = graph.addTensor(dType, zDeltas.dims(), "regroupedActs");
+      regroupedActs = graph.addTensor(dType, zDeltas.shape(), "regroupedActs");
       auto mapping = computeActivationsMapping(graph, regroupedActs,
                                                b, batchSize);
       applyTensorMapping(graph, regroupedActs, mapping);

lib/popnn/Pad.cpp
@@ -8,39 +8,39 @@ using namespace poplar;
 using namespace poplar::program;
 
 static void validatePadArgs(poplar::Tensor in,
-                            const std::vector<std::size_t> &dims,
+                            const std::vector<std::size_t> &shape,
                             const std::vector<std::size_t> &beforePadding) {
-  // Check the number of dimensions match.
-  const auto numDims = in.getDimensionality();
-  if (dims.size() != numDims) {
+  // Check the ranks match.
+  const auto rank = in.rank();
+  if (shape.size() != rank) {
     std::abort();
   }
-  if (beforePadding.size() != numDims) {
+  if (beforePadding.size() != rank) {
     std::abort();
   }
   // Check the size of the output is greater than the size of the input.
-  for (unsigned i = 0; i != numDims; ++i) {
-    if (in.dim(i) + beforePadding[i] > dims[i])
+  for (unsigned i = 0; i != rank; ++i) {
+    if (in.dim(i) + beforePadding[i] > shape[i])
       std::abort();
   }
 }
 
 poplar::Tensor
 pad(poplar::Graph &graph, poplar::Tensor t,
-    const std::vector<std::size_t> &dims,
+    const std::vector<std::size_t> &shape,
     const std::vector<std::size_t> &beforePadding) {
   const auto type = graph.getTensorElementType(t);
-  validatePadArgs(t, dims, beforePadding);
-  for (unsigned i = 0; i < dims.size(); ++i) {
-    if (dims[i] == t.dim(i))
+  validatePadArgs(t, shape, beforePadding);
+  for (unsigned i = 0; i < shape.size(); ++i) {
+    if (shape[i] == t.dim(i))
       continue;
-    auto beforePadDims = t.dims();
-    beforePadDims[i] = beforePadding[i];
-    auto beforePadding = graph.addConstantTensor(type, beforePadDims, 0);
+    auto beforePadShape = t.shape();
+    beforePadShape[i] = beforePadding[i];
+    auto beforePadding = graph.addConstantTensor(type, beforePadShape, 0);
     t = concat(beforePadding, t, i);
-    auto afterPadDims = t.dims();
-    afterPadDims[i] = dims[i] - t.dim(i);
-    auto afterPadding = graph.addConstantTensor(type, afterPadDims, 0);
+    auto afterPadShape = t.shape();
+    afterPadShape[i] = shape[i] - t.dim(i);
+    auto afterPadding = graph.addConstantTensor(type, afterPadShape, 0);
     t = concat(t, afterPadding, i);
   }
 

lib/popnn/Reduce.cpp
@@ -146,7 +146,7 @@ reduce(Graph &graph,
          std::vector<std::pair<unsigned, unsigned>>
        > &reducedMapping,
        ComputeSet reduceCS) {
-  assert(partials[0].dims() == reduced.dims());
+  assert(partials[0].shape() == reduced.shape());
   if (partials.dim(0) == 0) {
     zero(graph, reduced, reducedMapping, reduceCS);
     return;

lib/popnn/Regroup.cpp
@@ -8,10 +8,9 @@ using namespace poplar::program;
 
 Tensor
 regroup(Tensor in, unsigned outChansPerGroup) {
-  std::vector<std::size_t> originalDims = in.dims();
-  auto numDims = in.getDimensionality();
-  assert(numDims == 4 || numDims == 5);
-  if (numDims == 4)
+  auto rank = in.rank();
+  assert(rank == 4 || rank == 5);
+  if (rank == 4)
     in = in.reshape({1, in.dim(0), in.dim(1), in.dim(2), in.dim(3)});
 
   const auto batchSize = in.dim(0);
@@ -25,9 +24,9 @@ regroup(Tensor in, unsigned outChansPerGroup) {
   Tensor regrouped = in.dimShuffle({0, 2, 3, 1, 4})
            .reshape({batchSize, dimY, dimX, outNumChanGroups, outChansPerGroup})
            .dimShuffle({0, 3, 1, 2, 4});
-  if (numDims == 5)
+  if (rank == 5)
     return regrouped;
-  else //numDims == 4 so unbatched
+  else //rank == 4 so unbatched
     return regrouped.reshape({regrouped.dim(1), regrouped.dim(2),
                               regrouped.dim(3), regrouped.dim(4)});
 }

commit 33fe3c1ec406808de987f9e8912384a445b42c3a

Correct WU-AOP partials type
Summary: broken by merge in D1098/68f463830c468d8cdf320330766496a7652ab34f

Test Plan:
resnet50 --train=1 now runs to completion

Reviews:

Reviewers: davidn

Reviewed By: davidn

Differential Revision: https://phabricator.sourcevertex.net/D1120

lib/popnn/Convolution.cpp
@@ -2308,7 +2308,7 @@ convolutionWeightUpdateAop(Graph &graph,
     weightDeltas = partials[0][0][0];
   } else {
     auto reduceCS = graph.createComputeSet(layerName + "/Reduce");
-    weightDeltas = graph.addTensor(partialsType, weights.shape(),
+    weightDeltas = graph.addTensor(dType, weights.shape(),
                                    layerName + "/WeightDeltas");
     const auto
       weightDeltaMapping = calculateWeightMapping(weights, graph, plan,