4074e518cab2acc50e87ada723ed498c590a7b1b
Eddy Andrews <Eddy.Andrews@graphcore.ai>
2017-02-27 13:13:01 +0000

    Change convReduce cycle estimates to match trial code
    Reviewers: godfrey.da.costa
    
    Reviewed By: godfrey.da.costa
    
    Differential Revision: https://phabricator.sourcevertex.net/D1359

--- lib/popnn/popnn.cpp
+++ lib/popnn/popnn.cpp
@@ -882,14 +882,48 @@ public:
     bool isOutTypeFloat = std::is_same<OutType, float>::value;
     unsigned vectorWidth = dataPathWidth / (isPartialsFloat ? 32 : 16);
     bool conversionCyles = isPartialsFloat != isOutTypeFloat;
-    unsigned cycles = 4;
-    unsigned numReductions = out.size();
-    unsigned numPartials = partials.size() / numReductions;
-    for (unsigned r = 0; r < numReductions; ++r) {
-      unsigned numElem = out[r].size();
-      auto numVectors = (numElem + vectorWidth - 1) / vectorWidth;
-      cycles += 1 + numPartials * (1 + numVectors)
-                + conversionCyles * numVectors;
+    unsigned cycles = 1;
+    const unsigned numReductions = out.size();
+    const unsigned numPartials = partials.size() / numReductions;
+    const unsigned version=2;//TODO: update this following IS changes
+    switch (version) {
+    case 0: // Original optimistic estimate
+    default:
+      cycles = 4;
+      for (unsigned r = 0; r < numReductions; ++r) {
+        unsigned numElem = out[r].size();
+        auto numVectors = (numElem + vectorWidth - 1) / vectorWidth;
+        cycles += 1 + numPartials * (1 + numVectors)
+                  + conversionCyles * numVectors;
+      }
+      break;
+    case 1:
+      // Innermost loop accumulates vector across all input tiles
+      // This estimate based on float->float code
+      // Inner loop cycles likely to halve given minor IS change,
+      // may then be best practical choice
+      cycles = 2+4;
+      for (unsigned r = 0; r < numReductions; ++r) {
+        cycles += 16;
+        const unsigned numElem = out[r].size();
+        auto numVectors = (numElem + vectorWidth - 1) / vectorWidth;
+        cycles += (2 * numPartials + 3) * numVectors;
+      }
+      break;
+    case 2:
+      // Innermost loop adds one tile's input accross a region
+      // This estimate based on float->float code Reductions
+      // in loop overhead are expected given IS changes.
+      cycles = 2+3;
+      for (unsigned r = 0; r < numReductions; ++r) {
+        unsigned numElem = out[r].size();
+        auto numVectors = (numElem + vectorWidth - 1) / vectorWidth;
+        cycles += 24 + numVectors;
+        // inner loop processes 3 vectors
+        unsigned numInners = (numVectors + 2) / 3;
+        cycles += (7 + 3 * numInners + 4 + 3 + 1) * (numPartials - 1);
+      }
+      break;
     }
     return cycles;
   }
