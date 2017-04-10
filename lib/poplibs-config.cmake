a34e52312ccc36cc5f601e2d6896a880dfc490a6
David Lacey <dave.lacey@graphcore.ai>
2017-04-10 16:41:58 +0100

    Add exported cmake module for poplibs
    Summary:
    Add an exported cmake module for poplibs so other repos can add
    find_package(poplibs) to get access to these libraries.
    
    Reviewers: richard
    
    Reviewed By: richard
    
    Differential Revision: https://phabricator.sourcevertex.net/D1610

---
+++ lib/poplibs-config.cmake
@@ -0,0 +1,26 @@
+# Find dependencies
+find_package(poplar REQUIRED)
+
+# Compute paths
+get_filename_component(POPLIBS_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+
+# Our library dependencies (contains definitions for IMPORTED targets)
+if(NOT TARGET popstd AND NOT popstd_BINARY_DIR)
+  include("${POPLIBS_CMAKE_DIR}/../popstd/popstd-targets.cmake")
+endif()
+
+if(NOT TARGET popreduce AND NOT popreduce_BINARY_DIR)
+  include("${POPLIBS_CMAKE_DIR}/../popreduce/popreduce-targets.cmake")
+endif()
+
+if(NOT TARGET popconv AND NOT popconv_BINARY_DIR)
+  include("${POPLIBS_CMAKE_DIR}/../popconv/popconv-targets.cmake")
+endif()
+
+if(NOT TARGET poplin AND NOT poplin_BINARY_DIR)
+  include("${POPLIBS_CMAKE_DIR}/../poplin/poplin-targets.cmake")
+endif()
+
+if(NOT TARGET popnn AND NOT popnn_BINARY_DIR)
+  include("${POPLIBS_CMAKE_DIR}/../popnn/popnn-targets.cmake")
+endif()
