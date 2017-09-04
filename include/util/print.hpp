a0fe92673c3c0b3ed2b7dbb25d675a500cef376f
Eddy Andrews <Eddy.Andrews@graphcore.ai>
2017-09-04 12:03:47 +0100

    Add ostream operator<< for std:vector et al
    Summary:
    Add ostream operator<< for std:vector et al
    Note the templating probably catches other unwanted cases too
    
    Test Plan:
    no functional change to existing code
    tested with a local hack
    
    Reviewers: richard
    
    Reviewed By: richard
    
    Differential Revision: https://phabricator.sourcevertex.net/D2467

--- include/util/print.hpp
+++ include/util/print.hpp
@@ -1,9 +1,37 @@
 #ifndef _print_hpp_
+// Copyright (c) 2017, Graphcore Ltd, All rights reserved.
 #define _print_hpp_
 
 #include <ostream>
+#include <type_traits>
+#include <vector>
+#include <set>
+#include <map>
 
-// Print elements in a container in the form {a,b,c,d}
+// Overload to print a pair
+template <class A, class B>
+static std::ostream & operator<<(std::ostream & os, std::pair<A, B> const &pair)
+{
+  os << "{" << pair.first << ", " << pair.second << "}";
+  return os;
+}
+// Overload ostream output for containers.
+
+// Enable printing of specific containers
+template <class T> struct is_container : std::false_type {};
+template <class T, class A> struct is_container<std::vector<T, A>> :
+  std::true_type {};
+template <class T, class S, class A> struct is_container<std::set<T, S, A>> :
+  std::true_type {};
+template <class K, class T, class C, class A>
+  struct is_container<std::map<K, T, C, A>> : std::true_type {};
+
+//forward declation to support nested containers
+template <class Container>
+typename std::enable_if<is_container<Container>::value, std::ostream &>::type
+operator<<(std::ostream &os, const Container &container);
+
+// Print the elements in a container in the form {a,b,c,d}
 template <class T>
 static void printContainer(const T &container, std::ostream &os) {
   os << "{";
@@ -16,5 +44,15 @@ static void printContainer(const T &container, std::ostream &os) {
   }
   os << '}';
 }
+
+template <class Container>
+typename std::enable_if<is_container<Container>::value, std::ostream &>::type
+operator<<(std::ostream &os, const Container &container)
+{
+  printContainer(container, os);
+  return os;
+}
+
+
 
 #endif // _print_hpp_
