ef2f75ada5c1986f5fedbbfba227426f21472d25
Tim Hutt <timh@graphcore.ai>
2018-05-15 10:54:24 +0000

    Don't install the poplibs_support library or target
    Summary: CMake was still looking for poplibs_support even though it wasn't needed.
    
    Test Plan: Enigma now builds locally, but I will wait for the buildbots to confirm this time!
    
    Reviewers: davel
    
    Reviewed By: davel
    
    Differential Revision: https://phabricator.sourcevertex.net/D4212

--- lib/poplibs-config.cmake
+++ lib/poplibs-config.cmake
@@ -8,7 +8,7 @@ get_filename_component(POPLIBS_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 # These should be in the same order as the add_subdirectory()'s in the
 # CMakeLists.txt so dependencies are handled correctly
 foreach(t
-      popsolver poplibs_support poputil popops poprand popconv poplin popnn
+      popsolver poputil popops poprand popconv poplin popnn
     )
   if(NOT TARGET ${t} AND NOT ${t}_BINARY_DIR)
     include("${POPLIBS_CMAKE_DIR}/../${t}/${t}-targets.cmake")
