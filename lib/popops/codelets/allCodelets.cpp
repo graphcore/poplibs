68fe7427adb1af2dfa1eda70679b33956a601ba3
Sam Kellett <samk@graphcore.ai>
2018-05-22 12:13:48 +0100

    Removed allCodelets.cpp
    Summary: Ref T2886. Removed allCodelets.cpp.
    
    Test Plan: `ninja poplibs`
    
    Reviewers: davel
    
    Reviewed By: davel
    
    Maniphest Tasks: T2886
    
    Differential Revision: https://phabricator.sourcevertex.net/D4317

--- lib/popops/codelets/allCodelets.cpp
+++
@@ -1,5 +0,0 @@
-// Temporary hack until popc supports linking object files.
-#include "dynamicSliceCodelets.cpp"
-#include "elementwiseCodelets.cpp"
-#include "miscCodelets.cpp"
-#include "reduceCodelets.cpp"
