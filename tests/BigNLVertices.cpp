4df88934386e0fe2985964c14b78a92f92b8dfb7
Mark Pupilli <markp@graphcore.ai>
2018-07-06 12:23:32 +0100

    Fix and enable BigNLVertices test
    Summary:
    Splits the test in to sizes that should and shouldn't fit and
    test for the appropriate exception for sizes that do not fit
    on tow tiles.
    
    Also simplify the use of VARIANT so that all IpuModel;Sim;Sim:cpp
    are enabled by default and exceptions that only run on IpuModel
    are explicit.
    
    Reviewers: Eddy, richard, samk
    
    Reviewed By: samk
    
    Differential Revision: https://phabricator.sourcevertex.net/D5089

--- tests/BigNLVertices.cpp
+++ tests/BigNLVertices.cpp
@@ -27,7 +27,6 @@ const OptionFlags options {
 namespace utf = boost::unit_test;
 namespace fpc = boost::test_tools::fpc;
 
-BOOST_AUTO_TEST_CASE(BigVectorList) {
 #include <iostream>
 #include <poplar/Engine.hpp>
 #include <poplar/IPUModel.hpp>
@@ -37,54 +36,82 @@ BOOST_AUTO_TEST_CASE(BigVectorList) {
 #include <poputil/TileMapping.hpp>
 #include <vector>
 
-  // will perform Relu on tensors with this number of elements
-  std::vector<size_t> sizes({100, 1000, 10000, 100000, 1000000});
-  for (const size_t nElms : sizes) {
-    auto device = createTestDevice(TEST_TARGET, 1, 2);
+void testReluWithTensorOfSize(size_t nElms) {
+  auto device = createTestDevice(TEST_TARGET, 1, 2);
 
-    poplar::Graph graph(device);
-    popnn::addCodelets(graph);
+  poplar::Graph graph(device);
+  popnn::addCodelets(graph);
 
-    std::vector<float> hPre(nElms);
-    std::vector<float> hPost(nElms);
-    for (unsigned i = 0; i < nElms; ++i) {
-      // set all initial values to -1.0
-      hPre[i]  = -1.0;
-      hPost[i] = 10.0;
+  std::vector<float> hPre(nElms);
+  std::vector<float> hPost(nElms);
+  for (unsigned i = 0; i < nElms; ++i) {
+    // set all initial values to -1.0
+    hPre[i]  = -1.0;
+    hPost[i] = 10.0;
+  }
+  poplar::Tensor dPre  = graph.addVariable(poplar::FLOAT, {nElms});
+
+  poputil::mapTensorLinearly(graph, dPre);
+
+  poplar::Tensor dPost = graph.clone(dPre, "dPost");
+  poplar::program::Sequence prog;
+  poplar::program::Copy copyProg(dPre, dPost);
+  prog.add(copyProg);
+  popnn::nonLinearity(graph,
+                      popnn::NonLinearityType::NON_LINEARITY_RELU,
+                      dPost,
+                      prog,
+                      "Relu");
+
+  graph.createHostWrite("hPre", dPre);
+  graph.createHostRead("hPost", dPost);
+
+
+
+  poplar::Engine eng(device, graph, prog, options);
+  eng.writeTensor("hPre", hPre.data());
+  eng.run();
+  eng.readTensor("hPost", hPost.data());
+
+  float minVal = hPost[0];
+  for (auto &x : hPost) {
+    if (x < minVal) {
+      minVal = x;
     }
-    poplar::Tensor dPre  = graph.addVariable(poplar::FLOAT, {nElms});
+  }
+  std::cout << "with " << nElms << " elements, min val : " << minVal
+            << std::endl;
+  // Test a float is exactly zero without getting a compiler warning:
+  BOOST_CHECK(minVal >= -0.f && minVal <= 0.f);
+}
 
-    poputil::mapTensorLinearly(graph, dPre);
+BOOST_AUTO_TEST_CASE(BigVectorList) {
 
-    poplar::Tensor dPost = graph.clone(dPre, "dPost");
-    poplar::program::Sequence prog;
-    poplar::program::Copy copyProg(dPre, dPost);
-    prog.add(copyProg);
-    popnn::nonLinearity(graph,
-                        popnn::NonLinearityType::NON_LINEARITY_RELU,
-                        dPost,
-                        prog,
-                        "Relu");
+  // These may need to be updated for different arch versions or
+  // if the code size radically changes:
+  std::vector<size_t> sizesThatFit({100, 1000, 10000});
+  std::vector<size_t> sizesThatDoNotFit({100000, 1000000});
 
-    graph.createHostWrite("hPre", dPre);
-    graph.createHostRead("hPost", dPost);
+  const bool everythingFits = TEST_TARGET == DeviceType::IpuModel ||
+                              TEST_TARGET == DeviceType::Cpu;
 
-
-
-    poplar::Engine eng(device, graph, prog, options);
-    eng.writeTensor("hPre", hPre.data());
-    eng.run();
-    eng.readTensor("hPost", hPost.data());
-
-    float minVal = hPost[0];
-    for (auto &x : hPost) {
-      if (x < minVal) {
-        minVal = x;
-      }
+  // Everything fits on CPU and IPU model:
+  if (everythingFits) {
+    for (const auto s : sizesThatDoNotFit) {
+      sizesThatFit.push_back(s);
     }
-    std::cout << "with " << nElms << " elements, min val : " << minVal
-              << std::endl;
-    // Test a float is exactly zero without getting a compiler warning:
-    BOOST_CHECK(minVal >= -0.f && minVal <= 0.f);
+  }
+
+  for (const auto s : sizesThatFit) {
+    std::out << "Size " << s << " expected to fit\n";
+    testReluWithTensorOfSize(s);
+  }
+
+  if (everythingFits == false) {
+    for (const auto s : sizesThatDoNotFit) {
+      std::out << "Size " << s << " NOT expected to fit\n";
+      BOOST_CHECK_THROW(testReluWithTensorOfSize(s),
+                        graph_memory_allocation_error);
+    }
   }
 }
