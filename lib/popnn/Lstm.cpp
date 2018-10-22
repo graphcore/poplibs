3d74d0db8b5dfcff9f1dac9b3a205149d05cdb39
Richard Osborne <richard@graphcore.ai>
2018-10-22 21:16:36 +0100

    Update the layout / mapping of LSTM variables.
    Summary:
    The input / output is spread evenly over tiles with an grouping of
    16 to match the input grouping of the matrix multiply. After
    the matrix multiply we rearrange each output unit to match this layout
    so no further rearrangement / exchange is required for the elementwise
    operations.
    
    This reduces the number of cycles required for
    enigma/benchmarks/lstm --hidden=1024 --batch-size=32 --steps=20
    from 1139520 to 650858
    
    Depends on D6999
    
    Reviewers: godfrey.da.costa, tomm
    
    Reviewed By: tomm
    
    Differential Revision: https://phabricator.sourcevertex.net/D7008

--- lib/popnn/Lstm.cpp
+++ lib/popnn/Lstm.cpp
@@ -10,6 +10,7 @@
 #include <popops/DynamicSlice.hpp>
 #include <popops/Reduce.hpp>
 #include <popops/ScaledAdd.hpp>
+#include "poplibs_support/gcd.hpp"
 #include "poplibs_support/OptionParsing.hpp"
 #include <cstdint>
 
@@ -194,6 +195,74 @@ static void validateParams(const LstmParams &params) {
   }
 }
 
+/// Create a tensor with dimensions [sequenceLength, numGrains, grainSize]
+/// that satisfies the following properties:
+/// - Grains are never split across tiles.
+/// - The tile mapping and layout is identical for each sub-tensor in the
+///   sequence.
+/// - The elements on a tile form a single contigous region where the
+///   sequenceLength the outer dimension.
+/// These properties make the tensor well suited for use with dynamic
+/// slice / dynamic update
+static Tensor
+createDynamicSliceTensor(Graph &graph,
+                         poplar::Type dataType,
+                         unsigned sequenceLength,
+                         unsigned numGrains, unsigned grainSize,
+                         const std::string &name) {
+  const auto &target = graph.getTarget();
+  const auto numTiles = target.getNumTiles();
+  const auto grainsPerTile = (numGrains + numTiles - 1) / numTiles;
+  const auto numUsedTiles =
+      (numGrains + grainsPerTile - 1) / grainsPerTile;
+  const auto grainsOnLastTile =
+      numGrains - (numUsedTiles - 1) * grainsPerTile;
+  auto tExcludingLast =
+    graph.addVariable(dataType, {numUsedTiles - 1, sequenceLength,
+                                 grainsPerTile, grainSize},
+                      name);
+  auto tLast =
+    graph.addVariable(dataType, {sequenceLength, grainsOnLastTile, grainSize},
+                      name);
+  for (unsigned tile = 0; tile != numTiles; ++tile) {
+    unsigned usedTileIndex = tile * numUsedTiles / numTiles;
+    if (usedTileIndex != (tile + 1) * numUsedTiles / numTiles) {
+      if (usedTileIndex + 1 == numUsedTiles) {
+        graph.setTileMapping(tLast, tile);
+      } else {
+        graph.setTileMapping(tExcludingLast[usedTileIndex], tile);
+      }
+    }
+  }
+  return concat(
+    tExcludingLast.dimRoll(0, 1).flatten(1, 3),
+    tLast,
+    1
+  );
+}
+
+/// Create and map a tensor for a sequence of outputs from a LSTM layer.
+/// The sequence length is taken from \a sequenceLength parameter, not the
+/// \a params structure.
+static Tensor
+createOutputTensor(Graph &graph,
+                   const LstmParams &params,
+                   unsigned sequenceLength,
+                   const std::string &name) {
+  const auto outputSize = params.layerSizes[1];
+  const auto batchSize = params.batchSize;
+  // TODO take output grouping from matmul operation.
+  const auto outputGrouping = gcd(16UL, outputSize);
+  const auto numGroups = (outputSize * batchSize) / outputGrouping;
+  auto output =
+      createDynamicSliceTensor(graph, params.dataType, sequenceLength,
+                               numGroups, outputGrouping, name)
+      .reshapePartial(1, 2, {outputSize / outputGrouping, batchSize})
+      .dimRoll(1, 2)
+      .flatten(2, 4);
+  return output;
+}
+
 Tensor createInput(Graph &graph, const LstmParams &params,
                    const std::string &name,
                    const OptionFlags &options,
@@ -208,9 +277,9 @@ Tensor createInput(Graph &graph, const LstmParams &params,
 
   auto inputSize = params.layerSizes[0];
   auto outputSize = params.layerSizes[1];
-  auto fcOutputSize = BASIC_LSTM_CELL_NUM_UNITS * outputSize;
-  auto fcInputSize = inputSize;
   if (opt.preCalcWeights) {
+    auto fcOutputSize = BASIC_LSTM_CELL_NUM_UNITS * outputSize;
+    auto fcInputSize = inputSize;
     auto fcBatchSize = params.timeSteps * params.batchSize;
     auto in = createMatMulInputLHS(graph, params.dataType,
                                    {fcBatchSize, fcInputSize},
@@ -218,23 +287,18 @@ Tensor createInput(Graph &graph, const LstmParams &params,
                                    name, mmOpt, cache);
     return in.reshape({params.timeSteps, params.batchSize, inputSize});
   } else {
-    std::vector<Tensor> input;
-    auto fcBatchSize = params.batchSize;
-    // These parameters don't match the real parameters since we concatenate the
-    // input and the previous output and do one big matrix multiplication. We
-    // can't use the actual parameters since it might result in the input being
-    // spread across a small number of tiles as input to the LSTM is only half
-    // of the matrix on the LHS.
-    auto in = createMatMulInputLHS(graph, params.dataType,
-                                   {fcBatchSize, fcInputSize},
-                                   {fcInputSize, fcOutputSize},
-                                   name, mmOpt, cache);
-    input.emplace_back(in.expand({0}));
-    for (unsigned i = 1; i != params.timeSteps; ++i) {
-      input.emplace_back(graph.clone(input[0],
-                                     "input[" + std::to_string(i) + "]"));
-    }
-    return concat(input);
+    const auto batchSize = params.batchSize;
+    // TODO take input grouping from matmul operation.
+    const auto inputGrouping = gcd(16UL, inputSize);
+    const auto numInputGroups = (inputSize * batchSize) / inputGrouping;
+    auto in = createDynamicSliceTensor(graph, params.dataType,
+                                       params.timeSteps, numInputGroups,
+                                       inputGrouping,
+                                       name);
+    return
+        in.reshapePartial(1, 2, {inputSize / inputGrouping, batchSize})
+          .dimRoll(1, 2)
+          .flatten(2, 4);
   }
 }
 
@@ -243,15 +307,12 @@ LstmInitialState createInitialState(Graph &graph, const LstmParams &params,
                                     const OptionFlags &options,
                                     matmul::PlanningCache *cache) {
   validateParams(params);
-  auto outputSize = params.layerSizes[1];
-  auto state =
-    graph.addVariable(params.dataType, {2, params.batchSize,
-                                        outputSize},
-                      debugPrefix + "/fwdStateOut");
-  for (auto i = 0; i != 2; ++i) {
-    mapTensorLinearly(graph, state[i]);
-  }
-  return {state[0], state[1]};
+  auto initialOutput =
+      createOutputTensor(graph, params, 1, debugPrefix + "/initialOutput")
+          .squeeze({0});
+  auto initialCellState = graph.clone(initialOutput,
+                                      debugPrefix + "/initialCellState");
+  return {initialOutput, initialCellState};
 }
 
 void zeroInitialState(Graph &graph, const LstmInitialState &state,
@@ -400,6 +461,23 @@ struct LstmInternalState {
   }
 };
 
+static Tensor rearrangeMatMulOutput(Graph &graph, Tensor output,
+                                    Tensor prevCellState,
+                                    Sequence &prog,
+                                    const std::string &debugPrefix) {
+  std::vector<Tensor> toConcat;
+  for (unsigned i = 0; i != BASIC_LSTM_CELL_NUM_UNITS; ++i) {
+    toConcat.push_back(
+      graph.clone(prevCellState,
+                  debugPrefix + "/unitsOutputRearranged").expand({0})
+    );
+  }
+  auto outputRearranged = concat(toConcat);
+
+  prog.add(Copy(output, outputRearranged));
+  return outputRearranged;
+}
+
 static std::pair<LstmRecurrentState,LstmInternalState>
 basicLstmCellForwardPass(Graph &graph,
                          const Tensor &in_,
@@ -466,18 +544,25 @@ basicLstmCellForwardPass(Graph &graph,
                             prog, mmOpt, cache,
                             baseStr + "/ProcessUnits");
   }
+
+  // Rearrange the output of the matrix multiplication so each output unit
+  // arranged the same as the cell state. This avoids the rearrangement
+  // during the subsequent binary operations.
+  unitsOutput = rearrangeMatMulOutput(graph, unitsOutput, prevCellState,
+                                      prog, baseStr);
+
   for (auto u = 0; u != BASIC_LSTM_CELL_NUM_UNITS; ++u) {
     graph.setTileMapping(biases[u],
                          graph.getTileMapping(unitsOutput[u][0]));
   }
+  addInPlace(graph, unitsOutput, bBiases, prog, baseStr + "/AddBias");
+  applyGateNonlinearities(graph, unitsOutput, prog, baseStr);
+
   assert(unitsOutput.dim(0) == BASIC_LSTM_CELL_NUM_UNITS);
   auto forgetGate = unitsOutput[BASIC_LSTM_CELL_FORGET_GATE];
   auto candidate = unitsOutput[BASIC_LSTM_CELL_CANDIDATE];
   auto outputGate = unitsOutput[BASIC_LSTM_CELL_OUTPUT_GATE];
   auto inputGate = unitsOutput[BASIC_LSTM_CELL_INPUT_GATE];
-  addInPlace(graph, unitsOutput, bBiases, prog, baseStr + "/AddBias");
-  applyGateNonlinearities(graph, unitsOutput, prog, baseStr);
-
   auto prod = mul(graph, concat(forgetGate, candidate),
                   concat(prevCellState, inputGate), prog,
                   baseStr + "/{Forget + Input}Gate");
@@ -586,16 +671,22 @@ Tensor lstmFwd(Graph &graph,
           loop, opt.partialsType, opt.inferenceOnly, debugPrefix,
           cache);
   auto retainedState = getRetainedState(newState, internalState, opt);
-  // all output sequence elements take the same mapping so will only
-  // require on-tile copies
-  auto retainedStateSeq =
-      graph.addVariable(params.dataType,
-                        {seqSize, retainedState.dim(0), retainedState.dim(1),
-                         retainedState.dim(2)},
-                         debugPrefix + "/fwdState");
-  for (unsigned i = 0; i != seqSize; ++i) {
-    graph.setTileMapping(retainedStateSeq[i],
-                         graph.getTileMapping(retainedState));
+  Tensor retainedStateSeq;
+  if (opt.inferenceOnly) {
+    retainedStateSeq =
+        createOutputTensor(graph, params, seqSize, "name").expand({1});
+  } else {
+    // all output sequence elements take the same mapping so will only
+    // require on-tile copies
+    retainedStateSeq =
+        graph.addVariable(params.dataType,
+                          {seqSize, retainedState.dim(0), retainedState.dim(1),
+                           retainedState.dim(2)},
+                           debugPrefix + "/fwdState");
+    for (unsigned i = 0; i != seqSize; ++i) {
+      graph.setTileMapping(retainedStateSeq[i],
+                           graph.getTileMapping(retainedState));
+    }
   }
   auto thisStateTensor = thisState.getAsTensor();
   auto newStateTensor = newState.getAsTensor();
