d619aab33be89387cb9ad9155a61e5232854cc0e
Mark Pupilli <markp@graphcore.ai>
2018-10-29 15:15:13 +0000

    Add linspace and meshgrid to poplibs
    Summary:
    These are the building blocks for SpatialSoftMax (which will be
    added separately in a later diff). They also have general purpose
    uses so have been placed in poplin.
    
    Test Plan:
    Added two unit tests one for linspace and
    one for meshgrid.
    
    Reviewers: godfrey.da.costa
    
    Reviewed By: godfrey.da.costa
    
    Differential Revision: https://phabricator.sourcevertex.net/D7176

---
+++ include/poplin/MeshGrid.hpp
@@ -0,0 +1,40 @@
+// Copyright (c) 2018, Graphcore Ltd, All rights reserved.
+
+#ifndef poplin_MeshGrid_hpp
+#define poplin_MeshGrid_hpp
+
+#include <poplar/Graph.hpp>
+
+namespace poplin {
+
+/** Create a constant variable that contains values equally spaced in the
+ *  specified closed range [left, right].
+ * \param graph Graph to which the variable is added.
+ * \param left The first value in the range.
+ * \param right The last value in the range.
+ * \param type Data type of variable to create. Must be FLOAT or HALF.
+ * \return Constant Tensor of rank 1 (vector) containing the linspace values.
+ */
+poplar::Tensor linspace(poplar::Graph &graph, const poplar::Type &type,
+                        float left, float right, size_t count);
+
+/** Create a coordinate grid for each axis by broadcasting the input tensors.
+ * This 2D specialisation only supports two inputs that must be of rank 1
+ * (vectors) and hence the output coordinate grids are always two matrices
+ * (so two outputs of rank two).
+ *
+ * TODO: In the general case of N inputs there would be N outputs of rank N
+ *
+ * \param graph Graph to which the variables are added.
+ * \param x Co-ordinates for the x-axis
+ * \param y Co-ordinates for the y-axis
+ * \return A list of (two) tensors that form co-ordinate grids for each input
+ *         axis. These output tensors will be views of the inputs (reshaped and
+ *         broadcast)
+ */
+std::vector<poplar::Tensor>
+meshgrid2d(poplar::Graph &graph, poplar::Tensor x, poplar::Tensor y);
+
+} // end namespace poplin
+
+#endif // poplin_MeshGrid_hpp
---
+++ tests/MeshGridTest.cpp
@@ -0,0 +1,104 @@
+#define BOOST_TEST_MODULE MeshGridTest
+
+#include <boost/test/unit_test.hpp>
+#include <poplin/MeshGrid.hpp>
+#include "TestDevice.hpp"
+
+#include <vector>
+
+struct TestCase {
+  poplar::Type type;
+  float left;
+  float right;
+  size_t count;
+  std::string name;
+  std::vector<float> values;
+};
+
+BOOST_AUTO_TEST_CASE(LinSpace) {
+  poplar::Device device = createTestDevice(TEST_TARGET);
+  poplar::Graph g(device);
+
+  // Define some linspace arguments with expected result values:
+  std::vector<TestCase> testCases = {
+    {poplar::FLOAT, 11.f, 42.f, 2, "two", {11.f, 42.f}},
+    {poplar::FLOAT, -1.f, 1.f, 3, "small", {-1.f, 0.f, 1.f}},
+    {poplar::FLOAT, 10.f, -10.f, 5, "backwards", {10.f, 5.f, 0.f, -5.f, -10.f}}
+  };
+
+  poplar::program::Sequence prog;
+
+  for (auto c : testCases) {
+    auto var1 = poplin::linspace(g, c.type, c.left, c.right, c.count);
+    g.setTileMapping(var1, 0);
+    auto var2 = g.clone(c.type, var1);
+    prog.add(poplar::program::Copy(var1, var2));
+    g.createHostRead(c.name, var2);
+  }
+
+  poplar::Engine e(g, prog);
+  e.load(device);
+  e.run();
+
+  for (auto c : testCases) {
+    std::vector<float> result(c.values.size(), 0.f);
+    e.readTensor(c.name, result.data());
+    for (auto i = 0u; i < result.size(); ++i) {
+      BOOST_CHECK_EQUAL(result.at(i), c.values.at(i));
+    }
+  }
+}
+
+BOOST_AUTO_TEST_CASE(MeshGrid) {
+  poplar::Device device = createTestDevice(TEST_TARGET);
+  poplar::Graph g(device);
+
+  auto xCoords = poplin::linspace(g, poplar::FLOAT, -1.f, 1.f, 3);
+  auto yCoords = poplin::linspace(g, poplar::FLOAT, -2.f, 2.f, 2);
+  g.setTileMapping(xCoords, 0);
+  g.setTileMapping(yCoords, 0);
+  auto grids = poplin::meshgrid2d(g, xCoords, yCoords);
+
+  auto gridXOut = g.clone(poplar::FLOAT, grids.at(0));
+  auto gridYOut = g.clone(poplar::FLOAT, grids.at(1));
+
+  const auto rowsOut = 2u;
+  const auto colsOut = 3u;
+  BOOST_CHECK_EQUAL(gridXOut.shape()[0], rowsOut);
+  BOOST_CHECK_EQUAL(gridXOut.shape()[1], colsOut);
+  BOOST_CHECK_EQUAL(gridYOut.shape()[0], rowsOut);
+  BOOST_CHECK_EQUAL(gridYOut.shape()[1], colsOut);
+
+  poplar::program::Sequence prog = {
+    poplar::program::Copy(grids.at(0), gridXOut),
+    poplar::program::Copy(grids.at(1), gridYOut)
+  };
+
+  g.createHostRead("xs", gridXOut);
+  g.createHostRead("ys", gridYOut);
+
+  poplar::Engine e(g, prog);
+  e.load(device);
+  e.run();
+
+  // In Poplar, matrices will come back row major so these are
+  // the expected flat results:
+  const std::vector<float> correctXs =
+    {-1.f, 0.f, 1.f,
+     -1.f, 0.f, 1.f};
+  const std::vector<float> correctYs =
+    {-2.f, -2.f, -2.f,
+      2.f,  2.f,  2.f};
+
+  std::vector<float> resultX(2*3);
+  e.readTensor("xs", resultX.data());
+  for (auto i = 0u; i < resultX.size(); ++i) {
+    BOOST_CHECK_EQUAL(resultX.at(i), correctXs.at(i));
+  }
+
+  std::vector<float> resultY(2*3);
+  e.readTensor("ys", resultY.data());
+  for (auto i = 0u; i < resultY.size(); ++i) {
+    BOOST_CHECK_EQUAL(resultY.at(i), correctYs.at(i));
+  }
+}
