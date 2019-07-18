graphcore/poplibs

commit db6ce597807c2a1e61ccf8e9a7c6155b4e541871

Fixed a bug in the convolution planner where it would unconditionally prune for cycles
Summary:
Depends on D12553, D12779. Fixes T9644.

Before the model would always prune the search space based on the best cycle cost found so far. This is incorrect when we want to minimise for memory as it would cause lower-memory plans to not be found.

Additionally, we never pruned the search space based on the best memory cost found so far which would cause a compile time cost, which has been fixed too. Another memory issue was that we did not take into account the fact that the memory of the transformations could be greater than the convolutions -- this case appeared as a result of the changes in this diff and so therefore was fixed here too.

For ResNet50, batch size 4, training this change has the following effect:

|                    | Total Memory  | Max Tile Memory | Always Live          | Max Live Bytes
| ------------  | ----------------  | --------------------  | -------------------- | ------------------
| Before        | 342,376,084   | 306,698                 | 182,199,845       | 338,450,981
| After           | 337,826,590   | 303,366                 | 192,237,744       | 326,254,768
| Difference | 4,549,494        | 3,332                     | **-10,037,899**  | 12,196,213

The always live memory increase is roughly split across:
 - Exchange code (4MB)
 - Vertex and control code (2MB)
 - Vertex data (4MB)
As the planner does not consider always live memory this is unfortunate but hard to avoid.

Test Plan: `./test.sh poplibs -R conv`, additionally verified manually with the gemm tool that the smallest plan is now being found.

Reviewers: tomm, richard

Reviewed By: tomm, richard

Maniphest Tasks: T9644

Differential Revision: https://phabricator.sourcevertex.net/D12656

lib/poplin/ConvPlan.cpp
@@ -660,8 +660,8 @@ public:
   };
 private:
   Type type;
-  unsigned cyclesBound = 0;
-  unsigned tileTempMemoryBound = 0;
+  unsigned cyclesBound = std::numeric_limits<unsigned>::max();
+  unsigned tileTempMemoryBound = std::numeric_limits<unsigned>::max();
   PlanningObjective(Type type) : type(type) {}
 public:
   PlanningObjective() {}
@@ -1198,9 +1198,10 @@ addTempMemoryEstimate(
     popsolver::Model &m,
     const std::vector<PartitionVariables> &partitionVars,
     const std::vector<ConvSizeVariables> &convSizes,
-    const popsolver::Variable &inputsPerTile,
-    const popsolver::Variable &weightsPerTile,
-    const popsolver::Variable &partialsPerTile,
+    const popsolver::Variable inputsPerTile,
+    const popsolver::Variable weightsPerTile,
+    const popsolver::Variable partialsPerTile,
+    const popsolver::Variable transformBytes,
     const poplar::Target &target,
     const ConvParams &params,
     const std::vector<ConvTypes> &types)
@@ -1213,10 +1214,11 @@ addTempMemoryEstimate(
       m.product({m.addConstant(target.getTypeSize(types.back().partialType)),
                  partialsPerTile});
   auto convStorage = m.sum({inputStorage, weightStorage, partialStorage});
+
   // Rearrangements can require both pre- and post-rearranged inputs and/or
   // weights to be required. This may be bigger than the storage need during the
   // convolution.
-  return convStorage;
+  return m.max({convStorage, transformBytes});
 }
 
 static popsolver::Variable
@@ -1402,7 +1404,8 @@ getScaleFactorForTransform(const poplar::Type &type, unsigned dimSize) {
     return {5U, 3U};
 }
 
-popsolver::Variable
+// returns a pair of the number of cycles and the number of bytes per tile.
+static std::pair<popsolver::Variable, popsolver::Variable>
 addTransformCycleEstimate(
     popsolver::Model &m,
     const ConvParams &params,
@@ -1473,8 +1476,11 @@ addTransformCycleEstimate(
       std::min<unsigned>(target.getMemcpyBytesPerCycle(),
                          partialChansPerGroup * inputBytesPerElement);
   if (!rearrangeInput && !rearrangeOutput && !rearrangeWeights &&
-      !regroupOutput && !regroupWeights)
-    return m.addConstant(0);
+      !regroupOutput && !regroupWeights) {
+    const auto zero = m.addConstant(0);
+    return std::make_pair(zero, zero);
+  }
+
   const auto &convSize = transformedConvSizes[ipuLevel];
   std::vector<popsolver::Variable> outputFieldSizes;
   std::vector<popsolver::Variable> inputFieldSizes;
@@ -1522,7 +1528,10 @@ addTransformCycleEstimate(
                    partitionVars[ipuLevel].kernelSplit.end());
   auto ipuUsedTiles = m.product(ipuSplits);
   const auto exchangeBytesPerCycle = target.getExchangeBytesPerCycle();
+
+  std::vector<popsolver::Variable> memoryUsage;
   std::vector<popsolver::Variable> cyclesOperands;
+
   if (rearrangeInput || rearrangeWeights || regroupWeights) {
     const auto reorderBytesPerCycle =
         std::min<unsigned>(target.getMemcpyBytesPerCycle(),
@@ -1553,6 +1562,8 @@ addTransformCycleEstimate(
         auto cycles =
             m.ceildiv(m.product({bytesPerTile, m.addConstant(factor[0])}),
                       m.addConstant(factor[1] * regroupBytesPerCycle));
+
+        memoryUsage.push_back(bytesPerTile);
         cyclesOperands.push_back(cycles);
       }
     }
@@ -1560,6 +1571,7 @@ addTransformCycleEstimate(
     auto numElementsPerTile = m.ceildiv(numElements, ipuUsedTiles);
     auto bytesPerTile = m.product({numElementsPerTile,
                                    m.addConstant(inputBytesPerElement)});
+
     cyclesOperands.push_back(m.ceildiv(bytesPerTile,
                                        m.addConstant(exchangeBytesPerCycle)));
     const auto factor =
@@ -1571,6 +1583,7 @@ addTransformCycleEstimate(
     cyclesOperands.push_back(
         m.ceildiv(m.product({bytesPerTile, m.addConstant(factor[0])}),
                              m.addConstant(reorderBytesPerCycle * factor[1])));
+    memoryUsage.push_back(bytesPerTile);
   }
   if (rearrangeOutput || regroupOutput) {
     auto totalOutputFieldSize = m.product(outputFieldSizes);
@@ -1600,6 +1613,7 @@ addTransformCycleEstimate(
                             m.addConstant(outputReorderBytesPerCycle *
                                           factor[1]))
       );
+      memoryUsage.push_back(bytesPerTile);
     } else if (regroupOutput) {
       const auto factor =
           getScaleFactorForTransform(
@@ -1609,10 +1623,20 @@ addTransformCycleEstimate(
         m.product({bytesPerTile, m.addConstant(factor[0])}),
                   m.addConstant(outputRegroupBytesPerCycle * factor[1]))
       );
+      memoryUsage.push_back(bytesPerTile);
     }
   }
-  auto cycles = m.sum(cyclesOperands);
-  return cycles;
+
+  // the transforms happen serially therefore we sum the cycles and take the
+  // max of the bytes. we also decide that the amount of temporary memory
+  // required is two times the usage as the input and output must be live at the
+  // same time. of course this assumes that the inputs and outputs are the same
+  // size which is not always the case.
+  const auto cycles = m.sum(std::move(cyclesOperands));
+  const auto bytes =
+    m.product({m.max(std::move(memoryUsage)), m.addConstant(2u)});
+
+  return std::make_pair(cycles, bytes);
 }
 
 static std::pair<popsolver::Variable, popsolver::Variable>
@@ -1667,7 +1691,8 @@ addEstimates(popsolver::Model &m,
                                transformedOnceParams, types, linearizeTileOrder,
                                inputsPerLevel, weightsPerLevel);
 
-  const auto transformCycles =
+  popsolver::Variable transformCycles, transformBytes;
+  std::tie(transformCycles, transformBytes) =
       addTransformCycleEstimate(m, untransformedParams, transformedOnceParams,
                                 transformedOnceUnpaddedParams, transforms,
                                 partitionVars, transformedConvSize,
@@ -1681,8 +1706,9 @@ addEstimates(popsolver::Model &m,
 
   const auto tempBytes =
       addTempMemoryEstimate(m, partitionVars, convSize, inputsPerLevel.back(),
-                            weightsPerLevel.back(), partialsPerTile, target,
-                            transformedOnceParams, types);
+                            weightsPerLevel.back(), partialsPerTile,
+                            transformBytes, target, transformedOnceParams,
+                            types);
 
   const auto partialCalcCycles =
       addPartialCalcCycleEstimate(m, partitionVars.back().fieldGrainSize,
@@ -2648,16 +2674,23 @@ constructModel(const poplar::Target &target,
     maxTempBytes = m.max({tempBytes, bwdTempBytes, wuTempBytes});
   }
 
-  auto cyclesBound = bestCost.cycles;
-  if (objective.getCyclesBound() > 0) {
-    cyclesBound = std::min(cyclesBound, objective.getCyclesBound());
+  // if an explicit cycle or memory bound has been added to the objective then
+  // enforce that. additionally, depending on the object type prune the
+  // relevant variable based upon the best plan found so far.
+  auto cyclesBound = objective.getCyclesBound();
+  auto memoryBound = objective.getTileTempMemoryBound();
+
+  switch(objective.getType()) {
+    case PlanningObjective::MINIMIZE_CYCLES:
+      cyclesBound = std::min(cyclesBound, bestCost.cycles);
+      break;
+    case PlanningObjective::MINIMIZE_TILE_TEMP_MEMORY:
+      memoryBound = std::min(memoryBound, bestCost.tileTempMemory);
+      break;
   }
-  if (cyclesBound < std::numeric_limits<unsigned>::max()) {
-    m.lessOrEqual(cycles, cyclesBound);
-  }
-  if (objective.getTileTempMemoryBound() > 0) {
-    m.lessOrEqual(tempBytes, objective.getTileTempMemoryBound());
-  }
+
+  m.lessOrEqual(cycles, cyclesBound);
+  m.lessOrEqual(tempBytes, memoryBound);
 }
 
 static std::pair<Plan, Cost>
@@ -3568,7 +3601,7 @@ createPlan(const ConvParams &params,
   // It doesn't make sense to compare joint and separate planning when the
   // number of cycles is bounded since we can't easily derive bounds for each
   // individual pass from a bound on the total number of cycles.
-  assert(objective.getCyclesBound() == 0);
+  assert(objective.getCyclesBound() == std::numeric_limits<unsigned>::max());
   Plan jointPlan;
   Cost jointCost;
   std::tie(jointPlan, jointCost) =

commit 3ebe4d922d845365267c8d2ea93909476da843ae

Add temp memory estimate for the transforms to the convolution planner
Summary:
Ref T9403, T9644. This diff has been broken off from D12656.

This change has no effect on any of the enigma benchmarks in the rFRAMEWORKCOMP repo (this is probably because smaller plans don't get targeted without T9644).

Test Plan: `./test.sh poplibs` and running the rFRAMEWORKCOMP enigma benchmarks

Reviewers: tomm, richard

Reviewed By: tomm

Maniphest Tasks: T9644, T9403

Differential Revision: https://phabricator.sourcevertex.net/D12911

lib/poplin/ConvPlan.cpp
@@ -1197,13 +1197,13 @@ getScaledExchangeBytesPerCycle(popsolver::Model &m,
 
 
 static popsolver::Variable
-addTempMemoryEstimate(
+addConvTempMemoryEstimate(
     popsolver::Model &m,
     const std::vector<PartitionVariables> &partitionVars,
     const std::vector<ConvSizeVariables> &convSizes,
-    const popsolver::Variable &inputsPerTile,
-    const popsolver::Variable &weightsPerTile,
-    const popsolver::Variable &partialsPerTile,
+    const popsolver::Variable inputsPerTile,
+    const popsolver::Variable weightsPerTile,
+    const popsolver::Variable partialsPerTile,
     const poplar::Target &target,
     const ConvParams &params,
     const std::vector<ConvTypes> &types)
@@ -1217,9 +1217,10 @@ addTempMemoryEstimate(
   auto partialStorage =
       m.product({m.addConstant(target.getTypeSize(types.back().partialType)),
                  partialsPerTile},
-                 "tempConvPartialBytes");
+                "tempConvPartialBytes");
   auto convStorage = m.sum({inputStorage, weightStorage, partialStorage},
                            "tempConvBytes");
+
   // Rearrangements can require both pre- and post-rearranged inputs and/or
   // weights to be required. This may be bigger than the storage need during the
   // convolution.
@@ -1434,7 +1435,8 @@ getScaleFactorForTransform(const poplar::Type &type, unsigned dimSize) {
     return {5U, 3U};
 }
 
-popsolver::Variable
+// returns a pair of the number of cycles and the number of bytes per tile.
+static std::pair<popsolver::Variable, popsolver::Variable>
 addTransformCycleEstimate(
     popsolver::Model &m,
     const ConvParams &params,
@@ -1505,8 +1507,11 @@ addTransformCycleEstimate(
       std::min<unsigned>(target.getMemcpyBytesPerCycle(),
                          partialChansPerGroup * inputBytesPerElement);
   if (!rearrangeInput && !rearrangeOutput && !rearrangeWeights &&
-      !regroupOutput && !regroupWeights)
-    return m.addConstant(0);
+      !regroupOutput && !regroupWeights) {
+    const auto zero = m.addConstant(0);
+    return std::make_pair(zero, zero);
+  }
+
   const auto &convSize = transformedConvSizes[ipuLevel];
   std::vector<popsolver::Variable> outputFieldSizes;
   std::vector<popsolver::Variable> inputFieldSizes;
@@ -1554,7 +1559,10 @@ addTransformCycleEstimate(
                    partitionVars[ipuLevel].kernelSplit.end());
   auto ipuUsedTiles = m.product(ipuSplits);
   const auto exchangeBytesPerCycle = target.getExchangeBytesPerCycle();
+
+  std::vector<popsolver::Variable> memoryUsage;
   std::vector<popsolver::Variable> cyclesOperands;
+
   if (rearrangeInput || rearrangeWeights || regroupWeights) {
     const auto reorderBytesPerCycle =
         std::min<unsigned>(target.getMemcpyBytesPerCycle(),
@@ -1585,6 +1593,8 @@ addTransformCycleEstimate(
         auto cycles =
             m.ceildiv(m.product({bytesPerTile, m.addConstant(factor[0])}),
                       m.addConstant(factor[1] * regroupBytesPerCycle));
+
+        memoryUsage.push_back(bytesPerTile);
         cyclesOperands.push_back(cycles);
       }
     }
@@ -1592,6 +1602,7 @@ addTransformCycleEstimate(
     auto numElementsPerTile = m.ceildiv(numElements, ipuUsedTiles);
     auto bytesPerTile = m.product({numElementsPerTile,
                                    m.addConstant(inputBytesPerElement)});
+
     cyclesOperands.push_back(m.ceildiv(bytesPerTile,
                                        m.addConstant(exchangeBytesPerCycle)));
     const auto factor =
@@ -1603,6 +1614,7 @@ addTransformCycleEstimate(
     cyclesOperands.push_back(
         m.ceildiv(m.product({bytesPerTile, m.addConstant(factor[0])}),
                              m.addConstant(reorderBytesPerCycle * factor[1])));
+    memoryUsage.push_back(bytesPerTile);
   }
   if (rearrangeOutput || regroupOutput) {
     auto totalOutputFieldSize = m.product(outputFieldSizes);
@@ -1632,6 +1644,7 @@ addTransformCycleEstimate(
                             m.addConstant(outputReorderBytesPerCycle *
                                           factor[1]))
       );
+      memoryUsage.push_back(bytesPerTile);
     } else if (regroupOutput) {
       const auto factor =
           getScaleFactorForTransform(
@@ -1641,10 +1654,22 @@ addTransformCycleEstimate(
         m.product({bytesPerTile, m.addConstant(factor[0])}),
                   m.addConstant(outputRegroupBytesPerCycle * factor[1]))
       );
+      memoryUsage.push_back(bytesPerTile);
     }
   }
-  auto cycles = m.sum(cyclesOperands, "transformCycleEstimate");
-  return cycles;
+
+  // the transforms happen serially therefore we sum the cycles and take the
+  // max of the bytes. we also decide that the amount of temporary memory
+  // required is two times the usage as the input and output must be live at the
+  // same time. of course this assumes that the inputs and outputs are the same
+  // size which is not always the case.
+  const auto cycles = m.sum(std::move(cyclesOperands),
+                            "transformCycleEstimate");
+  const auto tempBytes =
+    m.product({m.max(std::move(memoryUsage)), m.addConstant(2u)},
+              "transformTempBytesEstimate");
+
+  return std::make_pair(cycles, tempBytes);
 }
 
 static std::pair<popsolver::Variable, popsolver::Variable>
@@ -1701,7 +1726,8 @@ addEstimates(popsolver::Model &m,
                                transformedOnceParams, types, linearizeTileOrder,
                                inputsPerLevel, weightsPerLevel);
 
-  const auto transformCycles =
+  popsolver::Variable transformCycles, transformTempBytes;
+  std::tie(transformCycles, transformTempBytes) =
       addTransformCycleEstimate(m, untransformedParams, transformedOnceParams,
                                 transformedOnceUnpaddedParams, transforms,
                                 partitionVars, transformedConvSize,
@@ -1713,10 +1739,11 @@ addEstimates(popsolver::Model &m,
                                                   partialChansPerGroup,
                                                   transformedConvSize.back());
 
-  const auto tempBytes =
-      addTempMemoryEstimate(m, partitionVars, convSize, inputsPerLevel.back(),
-                            weightsPerLevel.back(), partialsPerTile, target,
-                            transformedOnceParams, types);
+  const auto convTempBytes =
+      addConvTempMemoryEstimate(m, partitionVars, convSize,
+                                inputsPerLevel.back(), weightsPerLevel.back(),
+                                partialsPerTile, target, transformedOnceParams,
+                                types);
 
   const auto partialCalcCycles =
       addPartialCalcCycleEstimate(m, partitionVars.back().fieldGrainSize,
@@ -1743,9 +1770,13 @@ addEstimates(popsolver::Model &m,
       addReduceCycleEstimate(m, partitionVars, partialsPerTile, target, types,
                              cache);
 
-  const auto totalCycles =
-      m.sum({exchangeCycles, transformCycles, partialCalcCycles, reduceCycles});
-  return {totalCycles, tempBytes};
+  const auto totalCycles = m.sum({exchangeCycles,
+                                  transformCycles,
+                                  partialCalcCycles,
+                                  reduceCycles});
+  const auto totalTempBytes = m.max({transformTempBytes, convTempBytes});
+
+  return {totalCycles, totalTempBytes};
 }
 
 static Plan::Method