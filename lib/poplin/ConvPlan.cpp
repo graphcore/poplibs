graphcore/poplibs

commit 401b8bc655454ccac683fe256c814fe416637616

add logging to conv/matmul/multi-slice/-update
Summary: also fix a planning log typo + unnecessary fp

Test Plan: CI

Reviewers: samk

Reviewed By: samk

Subscribers: simonl

Differential Revision: https://phabricator.sourcevertex.net/D13006

lib/poplin/ConvPlan.cpp
@@ -4345,10 +4345,10 @@ runPlanner(
     }
 
     const auto cyclesBound =
-      cost.cycles * (100.0 + options.cycleBackoffPercent) / 100.0;
+      cost.cycles * (100 + options.cycleBackoffPercent) / 100;
     logging::info("Found fastest plan: {}.", cost);
     logging::info("Applying back-off to find the smallest plan that takes at "
-                  "most {} cycles.", cost, cyclesBound);
+                  "most {} cycles.", cyclesBound);
     logging::trace("{}", plan);
 
     // Now replan for minimimum memory, with the cycles limited.

include/poplibs_support/logging.hpp
@@ -2,6 +2,9 @@
 #define poplibs_support_logging_logging_hpp
 
 #include <string>
+// print.hpp must be included before fmt.hpp so that containers can
+// be logged successfully on macos
+#include "poplibs_support/print.hpp"
 #include <spdlog/fmt/fmt.h>
 #include <spdlog/fmt/ostr.h>
 

lib/poplin/Convolution.cpp
@@ -25,6 +25,7 @@
 #include "poplibs_support/Compiler.hpp"
 #include "poplibs_support/OptionParsing.hpp"
 #include "poplibs_support/VectorUtils.hpp"
+#include "poplibs_support/logging.hpp"
 #include "ConvReduce.hpp"
 #include "ChannelOps.hpp"
 #include "popops/DynamicSlice.hpp"
@@ -4232,15 +4233,26 @@ convolution(Graph &graph, const poplar::Tensor &in_,
 }
 
 Tensor
-convolution(Graph &graph, const poplar::Tensor &in_,
-            const poplar::Tensor &weights_,
-            const ConvParams &params_,
+convolution(Graph &graph, const poplar::Tensor &in,
+            const poplar::Tensor &weights,
+            const ConvParams &params,
             bool transposeAndFlipWeights, Sequence &prog,
             const std::string &debugPrefix,
             const poplar::OptionFlags &options_,
             PlanningCache *cache) {
   const auto options = parseConvOptions(graph.getTarget(), options_);
-  return convolution(graph, in_, weights_, params_, transposeAndFlipWeights,
+  poplibs_support::logging::info(
+      "Convolution(input {}x({}x{}), kernel {}, output = {}x({}x{}), pass={}, "
+      "name=\"{}\"",
+      params.inputFieldShape,
+      params.getNumConvGroups(), params.getNumInputChansPerConvGroup(),
+      params.kernelShape,
+      params.getOutputFieldShape(),
+      params.getNumConvGroups(), params.getNumOutputChansPerConvGroup(),
+      int(options.pass),
+      debugPrefix);
+
+  return convolution(graph, in, weights, params, transposeAndFlipWeights,
                      prog, debugPrefix, options, cache);
 }
 

lib/poplin/MatMul.cpp
@@ -4,6 +4,7 @@
 #include "popops/ScaledAdd.hpp"
 #include "poplibs_support/OptionParsing.hpp"
 #include "poplibs_support/Compiler.hpp"
+#include "poplibs_support/logging.hpp"
 #include <boost/optional.hpp>
 #include <cassert>
 #include <ostream>
@@ -692,6 +693,11 @@ matMul(poplar::Graph &graph,
        const poplar::OptionFlags &options_,
        matmul::PlanningCache *cache) {
   const auto options = parseMatMulOptions(options_);
+  poplibs_support::logging::info("matMul {} x {}, pass={}, name={}",
+                                 A_.shape(), B_.shape(),
+                                 int(options.fullyConnectedPass),
+                                 debugPrefix);
+
   matMulDimChecks(A_.shape(), B_.shape());
   const auto A = A_.expand({0});
   const auto B = B_.expand({0});

lib/popops/DynamicSlice.cpp
@@ -9,6 +9,7 @@
 #include "poputil/TileMapping.hpp"
 #include "poputil/exceptions.hpp"
 #include "popops/ElementWise.hpp"
+#include "poplibs_support/logging.hpp"
 #include <cassert>
 #include <numeric>
 #include <algorithm>
@@ -773,6 +774,9 @@ Tensor multiSlice(Graph &graph,
   // the number of slices changes
   auto sMulti = createUpdateTensor(graph, t, dims, sizes, offset.dim(0),
                                    dName);
+  poplibs_support::logging::info(
+      "multiSlice {} -> {}, name={}",
+      t.shape(), sMulti.shape(), debugPrefix);
   // When there are only a few slices the looping code can be larger than
   // instantiating multiple vertices
   constexpr unsigned inliningThreshold = 3;
@@ -823,6 +827,9 @@ void multiUpdate(Graph &graph,
                   const std::vector<std::size_t> &sizes,
                   Sequence &prog,
                   const std::string &debugPrefix) {
+  poplibs_support::logging::info(
+      "multiUpdate {} into {}, name={}",
+      sMulti.shape(), t.shape(), debugPrefix);
   // small number of slices are updated individually
   // large number of slices are updated by a specialisation or in a loop
   std::string dName = debugPrefix + "/multiUpdate";
@@ -886,6 +893,9 @@ void multiUpdateAdd(Graph &graph,
                     const std::vector<std::size_t> &sizes,
                     Sequence &prog,
                     const std::string &debugPrefix) {
+  poplibs_support::logging::info(
+      "multiUpdateAdd {} into {}, name={}",
+      sMulti.shape(), t.shape(), debugPrefix);
   std::string dName = debugPrefix + "/multiUpdateAdd";
   // Check the offsets have been specified with a multi-slice dimension
   if (offset.rank() != 2)

commit 8e6cb69577b7dc9fe7f831a0967c43f40712f303

correct cyclebackoff calculation
Summary: broken by earlier commit 401b8bc655454ccac683fe256c814fe416637616

Test Plan: TF CI

Reviewers: samk

Reviewed By: samk

Differential Revision: https://phabricator.sourcevertex.net/D13248

lib/poplin/ConvPlan.cpp
@@ -4344,8 +4344,8 @@ runPlanner(
           "No base plan found for unbounded plan");
     }
 
-    const auto cyclesBound =
-      cost.cycles * (100 + options.cycleBackoffPercent) / 100;
+    const unsigned cyclesBound =
+      cost.cycles * (100. + options.cycleBackoffPercent) / 100;
     logging::info("Found fastest plan: {}.", cost);
     logging::info("Applying back-off to find the smallest plan that takes at "
                   "most {} cycles.", cyclesBound);