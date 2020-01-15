fe269d2480ef0af106960009dffce857ebbb4433
Javed Absar <javeda@graphcore.ai>
2020-01-15 13:19:56 +0000

    T14776: Change popnn LossTransform to avoid SCALED_PTR32
    Summary:
    Fix for Loss{Cross,SumSquared}Transform to use ONE_PTR and not
    SCALED_PTR32 unless VECTOR_AVAIL_SCALED_PTR32 is available.
    
    Reviewers: ranjayc, Eddy, #poplar_team
    
    Reviewed By: ranjayc, Eddy, #poplar_team
    
    Subscribers: oleksiik
    
    Maniphest Tasks: T14582, T14776
    
    Differential Revision: https://phabricator.sourcevertex.net/D19766

--- lib/popnn/codelets/LossCrossEntropyTransform.cpp
+++ lib/popnn/codelets/LossCrossEntropyTransform.cpp
@@ -1,9 +1,16 @@
 // Copyright (c) Graphcore Ltd, All rights reserved.
+#include "poplar/AvailableVTypes.h"
 #include "poplibs_support/ExternalCodelet.hpp"
 #include "popops/EncodingConstants.hpp"
 #include <cmath>
 #include <poplar/HalfFloat.hpp>
 #include <poplar/Vertex.hpp>
+
+#ifdef VECTOR_AVAIL_SCALED_PTR32
+static constexpr auto PTR_ALIGN32 = poplar::VectorLayout::SCALED_PTR32;
+#else
+static constexpr auto PTR_ALIGN32 = poplar::VectorLayout::ONE_PTR;
+#endif
 
 using namespace poplar;
 static constexpr auto SCALED_PTR32 = poplar::VectorLayout::SCALED_PTR32;
@@ -14,10 +21,10 @@ template <typename FPType> class LossCrossEntropyTransform : public Vertex {
 public:
   LossCrossEntropyTransform();
 
-  Input<Vector<FPType, SCALED_PTR32, 4>> probs;
-  Input<Vector<FPType, SCALED_PTR32, 4>> expected;
-  Output<Vector<FPType, SCALED_PTR32, 4>> deltas;
-  Output<Vector<FPType, SCALED_PTR32, 4>> transformed;
+  Input<Vector<FPType, PTR_ALIGN32, 4>> probs;
+  Input<Vector<FPType, PTR_ALIGN32, 4>> expected;
+  Output<Vector<FPType, PTR_ALIGN32, 4>> deltas;
+  Output<Vector<FPType, PTR_ALIGN32, 4>> transformed;
   const unsigned short size;
   Input<FPType> deltasScale;
   Input<FPType> modelOutputScaling;
--- lib/popnn/codelets/LossSumSquaredTransform.cpp
+++ lib/popnn/codelets/LossSumSquaredTransform.cpp
@@ -1,7 +1,14 @@
 // Copyright (c) Graphcore Ltd, All rights reserved.
+#include "poplar/AvailableVTypes.h"
 #include "poplibs_support/ExternalCodelet.hpp"
 #include <poplar/HalfFloat.hpp>
 #include <poplar/Vertex.hpp>
+
+#ifdef VECTOR_AVAIL_SCALED_PTR32
+static constexpr auto PTR_ALIGN32 = poplar::VectorLayout::SCALED_PTR32;
+#else
+static constexpr auto PTR_ALIGN32 = poplar::VectorLayout::ONE_PTR;
+#endif
 
 using namespace poplar;
 static constexpr auto SCALED_PTR32 = poplar::VectorLayout::SCALED_PTR32;
@@ -12,10 +19,10 @@ template <typename FPType> class LossSumSquaredTransform : public Vertex {
 public:
   LossSumSquaredTransform();
 
-  Input<Vector<FPType, SCALED_PTR32, 4>> probs;
-  Input<Vector<FPType, SCALED_PTR32, 4>> expected;
-  Output<Vector<FPType, SCALED_PTR32, 4>> deltas;
-  Output<Vector<FPType, SCALED_PTR32, 4>> transformed;
+  Input<Vector<FPType, PTR_ALIGN32, 4>> probs;
+  Input<Vector<FPType, PTR_ALIGN32, 4>> expected;
+  Output<Vector<FPType, PTR_ALIGN32, 4>> deltas;
+  Output<Vector<FPType, PTR_ALIGN32, 4>> transformed;
   const unsigned short size;
 
   IS_EXTERNAL_CODELET(true);
