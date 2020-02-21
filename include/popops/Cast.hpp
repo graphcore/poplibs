59d5aca755de071e615ecf1b8f4528b414821916
Sam Loescher <saml@graphcore.ai>
2020-02-21 15:44:06 +0000

    Add documentation for parameters of cast operation
    Summary:
    Add documentation for parameters of cast operation
    
    Ref T5654
    
    Test Plan: None
    
    Reviewers: claudiop, jamiep, #poplar_team, #poplar_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, Eddy
    
    Reviewed By: jamiep, #poplar_team, #poplar_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, Eddy
    
    Maniphest Tasks: T5654
    
    Differential Revision: https://phabricator.sourcevertex.net/D21432

--- include/popops/Cast.hpp
+++ include/popops/Cast.hpp
@@ -10,35 +10,87 @@
 
 namespace popops {
 
-/// Cast elements of the specified \p src tensor to \p dstType, returning the
-/// result as a new tensor.
+/** Cast elements of the specified \p src tensor to \p dstType, returning the
+ * result as a new tensor.
+ *
+ * Note: If `dstType` == `src.elementType()`, then the operation is a copy.
+ *
+ * \param graph         The graph that the operation will be added to.
+ * \param src           Source tensor to cast.
+ * \param dstType       Type of the destination tensor.
+ * \param prog          Program to add the cast operation to.
+ * \param debugPrefix   Name of the operation, for debugging.
+ * \return              The resultant cast tensor.
+ */
 poplar::Tensor cast(poplar::Graph &graph, const poplar::Tensor &src,
                     const poplar::Type &dstType,
                     poplar::program::Sequence &prog,
                     const std::string &debugPrefix = "");
 
-/// Create a program to copy tensor casting between types (for
-/// example, half->float).
+/** Create a program to copy tensor casting between types (for example,
+ * half->float).
+ *
+ * Precondition: `src.shape()` == `dst.shape()`
+ *
+ * Note: If `dst.elementType()` == `src.elementType()`, then the operation is
+ * just a copy.
+ *
+ * \param graph         The graph that the operation will be added to.
+ * \param src           Source tensor.
+ * \param dst           Destination tensor.
+ * \param debugPrefix   Name of the operation, for debugging.
+ * \return              The program to perform this operation.
+ */
 poplar::program::Program cast(poplar::Graph &graph, poplar::Tensor src,
                               poplar::Tensor dst,
                               const std::string &debugPrefix = "");
 
-/// Create vertices to copy element wise from the \p src tensor to the \p dst
-/// tensor casting between types (for example, half->float).
-/// The vertices are added to the specified compute set.
+/** Create vertices to copy element wise from the \p src tensor to the \p dst
+ * tensor casting between types (for example, half->float).
+ * The vertices are added to the specified compute set.
+ *
+ * Precondition: `src.shape()` == `dst.shape()`
+ *
+ * \param graph     The graph that the operation will be added to.
+ * \param src       Source tensor.
+ * \param dst       Destination tensor.
+ * \param cs        Compute set to add the vertices to.
+ */
 void cast(poplar::Graph &graph, poplar::Tensor src, poplar::Tensor dst,
           poplar::ComputeSet cs);
 
-/// Create vertices to cast elements of the specified \p src tensor to \p
-/// dstType, returning the result as a new tensor. The vertices are added to the
-/// specified compute set.
+/** Create vertices to cast elements of the specified \p src tensor to
+ * \p dstType, returning the result as a new tensor. The vertices are added to
+ * the specified compute set.
+ *
+ * \param graph         The graph that the operation will be added to.
+ * \param src           Source tensor.
+ * \param dstType       Destination type.
+ * \param cs            Compute set to add the vertices to.
+ * \param debugPrefix   Name of the operation, for debugging.
+ * \return              Resultant destination tensor.
+ */
 poplar::Tensor cast(poplar::Graph &graph, poplar::Tensor src,
                     const poplar::Type &dstType, poplar::ComputeSet cs,
                     const std::string &debugPrefix = "");
 
-/// Helper function which checks the relative error in the tensor \p input
-/// when casting it to type \p outputType.  The result is a bool tensor
-/// which is set to true if the error is < \p tolerance.
+/** Helper function which checks the relative error in the tensor \p input
+ * when casting it to type \p outputType. The result is a single element bool
+ * tensor which is set to true if the error is < \p tolerance.
+ *
+ * Preconditions:
+ *  - `input.elementType() == FLOAT`
+ *  - `outputType == HALF`
+ *  - `input.numElements() == 1`
+ *
+ * \param graph         The graph that the operation will be added to.
+ * \param input         Input tensor.
+ * \param outputType    Output type after the cast operation.
+ * \param tolerance     Allowed tolerance in error from cast operation.
+ * \param prog          Program to add the check onto.
+ * \param debugPrefix   Name of the operation, for debugging.
+ * \return              Boolean tensor indicating error < \p tolerance
+ */
 poplar::Tensor checkAccuracyWhenCast(poplar::Graph &graph,
                                      const poplar::Tensor &input,
                                      poplar::Type outputType, double tolerance,
