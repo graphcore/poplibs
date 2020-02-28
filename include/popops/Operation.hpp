9a2ffa13e63a864dd71eb1284192f5012d551da9
Sam Loescher <saml@graphcore.ai>
2020-02-28 11:32:10 +0000

    Add documentation for Operation
    Summary:
    Add documentation for Operation
    
    Ref T5654
    
    Test Plan: Documentation only
    
    Reviewers: timp, richard, #poplar_team, #poplar_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, jamiep
    
    Reviewed By: timp, richard, #poplar_team, #poplar_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, jamiep
    
    Maniphest Tasks: T5654
    
    Differential Revision: https://phabricator.sourcevertex.net/D21936

--- include/popops/Operation.hpp
+++ include/popops/Operation.hpp
@@ -7,19 +7,24 @@
 
 namespace popops {
 
-/// Type of operation in a reduction
+/// Type of operation to use in a reduction.
+/// See Reduce.hpp for example usage.
 enum class Operation {
   ADD,
   MUL,
   MIN,
   MAX,
-  LOGICAL_AND,
-  LOGICAL_OR,
-  SQUARE_ADD,
+  LOGICAL_AND, // Only supports boolean operands.
+  LOGICAL_OR,  // Only supports boolean operands.
+  SQUARE_ADD,  // Squares each element before applying ADD reduction.
 };
 
+/// Parse token from istream to Operation, valid input values are the
+/// stringified enum, e.g. "ADD", "MUL", ...
 std::istream &operator>>(std::istream &is, Operation &op);
 
+/// Write Operation to ostream, value written is the stringified enum,
+/// e.g. "ADD", "MUL", ...
 std::ostream &operator<<(std::ostream &os, const Operation &op);
 
 } // End namespace popops
