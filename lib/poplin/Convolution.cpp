6e687dc4b34d39f441a4ec54f72c082c2b494ffd
Tom Murray <tomm@graphcore.ai>
2020-03-24 15:20:07 +0000

    Re-reverting D23086 - causing some regressions in tensorflow transformer tests.
    Summary: This reverts commit b1115b223ec41fee4948a78db329a6f30fc3e046.
    
    Test Plan: revert-hammer
    
    Reviewers:
    
    Subscribers:

--- lib/poplin/Convolution.cpp
+++ lib/poplin/Convolution.cpp
@@ -4752,7 +4752,10 @@ void convolutionWeightUpdate(Graph &graph, const Tensor &zDeltas,
                                             prog, debugPrefix, options, cache);
   // update weights
   assert(weightDeltas.shape() == weights.shape());
-  scaledAddTo(graph, weights, weightDeltas, scale, prog,
+  const auto maybeRegroupedWeightDeltas =
+      popops::rearrange::regroupIfBeneficial(graph, weightDeltas, weights, prog,
+                                             debugPrefix + "regroupGradds");
+  scaledAddTo(graph, weights, maybeRegroupedWeightDeltas, scale, prog,
               debugPrefix + "/UpdateWeights");
 }
 
