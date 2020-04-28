a7d59331c8682dfb45c17bbc3f15ca246f3416fd
Rich Coombs <richardc@graphcore.ai>
2020-04-28 16:04:48 +0100

    Extending plot_metric_variation.py to support separate graph/execution JSON files.
    Summary: Extending plot_metric_variation.py to support separate graph/execution JSON files.
    
    Test Plan: Tested locally
    
    Reviewers: Eddy, #poplar_team, #poplar_ip_review_-_any_oss_or_third-party_code_use_has_been_approved
    
    Reviewed By: Eddy, #poplar_team, #poplar_ip_review_-_any_oss_or_third-party_code_use_has_been_approved
    
    Maniphest Tasks: T19664
    
    Differential Revision: https://phabricator.sourcevertex.net/D24842

--- tools/plot_metric_variation.py
+++ tools/plot_metric_variation.py
@@ -22,15 +22,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 SCRIPT_NAME = os.path.splitext(os.path.basename(__file__))[0]
-VAR_PATTERN_RANGE = re.compile(r'\{(\d+):(\d+):(\d+)\}') # E.g. {10:100:5}
-VAR_PATTERN_LIST = re.compile(r'\{((\d+,?)+)\}') # E.g. {1,2,3,4}
-KB_SCALE = 1024
-MB_SCALE = 1024*1024
-CLOCK_SPEED = 1.6E9 # Hertz
-MK1_TILE_SIZE = 256 * KB_SCALE # Bytes
-MK2_TILE_SIZE = 624 * KB_SCALE # Bytes
-MK1_IPU_SIZE = 1216 * MK1_TILE_SIZE # Bytes
-MK2_IPU_SIZE = 1472 * MK2_TILE_SIZE # Bytes
+VAR_PATTERN_RANGE = re.compile(r'\\?\{(\d+):(\d+):(\d+)\}') # E.g. {10:100:5}
+VAR_PATTERN_LIST = re.compile(r'\\?\{((\d+,?)+)\}') # E.g. {1,2,3,4}
+PROFILER_FILE_KEYWORD = "PROFILE_FILE"
+GRAPH_PROFILE_FILE_KEYWORD = "GRAPH_PROFILE_FILE"
+EXECUTION_PROFILE_FILE_KEYWORD = "EXECUTION_PROFILE_FILE"
+PARAMETER_KEYWORD = "<PARAM>"
+MB_SCALE = 1024 * 1024
 
 matplotlib.rcParams['ytick.labelsize'] = 'x-small'
 
@@ -42,23 +40,53 @@ def sum_sum(lists):
 def get_args():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter,
-        description='''A tool to plot graph and execution statistics for a Poplar \
-program as a single parameter is methodically incremented.
+        description='''A tool to plot graph and execution statistics for a Poplar program as a \
+single parameter is methodically incremented.
 
-NOTE: Your test executable must support the '--profile-json' option flag.''',
-        epilog=r'''EXAMPLES
-- Show how Resnet metrics scale with batch-size, saving Pickle and Png to default locations:
-    {0} resnet --variant RESNET_32 --batch-size \{{10:51:10}}
+-----------------------------
+### TEST COMMAND VARIABLE ###
+-----------------------------
+The test command must include a parameter to be varied which may be represented in two ways:
 
-- Same again but using variable list instead of range - save to custom Pickle file:
-    {0} --pickle my.pickle resnet --variant RESNET_32 --batch-size \{{10,20,30,40,50}}
+ - {{A:B:C}} - for which the command will be executed once for each value D = A, A+C, A+C+C, ... \
+for all D < B.
+ - {{E,F,G...,H}} - in which the command will be executed once for each value in the list. All \
+values must be integers.
 
-- Load plot data from my.pickle and save plot to my.png:
-    {0} --pickle my.pickle --output my.png
+Remember to escape the opening brace - e.g. \\{{ - to avoid shell expansion.
 
-- Merge several Pickle files and save png to default location:
-    {0} --pickle-merge part1.pickle part2.pickle part3.pickle --pickle merge.pickle
-        '''.format(SCRIPT_NAME + ".py")
+-----------------------------
+### TEST COMMAND KEYWORDS ###
+-----------------------------
+Your test command may also contain the following keywords:
+
+ - {}
+ - {}
+ - {}
+
+Each of which will be replaced with a filename for the respective profile file or graph & \
+execution profile files. If none of these keywords are used --profile-json <file> will be appended \
+to the test command instead.
+
+You may also include {} anywhere in your test command, which will be replaced with the parameter \
+value currently being tested.'''.format(PROFILER_FILE_KEYWORD, GRAPH_PROFILE_FILE_KEYWORD,
+                                        EXECUTION_PROFILE_FILE_KEYWORD, PARAMETER_KEYWORD),
+        epilog='''\
+----------------
+### EXAMPLES ###
+----------------
+ - Show how Resnet metrics scale with batch-size, saving Pickle and Png to default locations:
+     {0} resnet --variant RESNET_32 --batch-size \\{{10:51:10}}
+
+ - Same again but using variable list instead of range - save to custom Pickle file:
+     {0} --pickle my.pickle resnet --variant RESNET_32 --batch-size \\{{10,20,30,40,50}}
+
+ - Load plot data from my.pickle and save plot to my.png:
+     {0} --pickle my.pickle --output my.png
+
+ - Merge several Pickle files and save png to default location:
+     {0} --pickle-merge part1.pickle part2.pickle part3.pickle --pickle merge.pickle
+         '''.format(SCRIPT_NAME + ".py")
     )
     parser.add_argument(
         "--output", default=SCRIPT_NAME + ".png",
@@ -66,38 +94,32 @@ NOTE: Your test executable must support the '--profile-json' option flag.''',
     )
     parser.add_argument(
         "--pickle", default=SCRIPT_NAME + ".pickle",
-        help="[=%(default)s] Name of Pickle file used for plot data save/restore. "
-        "Pickle file is used for output if 'command' is provided - input otherwise."
+        help="[=%(default)s] Name of Pickle file used for plot data save/restore. Pickle file is "
+        "used for output if 'command' is provided - input otherwise."
     )
     parser.add_argument(
         "--pickle-off", action='store_true',
         help="[=%(default)s] Disable save/restore of plot data in Pickle file."
     )
     parser.add_argument(
-        "--pickle-merge", nargs='*', help="Got results spread across several Pickle files? "
-        "List them all here and they'll be plotted on the same graph and Pickled to --pickle. "
-        "Cannot be used if `command` is provided."
+        "--pickle-merge", nargs='*', help="Got results spread across several Pickle files? List "
+        "them all here and they'll be plotted on the same graph and Pickled to --pickle. Cannot be "
+        "used if `command` is provided."
     )
     parser.add_argument(
         "--title", help="Plot figure title. If not set, test command will be used."
     )
     parser.add_argument(
-        "--max-parallel", type=int, default=8, help="[=%(default)s] Maximum number of "
-        "parallel processes to be running at any time."
+        "--max-parallel", type=int, default=8, help="[=%(default)s] Maximum number of parallel "
+        "processes to be running at any time."
     )
     parser.add_argument(
-        "--num-iterations", type=int, default=1, help="[=%(default)s] Indicate how many "
-        "iterations your test command uses - so that item rates are calculated correctly."
+        "--num-iterations", type=int, default=1, help="[=%(default)s] Indicate how many iterations "
+        "your test command uses - so that item rates are calculated correctly."
     )
     parser.add_argument(
         "command", nargs=argparse.REMAINDER,
-        help="Must be last argument. The program command to call and "
-        "analyse. The command must include a parameter to be varied which can "
-        "be represented in two ways. Either the pattern '{A:B:C}', for which the "
-        "command will be executed once for each value D = A, A+C, A+C+C, ... for "
-        "all D < B. Or '{E,F,G...,H}', in which the command will be executed once "
-        "for each value in the list. All values must be integers. Remember to escape "
-        r"the opening brace - e.g. \{ - to avoid shell expansion."
+        help="Must be last argument. The program test command to call and analyse. "
     )
     return parser.parse_args()
 
@@ -105,19 +127,20 @@ NOTE: Your test executable must support the '--profile-json' option flag.''',
 def get_var_name_params(cmd):
     all_var_matches_range = list(filter(VAR_PATTERN_RANGE.search, cmd))
     all_var_matches_list = list(filter(VAR_PATTERN_LIST.search, cmd))
+    var_name_pattern = r'--?([a-zA-Z\-]+)[=|\s][\"|\']?'
     if len(all_var_matches_range) is 1:
         var_match = VAR_PATTERN_RANGE.search(all_var_matches_range[0])
         params = np.arange(int(var_match.group(1)),
                            int(var_match.group(2)),
                            int(var_match.group(3)))
 
-        name_pattern = re.compile(r'--?([a-zA-Z\-]+)[=|\s]' + VAR_PATTERN_RANGE.pattern)
+        name_pattern = re.compile(var_name_pattern + VAR_PATTERN_RANGE.pattern)
         var_name = name_pattern.search(" ".join(cmd)).group(1)
         return (var_name, params, VAR_PATTERN_RANGE)
     elif len(all_var_matches_list) is 1:
         var_match = VAR_PATTERN_LIST.search(all_var_matches_list[0]).group(1)
         params = list(map(int, var_match.split(",")))
-        name_pattern = re.compile(r'--?([a-zA-Z\-]+)[=|\s]' + VAR_PATTERN_LIST.pattern)
+        name_pattern = re.compile(var_name_pattern + VAR_PATTERN_LIST.pattern)
         var_name = name_pattern.search(" ".join(cmd)).group(1)
         return (var_name, params, VAR_PATTERN_LIST)
     else:
@@ -180,42 +203,73 @@ def main():
         plot_data(var_name, x_values, data, args)
 
 
-def generate_data(args, params, pattern, x_values, data):
-    cmd = args.command
+def get_commands(args, param_pattern, params, out_dir):
+    file_pattern_count = 0
+    outfile_prefix = os.path.join(out_dir, PARAMETER_KEYWORD)
+    cmd = []
 
+    for sub in args.command:
+        sub = sub.replace(GRAPH_PROFILE_FILE_KEYWORD, outfile_prefix + '.graph_json')
+        sub = sub.replace(EXECUTION_PROFILE_FILE_KEYWORD, outfile_prefix + '.exec_json')
+        sub = sub.replace(PROFILER_FILE_KEYWORD, outfile_prefix + '.json')
+        file_pattern_count += sub.count(PARAMETER_KEYWORD)
+        sub = param_pattern.sub(PARAMETER_KEYWORD, sub)
+        cmd.append(sub)
+
+    if file_pattern_count is 0:
+        cmd.append('--profile-json')
+        cmd.append(outfile_prefix + '.json')
+
+    return [[sub.replace(PARAMETER_KEYWORD, str(param)) for sub in cmd] for param in params]
+
+
+def generate_data(args, params, pattern, x_values, data):
     assert (not os.path.dirname(args.pickle) or os.access(os.path.dirname(args.pickle), os.W_OK)),\
            "- ERROR: Cannot create {}.".format(args.pickle)
 
-    assert (os.path.isfile(cmd[0]) and os.access(cmd[0], os.X_OK)),\
-           "- ERROR: File '{}' does not exist or is not executable.".format(cmd[0])
-
     with tempfile.TemporaryDirectory() as out_dir:
-        # Create list of all cmd variants (variable value and JSON output file)
-        out_files = [['--profile-json', os.path.join(out_dir, str(param) + '.json')]
-                     for param in params]
-        cmds = [[pattern.sub(str(param), substring) for substring in cmd] for param in params]
-        cmds = [[*cmd, *out] for (cmd, out) in zip(cmds, out_files)]
-        print(" - Spawning {} processes ({} at a time)...".format(len(cmds), args.max_parallel))
+        commands = get_commands(args, pattern, params, out_dir)
+        print(" - Spawning {} processes ({} at a time)...".format(len(commands), args.max_parallel))
         with ThreadPoolExecutor(args.max_parallel) as pool:
-            exit_codes = pool.map(lambda cmd: call(cmd, stdout=DEVNULL, stderr=DEVNULL), cmds)
+            exit_codes = pool.map(lambda cmd: call(cmd, stdout=DEVNULL, stderr=DEVNULL), commands)
 
         print(" - All processes finished, with the following exit codes:")
         print("\n   | Test | Exit |")
         print("   | value| code | Command")
         print("\n".join("   |{: >5} | {: <5}| {}".format(x, y, " ".join(z))
-                        for x, y, z in zip(params, exit_codes, cmds)))
+                        for x, y, z in zip(params, exit_codes, commands)))
         print("\n - Investigate any unexpected exit codes by running your test command "
               "again with the corresponding parameter value.")
 
         for filename in os.listdir(out_dir):
+            # To maintain correct order, we will use .graph_json to find matching .exec_json files.
+            if filename.endswith(".exec_json"):
+                continue
+
             batch_size = int(os.path.splitext(filename)[0])
             x_values.append(batch_size)
 
             with open(os.path.join(out_dir, filename), 'r') as out:
                 result = json.load(out)
 
+            execution = {}
+            graph = {}
+            if filename.endswith(".graph_json"):
+                graph = result
+                exec_path = os.path.join(out_dir, str(batch_size) + '.exec_json')
+                if os.path.isfile(exec_path):
+                    with open(exec_path, 'r') as out:
+                        execution = json.load(out)
+            else:
+                graph = result['graphProfile']
+                execution = result['executionProfile']
+
+            # Target constants
+            data['total_memory'] = graph['target']['totalMemory']
+            data['bytes_per_tile'] = graph['target']['bytesPerTile']
+            data['clock_frequency'] = graph['target']['clockFrequency']
+
             # Memory usage
-            graph = result['graphProfile']
             data['exchange_bytes'].append(sum_sum(graph['exchanges']['codeBytesByTile']))
             data['memory_bytes_gaps'].append(sum(graph['memory']['byTile']['totalIncludingGaps']))
             liveness = graph['memory']['liveness']
@@ -241,12 +295,12 @@ def generate_data(args, params, pattern, x_values, data):
 
             # Simulated cycle counts
             # Host exchange and sync cycles are omitted as they are too large for the graph
-            if 'simulation' in result['executionProfile']:
-                simulation = result['executionProfile']['simulation']
-                data['cycles'].append(simulation['cycles'])
-                if 'compute' in simulation['tileCycles']:
-                    data['compute_cycles'].append(simulation['tileCycles']['compute'])
-                    data['exchange_cycles'].append(simulation['tileCycles']['doExchange'])
+            if 'simulation' in execution:
+                data['cycles'].append(execution['simulation']['cycles'])
+                tile_cycles = execution['simulation']['tileCycles']
+                if 'compute' in tile_cycles:
+                    data['compute_cycles'].append(tile_cycles['compute'])
+                    data['exchange_cycles'].append(tile_cycles['doExchange'])
 
 
 def plot_data(var_name, x_values, data, args):
@@ -264,20 +318,16 @@ def plot_data(var_name, x_values, data, args):
     axs[0, 0].plot(x_values, np.array(data['memory_bytes_gaps'])/MB_SCALE, "C5s", fillstyle='none')
     axs[0, 0].set_ylabel("Total Usage (MB)", color='C5')
     axs[0, 0].tick_params(axis='y', labelcolor='C5')
-    axs[0, 0].axhline((MK1_IPU_SIZE/MB_SCALE), dashes=[0, 4, 4, 0], color='C5')
     _, ymax = axs[0, 0].get_ylim()
-    if ymax > (0.75 * MK2_IPU_SIZE / MB_SCALE):
-        axs[0, 0].axhline((MK2_IPU_SIZE / MB_SCALE), color='C5')
+    axs[0, 0].axhline(data['total_memory'] / MB_SCALE, dashes=[0, 4, 4, 0], color='C5')
 
     max_ax = axs[0, 0].twinx()
     max_ax.plot(x_values, np.array(data['max_bytes'])/MB_SCALE, "C0o")
     max_ax.plot(x_values, np.array(data['max_bytes_gaps'])/MB_SCALE, "C0s", fillstyle='none')
     max_ax.set_ylabel("Max. Tile Usage (MB)", color='C0')
-    max_ax.axhline((MK1_TILE_SIZE/MB_SCALE), dashes=(4, 4), color='C0')
     max_ax.tick_params(axis='y', labelcolor='C0')
     _, ymax = max_ax.get_ylim()
-    if ymax > (0.75 * MK2_TILE_SIZE / MB_SCALE):
-        max_ax.axhline((MK2_TILE_SIZE / MB_SCALE), color='C0')
+    max_ax.axhline(data['bytes_per_tile'] / MB_SCALE, dashes=(4, 4), color='C0')
     max_ax.set_ylim(bottom=0)
 
     ############################################################################
@@ -294,7 +344,7 @@ def plot_data(var_name, x_values, data, args):
     item_rate_ax = axs[2, 1].twinx()
     if data['cycles']:
         axs[2, 1].plot(x_values, np.array(data['cycles'])*100, "C1^", label="Total (x100)")
-        item_rates = (args.num_iterations * CLOCK_SPEED *
+        item_rates = (args.num_iterations * data['clock_frequency'] *
                       np.array(x_values) / np.array(data['cycles']))
         item_rate_ax.plot(x_values, item_rates, "C0o", label="Items rate", fillstyle='none')
         item_rate_ax.set_ylabel("Items/second", color='C0')
