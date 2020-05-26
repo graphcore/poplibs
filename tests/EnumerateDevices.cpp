161b7e8ee74c2fa4b97bd52f692caddf8dc20fae
Hector Montaner <hectorm@graphcore.ai>
2020-05-26 14:07:42 +0100

    T19072: Hw2 variant for testing
    Summary:
    To allow enabling specific tests only on IPU2 hardware, a new fixture test has been created that can inspect the architecture of the present IPUs.
    Tests labelled with VARIANT Hw2 will run if ENABLED_TEST_VARIANTS contains Hw and the new fixture test finds IPU2 architecture.
    
    Reviewers: #poplar_team, #poplar_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, oleksiik, Eddy
    
    Reviewed By: #poplar_team, #poplar_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, oleksiik, Eddy
    
    Subscribers: samk
    
    Maniphest Tasks: T19072
    
    Differential Revision: https://phabricator.sourcevertex.net/D26455

--- tests/EnumerateDevices.cpp
+++ tests/EnumerateDevices.cpp
@@ -7,5 +7,16 @@
 BOOST_AUTO_TEST_CASE(Enumerate) {
   // This test relies on createTestDevice() throwing in case of failure to
   // enumerate at least one device. Could be more sophisticed in future:
-  auto device = createTestDevice(DeviceType::Hw);
+  auto device = createTestDeviceFullSize(DeviceType::Hw);
+  const auto argc = boost::unit_test::framework::master_test_suite().argc;
+  const auto argv = boost::unit_test::framework::master_test_suite().argv;
+  if (argc > 1) {
+    if (argc != 2) {
+      throw std::logic_error(
+          "Too many arguments: Takes one optional arguments: <arch>");
+    }
+    const auto archString = device.getTarget().getTargetArchString();
+    const auto requiredArchString = argv[1];
+    BOOST_CHECK(archString == requiredArchString);
+  }
 }
