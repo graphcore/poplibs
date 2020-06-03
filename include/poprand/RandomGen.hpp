c2b0e20d53a2dfb470731e4791413fff4bdd7f7a
Jamie Packer <jamiep@graphcore.ai>
2020-06-03 11:07:07 +0200

    Tidy up documentation for random operations
    Reviewers: #poplar_team, #poplar_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, jakeh, Eddy, stephenm
    
    Reviewed By: #poplar_team, #poplar_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, Eddy
    
    Maniphest Tasks: T21280
    
    Differential Revision: https://phabricator.sourcevertex.net/D26640

--- include/poprand/RandomGen.hpp
+++ include/poprand/RandomGen.hpp
@@ -13,100 +13,156 @@
 
 namespace poprand {
 
-/// Apply dropout to input tensor.
-///
-/// The \p input tensor is multiplied by a sequence of 1 or 0. The
-/// keep probability of the dropout is P(1) = \p keepProbability. The reference
-/// tensor must be of the same shape as the input. The layout of the output is
-/// the same as the reference to guarantee that if the same seed and
-/// \p seedModifier is given then the same mask is generated.
-///
-/// The scale factor scales the input tensor and should
-/// typically be the inverse of the dropout probability.
-///
-/// If \p seed is not null, it is a tensor of shape {2} that is used to
-/// seed the generator to generate the dropout mask. The h/w random generator
-/// state at the end of dropout is restored to be the same as before it is
-/// applied.
+/** Apply dropout to a tensor.
+ *
+ *  The elements of tensor \p input are multiplied by a mask consisting of a
+ *  sequence of randomly generated 1 or 0. The keep probability of the dropout
+ *  P(1) = \p keepProbability.
+ * The contents of the mask depend on the keep probability, seed, seed modifier
+ * and layout of the reference tensor.
+ *
+ *  \param graph            The graph to add this operation to.
+ *  \param seed             If not null, this is a pair of 32-bit integers used
+ *                          to seed the random number generator that generates
+ *                          the dropout mask.
+ *  \param seedModifier     Provides a further modification of the seed value.
+ *                          Ignored if \p seed is null.
+ *  \param input            The input tensor to be masked.
+ *  \param reference        A tensor that specifies the layout of the output
+ *                          tensor.
+ *                          Must be the same shape as the input.
+ *  \param keepProbability  The probability of keeping an input value.
+ *  \param scale            Scales the output tensor. This is typically the
+ *                          inverse of the dropout probability, (1 / P(1)).
+ *  \param prog             The program to add this operation to.
+ *  \param debugPrefix      A prefix string for debugging.
+ *
+ *  \returns A tensor with elements randomly set to either zero or the scaled
+ *           input value.
+ */
 poplar::Tensor dropout(poplar::Graph &graph, const poplar::Tensor *seed,
                        const uint32_t seedModifier, const poplar::Tensor &input,
                        const poplar::Tensor &reference, double keepProbability,
                        double scale, poplar::program::Sequence &prog,
                        const std::string &debugPrefix = "");
 
-/// Uniform distribution in a given interval with \p maxVal > \p minVal.
-///
-/// The tensor \p A may be of type "float", "half" or "int". It generates data
-/// with uniform distribution in the interval [minVal, maxVal]. For "int",
-/// data is generated in interval [minVal, maxVal] with uniform probability if
-/// maxVal - minVal is a power of 2. Otherwise there will be a small bias in
-/// the probability generated with the bias directly proportional to the ratio
-/// maxVal-minVal+1 / 2^32.
-///
-/// The output has the same shape and mapping as the reference to guarantee that
-/// the same output is generated if the \p seed and \p seedModifier is used.
-///
-/// If \p seed is not null, it is a tensor of shape {2} that is used to
-/// seed the generator. The h/w random generator state at the end of generation
-/// is restored to be the same as before it is applied. \p seedModifier is
-/// ignored if \p seed is a nullptr.
+/** Uniform distribution in a given interval with \p maxVal > \p minVal.
+ *
+ *  Generates random data with uniform distribution in the interval [\p minVal,
+ *  \p maxVal]. The output may be of type \c float, \c half or \c int.
+ *
+ *  For type \c int, data is generated in the interval [\p minVal, \p maxVal]
+ *  with uniform probability if (\p maxVal - \p minVal) is a power of 2.
+ *  Otherwise there will be a small bias in the probability generated, with the
+ *  bias directly proportional to the ratio (\p maxVal - \p minVal + 1 ) / 2^32.
+ *
+ *  \param graph            The graph to add this operation to.
+ *  \param seed             If not null, this is a pair of 32-bit integers used
+ *                          to seed the random number generator that generates
+ *                          the distribution.
+ *  \param seedModifier     Provides a further modification of the seed value.
+ *                          Ignored if \p seed is null.
+ *  \param reference        A tensor that specifies the layout of the output
+ *                          tensor.
+ *  \param outType          Type of the output tensor. One of \c float, \c half
+ *                          or \c int.
+ *  \param minVal           The minimum value of the distribution.
+ *  \param maxVal           The maximum value of the distribution.
+ *  \param prog             The program to add this operation to.
+ *  \param debugPrefix      A prefix string for debugging.
+ *
+ *  \returns A tensor with elements having a uniform distribution of random
+ *           values.
+ */
 poplar::Tensor uniform(poplar::Graph &graph, const poplar::Tensor *seed,
                        uint32_t seedModifier, const poplar::Tensor &reference,
                        const poplar::Type &outType, double minVal,
                        double maxVal, poplar::program::Sequence &prog,
                        const std::string &debugPrefix = "");
 
-/// Bernoulli distribution which has the value 1 with probability \p prob.
-///
-/// Tensor types supported are float, half and int.
-///
-/// The output has the same shape and mapping as the reference to guarantee
-/// that the same output is generated if the \p seed and \p seedModifier is
-/// used.
-///
-/// If \p seed is not null, it is a tensor of shape {2} that is used to
-/// seed the generator. The hardware random generator state at the end of
-/// generation
-/// is restored to be the same as before it is applied. \p seedModifier is
-/// ignored if \p seed is a nullptr.
+/** Bernoulli distribution which has the value 1 with the specified probability.
+ *
+ *  Generates a tensor with random values of 0 and 1, determined by \p prob.
+ *
+ *  \param graph            The graph to add this operation to.
+ *  \param seed             If not null, this is a pair of 32-bit integers used
+ *                          to seed the random number generator that generates
+ *                          the distribution.
+ *  \param seedModifier     Provides a further modification of the seed value.
+ *                          Ignored if \p seed is null.
+ *  \param reference        A tensor that specifies the layout of the output
+ *                          tensor.
+ *  \param outType          Type of the output tensor. One of \c float, \c half
+ *                          or \c int.
+ *  \param prob             Probability of an element being 1.
+ *  \param prog             The program to add this operation to.
+ *  \param debugPrefix      A prefix string for debugging.
+ *
+ *  \returns A tensor with elements randomly set to either zero or the scaled
+ *           input value.
+ */
 poplar::Tensor bernoulli(poplar::Graph &graph, const poplar::Tensor *seed,
                          uint32_t seedModifier, const poplar::Tensor &reference,
                          const poplar::Type &outType, double prob,
                          poplar::program::Sequence &prog,
                          const std::string &debugPrefix = "");
 
-/// Normal distribution with given mean and standard deviation.
-///
-/// The tensor A may be of type "half" and "float".
-///
-/// The output has the same shape and mapping as the reference to guarantee that
-/// the same output is generated if the \p seed and \p seedModifier is used.
-///
-/// If \p seed is not null, it is a tensor of shape {2} that is used to
-/// seed the generator. The h/w random generator state at the end of generation
-/// is restored to be the same as before it is applied. \p seedModifier is
-/// ignored if \p seed is a nullptr.
+/** Normal distribution with given mean and standard deviation.
+ *
+ *  Generates random data with a normal (Gaussian) distribution. The mean
+ *  is given by \p mean and the standard deviation by \p stdDev.
+ *
+ *  \param graph            The graph to add this operation to.
+ *  \param seed             If not null, this is a pair of 32-bit integers used
+ *                          to seed the random number generator that generates
+ *                          the distribution.
+ *  \param seedModifier     Provides a further modification of the seed value.
+ *                          Ignored if \p seed is null.
+ *  \param reference        A tensor that specifies the layout of the output
+ *                          tensor.
+ *  \param outType          Type of the output tensor. One of \c float or
+ *                          \c half.
+ *  \param mean             The mean value of the distribution.
+ *  \param stdDev           The standard deviation of the distribution.
+ *  \param prog             The program to add this operation to.
+ *  \param debugPrefix      A prefix string for debugging.
+ *
+ *  \returns A tensor with elements randomly set to either zero or the scaled
+ *           input value.
+ */
 poplar::Tensor normal(poplar::Graph &graph, const poplar::Tensor *seed,
                       uint32_t seedModifier, const poplar::Tensor &reference,
                       const poplar::Type &outType, double mean, double stdDev,
                       poplar::program::Sequence &prog,
                       const std::string &debugPrefix = "");
 
-/// Truncated normal distribution derived from a normal distribution with mean
-/// \p mean and standard deviation \p stdDev. This normal distribution is
-/// truncated symmetrically about the mean at:
-///
-///     (mean - alpha * stdDev) and (mean + alpha * stdDev)
-///
-/// The tensor A may be of type "half" and "float"
-///
-/// The output has the same shape and mapping as the reference to guarantee that
-/// the same output is generated if the \p seed and \p seedModifier is used.
-///
-/// If \p seed is not null, iit is a tensor of shape {2} that is used to
-/// seed the generator. The h/w random generator state at the end of generation
-/// is restored to be the samethe same as before it is applied. \p seedModifier
-/// is ignored if \p seed is a nullptr.
+/** Truncated normal distribution.
+ *
+ *  Generates a distribution derived from a normal distribution with mean
+ *  \p mean and standard deviation \p stdDev. This normal distribution is
+ *  truncated symmetrically about the mean at
+ *  (\p mean - \p alpha * \p stdDev) and (\p mean + \p alpha * \p stdDev)
+ *
+ *  \param graph            The graph to add this operation to.
+ *  \param seed             If not null, this is a pair of 32-bit integers used
+ *                          to seed the random number generator that generates
+ *                          the distribution.
+ *  \param seedModifier     Provides a further modification of the seed value.
+ *                          Ignored if \p seed is null.
+ *  \param reference        A tensor that specifies the layout of the output
+ *                          tensor.
+ *  \param outType          Type of the output tensor. One of \c float or
+ *                          \c half.
+ *  \param mean             The mean value of the distribution.
+ *  \param stdDev           The standard deviation of the distribution.
+ *  \param alpha            Defines the minimum and maximum values of the
+ *                          distribution.
+ *  \param prog             The program to add this operation to.
+ *  \param debugPrefix      A prefix string for debugging.
+ *
+ *  \returns A tensor with elements randomly set to either zero or the scaled
+ *           input value.
+ */
 poplar::Tensor truncatedNormal(poplar::Graph &graph, const poplar::Tensor *seed,
                                uint32_t seedModifier,
                                const poplar::Tensor &reference,
@@ -115,8 +171,15 @@ poplar::Tensor truncatedNormal(poplar::Graph &graph, const poplar::Tensor *seed,
                                poplar::program::Sequence &prog,
                                const std::string &debugPrefix = "");
 
-/// Sets the seed on all tiles given a single tensor of shape {2} and of type
-/// `UNSIGNED_INT`, and \p seedModifier.
+/** Sets the random number generator seed on all tiles.
+ *
+ *  \param graph            The graph to add this operation to.
+ *  \param masterSseed      A 64-bit integer to seed the random number
+ *                          on every tile.
+ *  \param seedModifier     Provides a further modification of the seed value.
+ *  \param prog             The program to add this operation to.
+ *  \param debugPrefix      A prefix string for debugging.
+ */
 void setSeed(poplar::Graph &graph, const poplar::Tensor &masterSeed,
              uint32_t seedModifier, poplar::program::Sequence &prog,
              const std::string &debugPrefix = "");
