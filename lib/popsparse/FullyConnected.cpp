graphcore/poplibs

commit 75a9c3bd4989270d76e7f479396d6cfcb26e37d6

Graph construction/minor fixes to get GradW pass working in sparse_fc_layer.
Summary: Ref T24154.

Reviewers: godfrey.da.costa, #poplar_team, #poplar_ip_review_-_any_oss_or_third-party_code_use_has_been_approved

Reviewed By: godfrey.da.costa, #poplar_team, #poplar_ip_review_-_any_oss_or_third-party_code_use_has_been_approved

Maniphest Tasks: T24154

Differential Revision: https://phabricator.sourcevertex.net/D31218

lib/popsparse/FullyConnected.cpp
@@ -971,8 +971,18 @@ static void compute(Graph &graph, const ComputeSet &cs,
                     const std::string &debugPrefix) {
   const std::string levelPrefix = debugPrefix + "/l" + std::to_string(level);
   const auto tile = getPartitionTile(hierarchy, plan, indices);
-  const std::array<std::size_t, 2> blockDimensions = {plan.grouping.x,
-                                                      plan.grouping.y};
+
+  // Though it complicates this function slightly, for clarity what we give
+  // to the onTileImpl is the dimensions of the block, as ordered for this
+  // pass. i.e. Forward = {x, y}, GradA = {y, x}, GradW = {x, y}.
+  const auto grouping = plan.grouping.asStdVector<std::size_t>();
+  std::vector<std::size_t> indicesOfBlockDims(plan.dimShuffleToFwd.begin() + 1,
+                                              plan.dimShuffleToFwd.begin() + 3);
+  std::sort(indicesOfBlockDims.begin(), indicesOfBlockDims.end());
+  std::array<std::size_t, 2> blockDimensions;
+  for (std::size_t i = 0; i < indicesOfBlockDims.size(); ++i) {
+    blockDimensions[i] = grouping[indicesOfBlockDims[i]];
+  }
   onTileImpl(graph, cs, tile, plan.method, zeroPartials, subGroupId,
              shape.asStdVector<std::size_t>(), metaInfo, weights, acts,
              partials, blockDimensions, levelPrefix);

lib/popsparse/FullyConnectedOnTile.cpp
@@ -42,6 +42,9 @@ getVertexClass(const OnTileMethod &method, const Type &inputType,
   case OnTileMethod::TransposeAMPBlock:
     return templateVertex("popsparse::SparseDenseMatMulBlockGradA", inputType,
                           partialsType, blockDimensions[0], blockDimensions[1]);
+  case OnTileMethod::GradWBlock:
+    return templateVertex("popsparse::SparseDenseMatMulBlockGradW", inputType,
+                          partialsType, blockDimensions[0], blockDimensions[1]);
   default:
     throw poplibs_error("Unhandled on-tile sparse fc method");
   }
@@ -170,6 +173,42 @@ void onTileImpl(Graph &graph, const ComputeSet &cs, unsigned tile,
     graph.setInitialValue(
         v["zeroInfo"],
         zeroPartials ? partials.numElements() / partialElemsPer32Bits : 0u);
+    break;
+  }
+  case OnTileMethod::GradWBlock: {
+    assert(partials.rank() == 6);
+    assert(weights.rank() == acts.rank());
+    assert(weights.rank() == 6);
+    assert(weights.dim(2) == acts.dim(1));
+    assert(weights.dim(5) == acts.dim(4));
+    const auto actsUngrouped = unfactorDims(acts, 3);
+    const auto weightsUngrouped = unfactorDims(weights, 3);
+    const auto subGroupIdToProcessTensor =
+        boost::get<Tensor>(subGroupIdToProcess);
+    assert(subGroupIdToProcessTensor.rank() <= 1);
+    const auto subGroupIdToProcessScalar =
+        subGroupIdToProcessTensor.flatten().squeeze({0});
+
+    const auto partialElemsPer32Bits = 4 / target.getTypeSize(partialsType);
+    assert(partials.numElements() % partialElemsPer32Bits == 0);
+    std::vector<std::size_t> squeezePartialsDims(5);
+    std::iota(squeezePartialsDims.begin(), squeezePartialsDims.end(), 0);
+    // Expect X as innermost dimension
+    graph.connect(v["qGrad"], weightsUngrouped.dimRoll(1, 2).flatten());
+    graph.connect(v["rGrad"], partials.squeeze(squeezePartialsDims));
+    graph.connect(v["metaInfo"], metaInfoBuckets.squeeze({0}));
+    // Expect Y as innermost dimension
+    graph.connect(v["s"], actsUngrouped.flatten());
+    graph.connect(v["subGroupIdToProcess"], subGroupIdToProcessScalar);
+    graph.setInitialValue(
+        v["zeroInfo"],
+        zeroPartials ? partials.numElements() / partialElemsPer32Bits : 0);
+    graph.setInitialValue(v["numZ"], weightsUngrouped.dim(2));
+    const auto inputElemsPer64Bits = 8 / target.getTypeSize(inputType);
+    const auto zStrideInQ = weightsUngrouped.dim(1) / inputElemsPer64Bits;
+    const auto zStrideInS = actsUngrouped.dim(2) / inputElemsPer64Bits;
+    graph.setInitialValue(v["zStrideInQ"], zStrideInQ);
+    graph.setInitialValue(v["zStrideInS"], zStrideInS);
     break;
   }
   default:

lib/popsparse/FullyConnectedPlan.cpp
@@ -727,7 +727,7 @@ addInitialComputeCostDenseDense(
             // divide between workers so there is some accounting for overheads.
             mulCycles = ceildiv(mulCycles, numYThisWorker.size());
             break;
-          case OnTileMethod::GradWAMPBlock:
+          case OnTileMethod::GradWBlock:
             mulCycles = sparseDenseBlockMultiplyGradW(
                 numBuckets, numBuckets, numSubGroupsPerBucket, numXThisWorker,
                 numZ, xGrouping, yGrouping, numYThisWorker, inputType == FLOAT,
@@ -1201,10 +1201,18 @@ static popsolver::Variable addMetaInfoElemsPerBucket(
     const auto subGroupElems =
         sizeof(BlockMetaInfo<MetaInfoType>::SubGroupEntry) /
         sizeof(MetaInfoType);
+    const auto gradWWorkerEntryElems =
+        options.doGradWPass
+            ? sizeof(BlockMetaInfo<MetaInfoType>::GradWWorkerEntry) /
+                  sizeof(MetaInfoType)
+            : 0;
+
     const auto numElemsPerfectlyUniform =
         xNonZeroGroups * (outputEntryElems + yNonZeroGroups);
     const unsigned elems =
-        (subGroupElems * numSubgroupsPerBucket +
+        ((subGroupElems +
+          target.getNumWorkerContexts() * gradWWorkerEntryElems) *
+             numSubgroupsPerBucket +
          std::ceil(numElemsPerfectlyUniform *
                    (1.0 + options.metaInfoBucketOversizeProportion)));
     return popsolver::DataType{roundUp(elems, atomSizeInMetaInfoElems)};
@@ -1514,7 +1522,7 @@ getCandidateMethods(const Target &target, const Type &inputType,
     };
     methods.emplace_back(Method{grouping, OnTileMethod::ForwardAMPBlock,
                                 OnTileMethod::TransposeAMPBlock,
-                                OnTileMethod::GradWAMPBlock});
+                                OnTileMethod::GradWBlock});
   }
   return methods;
 }
@@ -1640,8 +1648,8 @@ std::ostream &operator<<(std::ostream &os, const OnTileMethod &m) {
   case OnTileMethod::TransposeAMPBlock:
     os << "TransposeAMPBlock";
     break;
-  case OnTileMethod::GradWAMPBlock:
-    os << "GradWAMPBlock";
+  case OnTileMethod::GradWBlock:
+    os << "GradWBlock";
     break;
   default:
     throw poputil::poplibs_error("Unrecognised on-tile method");

lib/popsparse/FullyConnectedPlan.hpp
@@ -61,9 +61,8 @@ enum class OnTileMethod {
   // Codelet for GradA pass reusing forward pass meta-info for block-sparsity
   // utilising AMP.
   TransposeAMPBlock,
-  // Codelet for GradW pass reusing forward pass meta-info for block-sparsity
-  // utilising AMP.
-  GradWAMPBlock,
+  // Codelet for GradW pass reusing forward pass meta-info for block-sparsity.
+  GradWBlock,
 };
 
 std::ostream &operator<<(std::ostream &os, const OnTileMethod &m);

lib/popsparse/PerformanceEstimation.hpp
@@ -183,7 +183,7 @@ static inline std::uint64_t sparseDenseBlockMultiply(
 
   std::uint64_t supervisorOverhead = 40;
   std::uint64_t supervisorCyclesWithBucketsNotForPN =
-      32 * (numBuckets - numBucketsWithInfoForPN) * averageSubgroupsPerBucket;
+      33 * (numBuckets - numBucketsWithInfoForPN) * averageSubgroupsPerBucket;
 
   // assume uniform distribution of location. For the initial distribution this
   // can be set such that subgroup found if the very first.
@@ -191,7 +191,7 @@ static inline std::uint64_t sparseDenseBlockMultiply(
       static_cast<double>(averageSubgroupsPerBucket - 1) / 2;
 
   std::uint64_t supervisorCyclesWithBucketsForPN =
-      (avgSubgroupsForFirstMatch + 1) * 46 + 48;
+      (avgSubgroupsForFirstMatch + 1) * 46 + 56;
 
   std::uint64_t totalSupervisorCycles =
       supervisorOverhead + supervisorCyclesWithBucketsNotForPN +

lib/popsparse/SparsePartitionerImpl.cpp
@@ -1195,7 +1195,7 @@ bucketsImplInternal(const PNBucket &bucket, const std::vector<T> &nzValues,
         std::vector<MetaInfo<std::size_t>::OutputEntry> outputEntries(numRows);
 
         for (const auto &w : gradWEntries) {
-          group.push_back(w.sparseOffset);
+          group.push_back(w.sparseOffset * blockSize);
           std::size_t offset =
               w.sparseOffset - w.metaInfoOffsetToOffsetsYInSFirst +
               (workersRemaining *

lib/popsparse/codelets/SparseDenseMatMulBlock.cpp
@@ -118,9 +118,12 @@ public:
 
         if (subGroupEntry->id == subGroupIdToProcess) {
           const auto *r = rBucketIter;
+          const auto *gradWWorkerEntries =
+              reinterpret_cast<const MetaInfo::GradWWorkerEntry *>(
+                  subGroupEntry + 1);
           const auto *outputEntry =
-              reinterpret_cast<const MetaInfo::OutputEntry *>(subGroupEntry +
-                                                              1);
+              reinterpret_cast<const MetaInfo::OutputEntry *>(
+                  gradWWorkerEntries + subGroupEntry->numGradWWorkers);
           for (std::size_t xBlock = 0; xBlock < subGroupEntry->numXm1 + 1;
                ++xBlock) {
             const auto *inputEntry =

lib/popsparse/codelets/SparseDenseMatMulBlockGradA.cpp
@@ -122,9 +122,12 @@ public:
 
         if (subGroupEntry->id == subGroupIdToProcess) {
           const auto *r = rBucketIter;
+          const auto *gradWWorkerEntries =
+              reinterpret_cast<const MetaInfo::GradWWorkerEntry *>(
+                  subGroupEntry + 1);
           const auto *outputEntry =
-              reinterpret_cast<const MetaInfo::OutputEntry *>(subGroupEntry +
-                                                              1);
+              reinterpret_cast<const MetaInfo::OutputEntry *>(
+                  gradWWorkerEntries + subGroupEntry->numGradWWorkers);
           for (std::size_t xBlock = 0; xBlock < subGroupEntry->numXm1 + 1;
                ++xBlock) {
             const auto *inputEntry =

commit 92c44c67c6cb3bbc778da557a0a0239f23a03d1c

Remove incorrect assertion check given blocks are now supported
Summary:
Fixes tests in debug which started failing with commit
75a9c3bd4989270d76e7f479396d6cfcb26e37d6

Reviewers: tomm, #poplar_team, #poplar_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, samk

Reviewed By: #poplar_team, #poplar_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, samk

Maniphest Tasks: T24154

Differential Revision: https://phabricator.sourcevertex.net/D31264

lib/popsparse/FullyConnected.cpp
@@ -1618,7 +1618,6 @@ static Tensor fullyConnectedSparseGradWImpl(
   // At the top level for now, before doing anything else, enforce and
   // introduce grouping into the given tensors.
   if (level == 0) {
-    assert(plan.grouping.groups * plan.grouping.x * plan.grouping.z == 1);
     // Accumulated dimension is always inner-most
     std::vector<std::size_t> weightGrouping = {
         plan.grouping.groups, plan.grouping.x, plan.grouping.y};