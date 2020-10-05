c4713465fe927d61fca8dcdd99fde626b238906c
Claudio Potenza <claudiop@graphcore.ai>
2020-10-05 09:06:16 +0100

    Fix copyright comment
    Reviewers: #poplar_team, #poplar_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, samk
    
    Reviewed By: #poplar_team, #poplar_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, samk
    
    Maniphest Tasks: T24229
    
    Differential Revision: https://phabricator.sourcevertex.net/D33444

--- lib/popops/codelets/elemwiseBinaryOps.hpp
+++ lib/popops/codelets/elemwiseBinaryOps.hpp
@@ -1,4 +1,4 @@
-/// Copyright (c) 2020 Graphcore Ltd. All rights reserved.
+// Copyright (c) 2020 Graphcore Ltd. All rights reserved.
 
 #ifndef POPLIBS_LIB_POPOPS_CODELETS_ELEMWISEBINARYOPS_HPP_
 #define POPLIBS_LIB_POPOPS_CODELETS_ELEMWISEBINARYOPS_HPP_
