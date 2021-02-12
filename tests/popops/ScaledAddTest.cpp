cda4a858894c3703892902992175d426f084adba
Mark Fowden <markf@graphcore.ai>
2021-02-12 10:58:07 +0000

    Add test for using INT input with scaledAddTo(a, X, b, Y)
    Summary:
    - Also rename ScaledAddEquivalentTest -> ScaledAddTest to reflect its more general new role.
    
    Fixes T34122
    
    Test Plan: CI. New test passes with diff and fails without diff.
    
    Reviewers: samk, timp, #poplar_team, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved
    
    Reviewed By: samk, #poplar_team, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved
    
    Maniphest Tasks: T34122
    
    Differential Revision: https://phabricator.sourcevertex.net/D40486

--- tests/popops/ScaledAddTest.cpp
+++ tests/popops/ScaledAddTest.cpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2020 Graphcore Ltd. All rights reserved.
-#define BOOST_TEST_MODULE ScaledAddEquivalenceTest
+#define BOOST_TEST_MODULE ScaledAddTest
 
 #include <boost/test/unit_test.hpp>
 #include <poplar/CSRFunctions.hpp>
@@ -12,7 +12,43 @@
 #include <vector>
 using namespace poplibs_support;
 
-BOOST_AUTO_TEST_CASE(ScaledAddEquivalenceTestCheckEqual) {
+BOOST_AUTO_TEST_CASE(ScaledAddTestCheckaXPlusbYInt) {
+  auto device = createTestDevice(TEST_TARGET);
+  const auto target = device.getTarget();
+  poplar::Graph g(target);
+  popops::addCodelets(g);
+
+  auto X = g.addVariable(poplar::INT, {4}, "X");
+  auto Y = g.addVariable(poplar::INT, {4}, "Y");
+  g.setTileMapping(X, 0);
+  g.setTileMapping(Y, 0);
+  g.createHostWrite("X_write", X);
+  g.createHostWrite("Y_write", Y);
+  g.createHostRead("X_read", X);
+
+  poplar::program::Sequence prog;
+  poplar::setStochasticRounding(g, prog, false);
+  popops::scaledAddTo(g, X, 2, Y, 1, prog, "scaledAdd");
+
+  const std::vector<int> rawX = {1, 2, 3, 4};
+  const std::vector<int> rawY = {1, 2, 3, 4};
+  std::vector<int> rawOut(4);
+  const std::vector<int> rawTarget = {3, 6, 9, 12};
+
+  poplar::Engine e(g, prog);
+  device.bind([&](const poplar::Device &d) {
+    e.load(d);
+    e.writeTensor("X_write", rawX.data(), rawX.data() + rawX.size());
+    e.writeTensor("Y_write", rawY.data(), rawY.data() + rawY.size());
+    e.run(0);
+    e.readTensor("X_read", rawOut.data(), rawOut.data() + rawOut.size());
+  });
+
+  BOOST_CHECK_EQUAL_COLLECTIONS(rawOut.begin(), rawOut.end(), rawTarget.begin(),
+                                rawTarget.end());
+}
+
+BOOST_AUTO_TEST_CASE(ScaledAddTestCheckEquivalenceHalf) {
   auto device = createTestDevice(TEST_TARGET);
   const auto target = device.getTarget();
   poplar::Graph g(target);
