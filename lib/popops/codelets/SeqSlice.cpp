graphcore/poplibs

commit 3a6979c19734b3a4c9df151eb56351ec1fdc7a49

add sequenceSlice
Summary:
This copies variably offset and sized slices from one tensor to another.

This initial version targets efficient transformer (attention) optimisations
but it should be more generally useful.

Ref T28274

Test Plan: tests included; further testing within frameworks required

Reviewers: #poplar_team, #poplar_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, tomm

Reviewed By: #poplar_team, #poplar_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, tomm

Maniphest Tasks: T28274

Differential Revision: https://phabricator.sourcevertex.net/D39391

lib/popops/codelets/SeqSlice.cpp
@@ -1,0 +1,182 @@
+// Copyright (c) 2021 Graphcore Ltd. All rights reserved.
+#include "poplibs_support/ExternalCodelet.hpp"
+#include "poplibs_support/TileConstants.hpp"
+#include <cassert>
+#include <cmath>
+#include <poplar/HalfFloat.hpp>
+#include <poplar/Vertex.hpp>
+#include <print.h>
+#include <type_traits>
+
+#ifdef __IPU__
+#include "colossus/tileimplconsts.h"
+#endif
+
+using namespace poplar;
+
+static constexpr auto ONE_PTR = poplar::VectorLayout::ONE_PTR;
+
+namespace popops {
+
+#ifdef __IPU__
+template <typename Type>
+class [[poplar::constraint("elem(*src) != elem(*dst)")]] SequenceSliceCopy
+    : public Vertex {
+  Input<Vector<Type, ONE_PTR>> src;
+  Input<Vector<Type, ONE_PTR>> dst;
+  unsigned short nAtoms; // must be non-zero
+
+public:
+  IS_EXTERNAL_CODELET(false);
+  bool compute() {
+    const Type *srcPtr = &src[0];
+    Type *dstPtr = &dst[0];
+    unsigned n = nAtoms;
+
+    if (std::is_same<Type, half>::value) {
+      // Minimise sub-word writes
+      if (uintptr_t(dstPtr) & 0x2) {
+        // initial misaligned output element
+        auto x = *srcPtr++;
+        *dstPtr++ = x;
+        --n;
+        if (n == 0)
+          return true;
+      }
+      unsigned short loopCount = n / 2;
+      n &= 1; // There may be an odd element at the end
+      auto *dstAlignedPtr = (half2 *)dstPtr;
+      half a = *srcPtr++;
+      half b = *srcPtr++;
+      for (unsigned i = 0; i != loopCount; ++i) {
+        half2 word{a, b};
+        a = *srcPtr++;
+        b = *srcPtr++;
+        *dstAlignedPtr++ = word;
+      }
+      if (n) {
+        (*dstAlignedPtr)[0] = a;
+      }
+    } else {
+      auto prev = *srcPtr++;
+      unsigned short loopCount = n - 1;
+      for (unsigned i = 0; i != loopCount; ++i) {
+        *dstPtr++ = prev;
+        prev = *srcPtr++;
+      }
+      *dstPtr = prev;
+    }
+    return true;
+  }
+};
+template class SequenceSliceCopy<half>;
+template class SequenceSliceCopy<float>;
+#endif
+
+// Dynamically update slices from a source to a destination tensor.
+// Both src and dst offsets plus the number of elements sliced are configured at
+// runtime.
+// The vertex can be connected to a subset of a tensor, with slicing clipped by
+// both src and dst boundaries.
+// Update slices from multiple offsets \a baseT to \a subT.
+// Each slice has its own size.
+// srcOffsetT and dstOffsetT are absolute
+// This variant takes a 2d input and calculates the offsets given the start
+// address of the base and sub Tensors. the updates are added to the core tensor
+// indices that are not within the range of [baseOffset,
+// baseOffset + numBaseElements) are ignored.
+template <typename Type>
+class [[poplar::constraint("elem(*srcT) != elem(*dstT)")]] SequenceSlice
+    : public SupervisorVertex {
+  // TODO: make this a supervisor vertex so multiple offsets can be handled
+  // by multiple workers. Can't split into multple vertices as they would all
+  // have the same dstT
+public:
+  SequenceSlice();
+
+  IS_EXTERNAL_CODELET(false);
+
+  Input<Vector<Type, ONE_PTR, 4>> srcT;
+  InOut<Vector<Type, ONE_PTR, 4>> dstT;
+  Input<Vector<unsigned>> srcOffsetT;          // in \a baseT
+  Input<Vector<unsigned, ONE_PTR>> dstOffsetT; // in \a subT
+  Input<Vector<unsigned, ONE_PTR>> nElementsT;
+  Output<Vector<unsigned, ONE_PTR>> tmpT; // size should be 3*srcOffsetT.size()
+  const unsigned srcFirst;
+  const unsigned dstFirst;         // in the slice dimension
+  const unsigned numSrcElements;   // in the slice dimension
+  const unsigned numDstElements;   // in the slice dimension
+  const unsigned short regionSize; // stride between elements
+
+#ifdef __IPU__
+  __attribute__((target("supervisor")))
+#endif
+  bool
+  compute() {
+    for (unsigned o = 0; o != srcOffsetT.size(); ++o) {
+      auto srcOffset = srcOffsetT[o];
+      auto dstOffset = dstOffsetT[o];
+      int nE = nElementsT[o];
+      if (nE == 0)
+        continue;
+      // Clip the starts and lengths so neither src nor dst accesses will
+      // overflow.
+      auto startSkip = 0u;
+      if (srcFirst > srcOffset)
+        startSkip = srcFirst - srcOffset;
+      if (dstOffset + startSkip < dstFirst)
+        startSkip = dstFirst - dstOffset;
+      nE -= startSkip;
+      const Type *srcPtr =
+          &srcT[(srcOffset + startSkip - srcFirst) * regionSize];
+      Type *dstPtr = &dstT[(dstOffset + startSkip - dstFirst) * regionSize];
+
+      if (srcOffset + nE > numSrcElements)
+        nE -= srcOffset + nE - numSrcElements;
+      if (dstOffset + nE > numDstElements)
+        nE -= dstOffset + nE - numDstElements;
+
+      if (nE == 0)
+        continue;
+      auto nAtoms = nE * regionSize;
+
+#ifdef __IPU__
+      // Invoke a worker for each slice
+      tmpT[3 * o + 0] = uintptr_t(srcPtr);
+      tmpT[3 * o + 1] = uintptr_t(dstPtr);
+      tmpT[3 * o + 2] = nAtoms;
+
+      unsigned dummy;
+      if (std::is_same<Type, half>::value) {
+        asm volatile(
+            " setzi %0, __runCodelet_popops__SequenceSliceCopy___half\n"
+            " run %0, %1, 0\n"
+            : "+r"(dummy) // actually scratch rather than output
+            : "r"(uintptr_t(&tmpT[3 * o] - TMEM_REGION0_BASE_ADDR / 4)));
+      } else {
+        asm volatile(
+            " setzi %0, __runCodelet_popops__SequenceSliceCopy___float\n"
+            " run %0, %1, 0\n"
+            : "+r"(dummy) // actually scratch rather than output
+            : "r"(uintptr_t(&tmpT[3 * o] - TMEM_REGION0_BASE_ADDR / 4)));
+      }
+
+#else
+      for (unsigned i = 0; i != nAtoms; ++i) {
+        *dstPtr++ = *srcPtr++;
+      }
+#endif
+    }
+#ifdef __IPU__
+    asm volatile(" sync %0\n" ::"i"(TEXCH_SYNCZONE_LOCAL));
+#endif
+    return true;
+  }
+};
+
+template class SequenceSlice<float>;
+template class SequenceSlice<half>;
+template class SequenceSlice<int>;
+template class SequenceSlice<unsigned>;
+
+} // namespace popops

include/popops/SequenceSlice.hpp
@@ -1,0 +1,53 @@
+// Copyright (c) 2021 Graphcore Ltd. All rights reserved.
+/** \file
+ *
+ * Support for dynamic slices.
+ *
+ */
+
+#ifndef popops_SequenceSlice_hpp
+#define popops_SequenceSlice_hpp
+#include <poplar/Graph.hpp>
+#include <poplar/Program.hpp>
+#include <poputil/DebugInfo.hpp>
+#include <string>
+#include <vector>
+
+namespace poplar {
+class Tensor;
+}
+
+namespace popops {
+
+/** Slice a 2d tensor based on offsets specified by a tensor.
+ *
+ * Typically this is used to copy subsequences of one tensor to another.
+ *  The outermost dimension is sliced;
+ *    tOut[tOutOffset:tOutOffset+tN][...] = tIn[tInOffset:tInOffset+tN][...]
+ *  for each entry in tN/tInOffset/tOutOffset; entries after the first tN==0 may
+ *  be ignored.
+ *  Unreferenced elements of tOut are zeroed if zeroUnused is set. The same
+ *  output element should not be written by multiple inputs.
+ *
+ *  \p tIn and \p tOut must have rank >=2. The outer dimension is sliced; the
+ *  product of the inner dimensions must match.
+ *  \p tInOffset, \p tOutOffset and \p tN must be 1d and the same size.
+ *  \param graph       The Poplar graph.
+ *  \param tIn         The source tensor.
+ *  \param tOut        The destination tensor.
+ *  \param tN          The number of elements to copy.
+ *  \param tInOffset   First element read from \p tIn.
+ *  \param tOutOffset  First element written in \p tOut.
+ *  \param zeroUnused	 Whether to zero unreferenced \p tOut elements.
+ *  \param prog        The program to be extended.
+ *  \param debugContext Optional debug information.
+ **/
+void sequenceSlice(poplar::Graph &graph, const poplar::Tensor &tIn,
+                   const poplar::Tensor &tOut, const poplar::Tensor &tN,
+                   const poplar::Tensor &tInOffset,
+                   const poplar::Tensor &tOutOffset, bool zeroUnused,
+                   poplar::program::Sequence &prog,
+                   const poplar::DebugContext &debugContext = {});
+
+} // end namespace popops
+#endif // popops_DynamicSlice_hpp

lib/popops/CMakeLists.txt
@@ -34,6 +34,7 @@ add_library(popops SHARED
   ScaledAdd.cpp
   Scatter.cpp
   SelectScalarFromRows.cpp
+  SequenceSlice.cpp
   Sort.cpp
   SparseUtils.cpp
   UpdateScalarInRows.cpp
@@ -58,6 +59,7 @@ add_library(popops SHARED
   ${CMAKE_SOURCE_DIR}/include/popops/Operation.hpp
   ${CMAKE_SOURCE_DIR}/include/popops/Pad.hpp
   ${CMAKE_SOURCE_DIR}/include/popops/Rearrange.hpp
+  ${CMAKE_SOURCE_DIR}/include/popops/SequenceSlice.hpp
   ${CMAKE_SOURCE_DIR}/include/popops/Reduce.hpp
   ${CMAKE_SOURCE_DIR}/include/popops/Zero.hpp
   ${CMAKE_SOURCE_DIR}/include/poplibs_support/popopsPerformanceEstimation.hpp
@@ -193,6 +195,7 @@ add_gp_library(
     ${CMAKE_CURRENT_SOURCE_DIR}/codelets/SelectFromInterval.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/codelets/SelectFromIntervals.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/codelets/SelectFromRowsInColumns.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/codelets/SeqSlice.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/codelets/Transpose.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/codelets/Transpose2d.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/codelets/TransposeSupervisor.cpp

lib/popops/DynamicSlice.cpp
@@ -1185,13 +1185,14 @@ static Tensor createSliceableTensorGivenOrder(
     const auto &target = graph.getTarget();
     const auto bytesPerElement = target.getTypeSize(type);
     const auto granularity = target.getAtomicStoreGranularity();
+
     if (granularity > bytesPerElement &&
         minGrainSize * bytesPerElement >= granularity) {
       const auto elemPerAtom = granularity / bytesPerElement;
       const auto unrounded = unslicedElemsPerSplit;
       auto remainder = unslicedElemsPerSplit % elemPerAtom;
       if (remainder != 0) {
-        unslicedElemsPerSplit += granularity - remainder;
+        unslicedElemsPerSplit += elemPerAtom - remainder;
         logging::popops::trace("createSliceableTensor rounded {} to {}",
                                unrounded, unslicedElemsPerSplit);
       }

lib/popops/SequenceSlice.cpp
@@ -1,0 +1,332 @@
+// Copyright (c) 2021 Graphcore Ltd. All rights reserved.
+#include "popops/SequenceSlice.hpp"
+#include "poplar/Interval.hpp"
+#include "poplar/Program.hpp"
+#include "poplar/Tensor.hpp"
+#include "poplibs_support/Algorithm.hpp"
+#include "poplibs_support/ContiguousRegionsByTile.hpp"
+#include "poplibs_support/logging.hpp"
+#include "popops/DynamicSlice.hpp"
+#include "popops/Zero.hpp"
+#include "poputil/DebugInfo.hpp"
+#include "poputil/OptionParsing.hpp"
+#include "poputil/TileMapping.hpp"
+#include "poputil/Util.hpp"
+#include "poputil/VarStructure.hpp"
+#include "poputil/VertexTemplates.hpp"
+#include "poputil/exceptions.hpp"
+
+#include <boost/optional.hpp>
+
+#include <cassert>
+#include <numeric>
+#include <type_traits>
+
+using namespace poplar;
+using namespace poplar::program;
+using namespace poputil;
+using namespace poplibs_support;
+using namespace poplibs;
+
+namespace {
+struct SliceOptions {
+  SliceOptions() = default;
+
+  // The target maximum temporary memory usage for the operation. This
+  // may not be satisfiable.
+  double availableMemoryProportion = 0.6;
+};
+
+static SliceOptions parseSliceOptions(const OptionFlags &optionFlags) {
+  SliceOptions options;
+
+  using poplibs::OptionHandler;
+  using poplibs::OptionSpec;
+
+  /*
+   * Any changes to spec must be reflected in the documentation comment in
+   * the header.
+   */
+  const OptionSpec spec{
+      {"availableMemoryProportion",
+       OptionHandler::createWithDouble(options.availableMemoryProportion)}};
+
+  for (const auto &entry : optionFlags) {
+    spec.parse(entry.first, entry.second);
+  }
+  return options;
+}
+
+} // unnamed namespace
+
+namespace popops {
+
+// Generate sequenceSlice vertices on the specified tile. These are
+// supervisor vertices.
+static void generateSequenceSliceVertexOnTile(
+    Graph &graph, const ComputeSet &cs, unsigned tile, const Tensor &tSrc,
+    const Tensor &tDst, const Tensor &tSrcOffset, const Tensor &tDstOffset,
+    const Tensor &tN, const std::string &vertexName,
+    boost::optional<unsigned> baseOffset, const DebugNameAndId &dnai) {
+  assert(tSrc.rank() >= 2);
+  assert(tDst.rank() >= 2);
+  auto tSrc2d = tSrc.flatten(1, tSrc.rank());
+  auto tDst2d = tDst.flatten(1, tDst.rank());
+  assert(tSrc2d.dim(1) == tDst2d.dim(1));
+  assert(tSrcOffset.rank() == 1);
+  assert(tDstOffset.rank() == 1);
+  assert(tN.rank() == 1);
+  const auto dType = tSrc.elementType();
+  assert(dType == tDst.elementType());
+  const auto &target = graph.getTarget();
+
+  auto bytesPerElement = tSrc.dim(1) * target.getTypeSize(dType);
+  if (bytesPerElement % graph.getTarget().getAtomicStoreGranularity() != 0)
+    logging::popops::warn("sequenceSlice with {} bytesPerElement on tile {}",
+                          bytesPerElement, tile);
+  Tensor tTmp = graph.addVariable(UNSIGNED_INT, {3 * tN.numElements()},
+                                  {dnai, "temp copy info"});
+  auto v = graph.addVertex(cs, templateVertex(vertexName, dType),
+                           {{"srcT", tSrc2d.flatten()},
+                            {"dstT", tDst2d.flatten()},
+                            {"srcOffsetT", tSrcOffset},
+                            {"dstOffsetT", tDstOffset},
+                            {"nElementsT", tN},
+                            {"tmpT", tTmp}});
+  graph.setInitialValue(v["srcFirst"], 0);
+  graph.setInitialValue(v["dstFirst"], 0);
+  graph.setInitialValue(v["numSrcElements"], tSrc2d.dim(0));
+  graph.setInitialValue(v["numDstElements"], tDst2d.dim(0));
+  graph.setInitialValue(v["regionSize"], tSrc2d.dim(1));
+
+  graph.setTileMapping(v, tile);
+  graph.setTileMapping(tTmp, tile);
+}
+
+// Generate vertices for sequenceSlice.
+// This is based on generateMultiSliceVertices.
+// Enhancements may be needed to split the slicing dimension across tiles.
+static void generateSequenceSliceVertices(
+    Graph &graph, Sequence &prog, Tensor tSrc, Tensor tDst, Tensor tN,
+    Tensor tSrcOffsets, Tensor tDstOffsets, boost::optional<unsigned> srcOffset,
+    const OptionFlags &optionFlags, const DebugNameAndId &dnai) {
+  const auto options = parseSliceOptions(optionFlags);
+
+  auto cs = graph.addComputeSet({dnai, "SequenceSlice"});
+
+  assert(tSrc.rank() == 2);
+  assert(tDst.rank() == 2);
+  assert(tSrc.dim(1) == tDst.dim(1));
+  assert(tSrc.elementType() == tDst.elementType());
+  assert(tSrcOffsets.rank() == 1);
+  assert(tDstOffsets.shape() == tDstOffsets.shape());
+  assert(tN.shape() == tSrcOffsets.shape());
+
+  const auto &target = graph.getTarget();
+  const auto numTiles = target.getNumTiles();
+  const auto type = tDst.elementType();
+
+  // We will map vertices based on whichever of the input and output is
+  // the biggest in the hope of minimising exchange. Exchanging the destination
+  // is more expensive as it's an InOut.
+  // TODO: When zeroing unused elements it may be better to do specialise the
+  // vertices to include the zeroing. This would give them an Output rather than
+  // InOut and remove the need for a precopy.
+  bool baseIsSrc = tSrc.numElements() >= tDst.numElements() / 2;
+  auto &base = baseIsSrc ? tSrc : tDst;
+  logging::popops::debug("baseIsSrc {} because {} >= {}", baseIsSrc,
+                         tSrc.numElements(), tDst.numElements());
+  logging::popops::debug("base shape {}", base.shape());
+
+  // Build vertices assuming all sliced dimensions have the same mapping as
+  // the first one and the non-sliced dimension is contiguous. If this is
+  // not honoured gathering internal exchange/copies will be generated
+  auto baseSlice0 = base.slice(0, 1, 0);
+  auto mappingSlice0 = graph.getTileMapping(baseSlice0);
+
+  // Check the spread of the base tensor over tiles against an available
+  // memory proportion to determine if we will use excessive temporary memory
+  // for the base tensor and result or not.
+  //
+  boost::optional<Tensor> originalDst;
+  {
+    const auto maxUnslicedElemsPerTile = [&] {
+      std::size_t maxElems = 0;
+      for (unsigned tile = 0; tile < mappingSlice0.size(); ++tile) {
+        const auto elemsThisTile = std::accumulate(
+            mappingSlice0[tile].begin(), mappingSlice0[tile].end(),
+            std::size_t(0),
+            [](std::size_t t, const Interval &i) { return t + i.size(); });
+        if (elemsThisTile != 0)
+          logging::popops::trace("tile {} has elements: {}", tile,
+                                 elemsThisTile);
+        maxElems = std::max(maxElems, elemsThisTile);
+      }
+      return maxElems;
+    }();
+
+    // Report balance.
+    if (logging::shouldLog(logging::Module::popops, logging::Level::Info)) {
+      const auto maxTUnslicedElemsPerTile = [&] {
+        std::size_t maxElems = 0;
+        auto mappingT = graph.getTileMapping(base.transpose().slice(0, 1, 0));
+        for (unsigned tile = 0; tile < mappingT.size(); ++tile) {
+          const auto elemsThisTile = std::accumulate(
+              mappingT[tile].begin(), mappingT[tile].end(), std::size_t(0),
+              [](std::size_t t, const Interval &i) { return t + i.size(); });
+          if (elemsThisTile != 0)
+            logging::popops::trace("mappingT {} has elements: {}", tile,
+                                   elemsThisTile);
+          maxElems = std::max(maxElems, elemsThisTile);
+        }
+        return maxElems;
+      }();
+      logging::popops::info("seqSlice: mappingT has max: {}",
+                            maxTUnslicedElemsPerTile);
+    }
+
+    const auto balancedUnslicedElemsPerTile = ceildiv(base.dim(1), numTiles);
+
+    // If we are already as well balanced as we can be then we can't do
+    // anything about this without a planned multi-stage or even a serialized
+    // slice which we won't try for the timebeing.
+    logging::popops::info("sequenceSlice balance check {} > {}",
+                          maxUnslicedElemsPerTile,
+                          balancedUnslicedElemsPerTile);
+    if (maxUnslicedElemsPerTile > balancedUnslicedElemsPerTile) {
+      const auto bytesPerElem = target.getTypeSize(type);
+      const auto maxBaseBytesPerTile =
+          maxUnslicedElemsPerTile * base.dim(0) * bytesPerElem;
+      const unsigned availableBytesPerTile = std::ceil(
+          target.getBytesPerTile() * options.availableMemoryProportion);
+
+      // We first check if having to rearrange the base slice would cause us to
+      // exceed our temporary memory limit to avoid introspecting again if we
+      // don't need to.
+      logging::popops::info("sequenceSlice bytesPerTile {} vs {}",
+                            maxBaseBytesPerTile, availableBytesPerTile);
+      bool alwaysRearrange = true;
+      if (alwaysRearrange)
+        logging::popops::info("sequenceSlice forcing rearrangement");
+      if (alwaysRearrange || maxBaseBytesPerTile > availableBytesPerTile) {
+        // Do a cheap but imprecise approximation of whether or not all the
+        // slices of the base tensor have the same tile mapping as the first by
+        // checking just one other slice, chosen to be the last heuristically.
+        //
+        // If the mapping of all slices does not match we will have to
+        // rearrange and hence we will know that our temporary memory budget
+        // will be exceeded by rearranging this base tensor (we already checked
+        // the max size on a tile above).
+        auto n = base.dim(0);
+        auto baseSliceN = base.slice(n - 1, n, 0);
+        auto mappingSliceN = graph.getTileMapping(baseSliceN);
+
+        if (mappingSlice0 != mappingSliceN) {
+          // Rearrange the base tensor to be better spread
+          if (!baseIsSrc)
+            originalDst = base;
+          auto minGrainSize = type == HALF ? 2 : 1;
+          Tensor newBase =
+              createSliceableTensor(graph, type, base.shape(), {0}, {1},
+                                    minGrainSize, {dnai, "baseRearranged"});
+          logging::popops::info("sequenceSlice rearranging (shape {})",
+                                newBase.shape());
+          prog.add(Copy(base, newBase, false, {dnai}));
+          baseSlice0 = newBase.slice(0, 1, 0);
+          mappingSlice0 = graph.getTileMapping(baseSlice0);
+          if (baseIsSrc)
+            tSrc = newBase;
+          else
+            tDst = newBase;
+        }
+      }
+    }
+  }
+
+  // instantiate vertices following the mapping of the base's first slice
+  std::size_t nActiveTiles{0}, maxTcrs{0};
+  for (unsigned tile = 0; tile != numTiles; ++tile) {
+    const auto tileContiguousRegions =
+        graph.getSortedContiguousRegions(baseSlice0, mappingSlice0[tile]);
+    if (tileContiguousRegions.size() == 0) {
+      // do nothing on this tile
+      continue;
+    }
+    ++nActiveTiles;
+    maxTcrs = std::max(maxTcrs, tileContiguousRegions.size());
+
+    Tensor tileSrc = concat(tSrc.slices(tileContiguousRegions, 1), 1);
+    Tensor tileDst = concat(tDst.slices(tileContiguousRegions, 1), 1);
+
+    std::string vertexName{"popops::SequenceSlice"};
+    generateSequenceSliceVertexOnTile(graph, cs, tile, tileSrc, tileDst,
+                                      tSrcOffsets, tDstOffsets, tN, vertexName,
+                                      srcOffset, {dnai});
+  }
+  logging::popops::info(
+      "sequenceSlice added for up to {} contiguous regions on {} tiles",
+      maxTcrs, nActiveTiles);
+
+  prog.add(Execute(cs, {dnai}));
+
+  // If we rearranged the output copy back to the original
+  if (originalDst) {
+    prog.add(Copy(tDst, *originalDst, false, {dnai}));
+  }
+}
+
+void sequenceSlice(poplar::Graph &graph, const poplar::Tensor &tSrc,
+                   const poplar::Tensor &tDst, const poplar::Tensor &tN,
+                   const poplar::Tensor &tSrcOffset,
+                   const poplar::Tensor &tDstOffset, bool zeroUnused,
+                   poplar::program::Sequence &prog,
+                   const poplar::DebugContext &debugContext) {
+  poputil::PoplibsOpDebugInfo di(
+      debugContext, DI_ARGS(tSrc, tDst, tN, tSrcOffset, tDstOffset));
+
+  logging::popops::info("sequenceSlice {} -> {}, name={}, {} slices, zero={}",
+                        tSrc.shape(), tDst.shape(), debugContext.getPathName(),
+                        tN.numElements(), zeroUnused);
+
+  if (tN.rank() != 1)
+    throw graph_connection_error("sequenceSlice: tN must be a 1d tensor");
+  if (tSrcOffset.shape() != tN.shape())
+    throw graph_connection_error("sequenceSlice: tSrcOffset must be a 1d "
+                                 "tensor with the same length as tN");
+  if (tDstOffset.shape() != tN.shape())
+    throw graph_connection_error("sequenceSlice: tDstOffset must be a 1d "
+                                 "tensor with the same length as tN");
+  if (tSrc.rank() < 2)
+    throw graph_connection_error(
+        "sequenceSlice: tSrc must have a rank of at least 2");
+  if (tDst.rank() < 2)
+    throw graph_connection_error(
+        "sequenceSlice: tDst must have a rank of at least 2");
+  if (tDst.containsAliases() || tDst.containsConstant())
+    throw graph_connection_error(
+        "sequenceSlice: tDst must not contain aliases or constants");
+  if (tSrc.dim(0) == 0 || tDst.dim(0) == 0) {
+    // No slicing to be done.
+    if (zeroUnused)
+      popops::zero(graph, tDst, prog, {debugContext, "AlwaysZero"});
+    return;
+  }
+  if (tSrc.numElements() / tSrc.dim(0) != tDst.numElements() / tDst.dim(0))
+    throw graph_connection_error(
+        "sequenceSlice: tSrc and tDst (inner) dimensions " +
+        tSrc.shapeToString() + " / " + tDst.shapeToString() +
+        " are incompatible");
+  // Zero the output if required.
+  // If we encounter signficiant output rearrangement costs this could be
+  // pushed into a different vertex with an Output rather than InOut tOut.
+  if (zeroUnused)
+    popops::zero(graph, tDst, prog, {di, "InitialZero"});
+
+  // Expand to get an index for each element in the output
+  auto tSrc2d = tSrc.flatten(1, tSrc.rank());
+  auto tDst2d = tDst.flatten(1, tDst.rank());
+  generateSequenceSliceVertices(graph, prog, tSrc2d, tDst2d, tN, tSrcOffset,
+                                tDstOffset, {}, {}, {di, "Slicing"});
+}
+
+} // end namespace popops

lib/popops/popopsCycleEstimators.cpp
@@ -2384,6 +2384,34 @@ MAKE_PERF_ESTIMATOR_NAME(MultiUpdateAdd)(const VertexIntrospector &vertex,
                        flopsPerBinaryOpElement(BinaryOpType::MULTIPLY))};
 }
 
+VertexPerfEstimate MAKE_PERF_ESTIMATOR_NAME(SequenceSlice)(
+    const VertexIntrospector &vertex, const Target &target, const Type &type) {
+  CODELET_FIELD(srcOffsetT);
+  CODELET_FIELD(dstT);
+  CODELET_FIELD(srcT);
+  CODELET_SCALAR_VAL(regionSize, unsigned short);
+  if (srcOffsetT.size() == 0)
+    return 40;
+
+  const auto tN = vertex.getFieldInfo("nElementsT").size();
+  std::uint64_t cycles = 60; // call overhead
+  // We don't know how much was requested so assume all of one of the buffers is
+  // copied. This may be quite pessimistic.
+  const unsigned bytesPerAtom = target.getTypeSize(type);
+  unsigned nElementsCopied = std::min(dstT.size(), srcT.size());
+  unsigned nAtomsCopies = nElementsCopied * regionSize;
+  // Assume 32bits/2 cycles for worker c++ codelet.
+  // Assume all subsequences are the same size. This may be optimistic.
+  auto oneWorkerCycles = (nAtomsCopies * bytesPerAtom / 4 * 2 + tN - 1) / tN;
+  // Some workers may be idle.
+  unsigned numWorkerContexts = target.getNumWorkerContexts();
+  auto numWorkerRuns = (tN + numWorkerContexts - 1) / numWorkerContexts;
+  cycles += oneWorkerCycles * numWorkerContexts * numWorkerRuns;
+  // Outerloop overhead of 60 cycles for supervisor c++ codelet.
+  cycles += tN * 60;
+  return cycles;
+}
+
 VertexPerfEstimate
 MAKE_PERF_ESTIMATOR_NAME(CircBufIncrIndex)(const VertexIntrospector &vertex,
                                            const Target &target) {
@@ -3060,6 +3088,11 @@ poplibs::PerfEstimatorTable makePerfFunctionTable() {
       CYCLE_ESTIMATOR_ENTRY(popops, MultiUpdateAdd, FLOAT, false),
       CYCLE_ESTIMATOR_ENTRY(popops, MultiUpdateAdd, INT, false),
       CYCLE_ESTIMATOR_ENTRY(popops, MultiUpdateAdd, UNSIGNED_INT, false),
+
+      CYCLE_ESTIMATOR_ENTRY(popops, SequenceSlice, FLOAT),
+      CYCLE_ESTIMATOR_ENTRY(popops, SequenceSlice, HALF),
+      CYCLE_ESTIMATOR_ENTRY(popops, SequenceSlice, INT),
+      CYCLE_ESTIMATOR_ENTRY(popops, SequenceSlice, UNSIGNED_INT),
 
       CYCLE_ESTIMATOR_ENTRY_NOPARAMS(popops, CircBufIncrIndex),
       CYCLE_ESTIMATOR_ENTRY_NOPARAMS(popops, CircOffset),

commit d3415c8c69c094568f991549f320a78a03dc91ea

change to include file present in sdk Fixes breakage introduced by D39391
Summary: Ref T28274

Test Plan: CI

Reviewers: samk, #poplar_team, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved

Reviewed By: samk, #poplar_team, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved

Subscribers: davidn

Maniphest Tasks: T28274

Differential Revision: https://phabricator.sourcevertex.net/D41119

lib/popops/codelets/SeqSlice.cpp
@@ -8,9 +8,7 @@
 #include <print.h>
 #include <type_traits>
 
-#ifdef __IPU__
-#include "colossus/tileimplconsts.h"
-#endif
+#include "poplibs_support/TileConstants.hpp"
 
 using namespace poplar;
 