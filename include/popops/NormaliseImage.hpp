e7d671e7b146dbba6840127c63256d8024a549c5
Jamie Packer <jamiep@graphcore.ai>
2021-06-19 15:40:02 +0200

    Update Doxygen comments for NormaliseImage.hpp
    Summary: Fix formatting. Tidy up descriptions.
    
    Reviewers: #poplar_team, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, samk
    
    Reviewed By: #poplar_team, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, samk
    
    Maniphest Tasks: T41056
    
    Differential Revision: https://phabricator.sourcevertex.net/D47171

--- include/popops/NormaliseImage.hpp
+++ include/popops/NormaliseImage.hpp
@@ -1,7 +1,7 @@
 // Copyright (c) 2021 Graphcore Ltd. All rights reserved.
 /** \file
  *
- * Functions for padding and normalising image tensors
+ * Functions for padding and normalising image tensors.
  *
  */
 
@@ -12,37 +12,41 @@
 
 namespace popops {
 
-/// Add a tensor for a 3-channel image suitable for padding to 4 channels
+/// Add a tensor for a 3-channel image suitable for padding to 4 channels.
 ///
 /// \param graph The graph to which the tensor will be added.
-/// \param type  The type of the elements. Must be UNSIGNED_CHAR, HALF or FLOAT.
-/// \param shape Required Tenor shape. Must have an inner
-///              dimension of 3 channels \param debugContext Debugging context.
+/// \param type  The type of the elements. Must be \c UNSIGNED_CHAR, \c HALF
+///  or \c FLOAT.
+/// \param shape Required tensor shape. Must have an inner
+///              dimension of three.
+/// \param debugContext Debugging context.
 ///
 poplar::Tensor
 createNormaliseImageInput(poplar::Graph &graph, const poplar::Type &type,
                           const poplar::ArrayRef<std::size_t> shape,
                           const poplar::DebugContext &debugContext = {});
 
-/// Pad a Tensor to have 4 channel dimensions
+/// Pad a tensor to have 4 channel dimensions.
 ///
-/// Each channel is normalised via
-///  (in[c] * inScale - offset[c]) * scale[c]
-/// UINT8 inputs are cast to HALF. Otherwise the output tensor follows the input
-/// type.
+/// Each channel is normalised via:
 ///
-/// \param graph      The graph.
+///      tIn[c] * inScale - offset[c]) * scale[c]
+///
+/// \c UINT8 inputs are cast to \c HALF. Otherwise the output tensor follows the
+/// input type.
+///
+/// \param graph      The graph containing the tensor.
 /// \param seq        The sequence to which the normalisation programs will be
 ///                   added.
-/// \param tIn        Input image. It must have an inner dimension of 3
-///                   channels
-///                   and be UNSIGNED_CHAR, HALF or FLOAT.
+/// \param tIn        Input image. It must have an inner dimension of 3.
+///                   and be \c UNSIGNED_CHAR, \c HALF or \c FLOAT.
 /// \param inScale    Input scaling.
-/// \param offsets    Offset for each channel. Must be shape {3}, must match the
-///                   output type.
-/// \param scales     Scaling factor for each channel. Must be shape {3}, must
-///                   match the output type.
-/// \param debugContext Debugging context
+/// \param offsets    Offset for each channel. Must be shape {3} and must match
+///                   the output type.
+/// \param scales     Scaling factor for each channel. Must be shape {3} and
+///                   must match the output type.
+/// \param debugContext Debugging context.
+///
 poplar::Tensor normaliseImage(poplar::Graph &graph,
                               poplar::program::Sequence &seq,
                               poplar::Tensor tIn, float inScale,
