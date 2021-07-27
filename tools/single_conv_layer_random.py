graphcore/poplibs

commit 14140ae9c97849e29292f63907aeaf72f65763cf

only set --num-determinism-checks when non zero
Summary:
It's not supported by the multiconv tests, but was being set there

Ref T37481

Test Plan: the multi_conv tests now run&pass on pod

Reviewers: samk, mattl, #poplar_team, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved

Reviewed By: samk, #poplar_team, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved

Maniphest Tasks: T37481

Differential Revision: https://phabricator.sourcevertex.net/D48587

tools/single_conv_layer_random.py
@@ -449,8 +449,8 @@ def main():
     try:
         extra_args=device_args + ['--device-type=' +
             str(args.device_type)];
-        if args.device_type == 'Hw':
-            extra_args.append('--num-determinism-checks=' + str(args.num_determinism_checks))
+        if args.device_type == 'Hw' and args.num_determinism_checks != 0:
+              extra_args.append('--num-determinism-checks=' + num_determinism_checks)
         if args.profile:
             extra_args.append('--profile');
         run(params, binary=args.binary,

commit a68aacf7ba8f29add8851eafd9ecf93a5d489c93

fix num_determinism check broken by D48587
Summary: Ref T37481

Test Plan: CI

Reviewers: mattl, #poplar_team, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved

Reviewed By: mattl, #poplar_team, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved

Maniphest Tasks: T37481

Differential Revision: https://phabricator.sourcevertex.net/D48869

tools/single_conv_layer_random.py
@@ -450,7 +450,7 @@ def main():
         extra_args=device_args + ['--device-type=' +
             str(args.device_type)];
         if args.device_type == 'Hw' and args.num_determinism_checks != 0:
-              extra_args.append('--num-determinism-checks=' + num_determinism_checks)
+              extra_args.append('--num-determinism-checks=' + str(args.num_determinism_checks))
         if args.profile:
             extra_args.append('--profile');
         run(params, binary=args.binary,