7b424f53342a478e4dc2b73746a0b44489890b94
Godfrey Da Costa <godfrey.da.costa@graphcore.ai>
2021-08-10 10:15:03 +0100

    <<Replace this line with your revision title>
    Summary: This reverts commit 54de9fcb6d80080ad813591e5ad421d2a534a410.
    
    Test Plan: revert-hammer
    
    Reviewers:
    
    Subscribers:

--- lib/popops/ExpressionGenerator.cpp
+++ lib/popops/ExpressionGenerator.cpp
@@ -37,8 +37,7 @@ namespace popops {
 
 static bool isSupportedType(poplar::Type t) {
   return t == poplar::FLOAT || t == poplar::HALF || t == poplar::INT ||
-         t == poplar::UNSIGNED_INT || t == poplar::BOOL ||
-         t == poplar::UNSIGNED_LONGLONG || t == poplar::LONGLONG;
+         t == poplar::UNSIGNED_INT || t == poplar::BOOL;
 }
 
 static bool traverseAndCheck(const expr::Expr &expr,
@@ -151,7 +150,7 @@ std::string getTypeAlias(const std::string &typeAsStr) {
   if (pos != std::string::npos) {
     str.replace(0, 9, "u", 1);
   }
-  str.erase(std::remove(str.begin(), str.end(), ' '), str.end());
+
   return str;
 }
 
