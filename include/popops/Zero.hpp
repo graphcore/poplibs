820e58b9fd9bb651b32a476f10103b13965c47c0
Jaynie Padayachee <jayniep@graphcore.ai>
2021-09-17 07:27:36 +0200

    Made minor fixes to comments
    Reviewers: jamiep, #poplar_team, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, mattl
    
    Reviewed By: jamiep, #poplar_team, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, mattl
    
    Maniphest Tasks: T39871
    
    Differential Revision: https://phabricator.sourcevertex.net/D51527

--- include/popops/Zero.hpp
+++ include/popops/Zero.hpp
@@ -14,49 +14,51 @@
 
 namespace popops {
 
-/** Appends vertices to \p zeroCS which zeroes elements in \p tileRegions of
- * \p t which reside on tile \p tile.
+/** Append vertices to compute set \p zeroCS which zeroes elements in \p
+ * tileRegions of tensor \p t which reside on \p tile.
  *
  * \param graph         The graph that the operation will be added to.
  * \param t             The tensor whose elements are to be set to zero.
- * \param tileRegions   Region mapping of the tensor on \p tile.
- * \param tile          Tile which the regions relate to.
- * \param zeroCS        Compute set to add the operation into.
+ * \param tileRegions   The region mapping of the tensor on \p tile.
+ * \param tile          The tile which the regions relate to.
+ * \param zeroCS        The compute set to add the operation to.
  */
 void zero(poplar::Graph &graph, poplar::Tensor t,
           const std::vector<poplar::Interval> &tileRegions, unsigned tile,
           poplar::ComputeSet zeroCS);
 
-/** Appends vertices to \p zeroCS which zeroes all elements of \p t
- * which reside on tile \p tile.
+/** Append vertices to compute set \p zeroCS which zeroes all elements of
+ * tensor \p t which reside on \p tile.
  *
  * \param graph         The graph that the operation will be added to.
  * \param t             The tensor whose elements are to be set to zero.
- * \param tile          Tile on which the tensor is mapped to.
- * \param zeroCS        Compute set to add the operation into.
+ * \param tile          The tile which the tensor is mapped to.
+ * \param zeroCS        The compute set to add the operation to.
  */
 void zero(poplar::Graph &graph, const poplar::Tensor &t, unsigned tile,
           poplar::ComputeSet zeroCS);
 
-/** Appends vertices to \p zeroCS which zeroes elements in \p mapping of \p t
- * which reside on tiles represented with \p mapping.
+/** Append vertices to compute set \p zeroCS which zeroes elements in
+ * \p mapping of tensor \p t which reside on tiles represented with \p mapping.
  *
  * \param graph         The graph that the operation will be added to.
  * \param t             The tensor whose elements are to be set to zero.
  * \param mapping       The tensor's region mapping per tile. Each element
  *                      describes a region mapping of a tile (ordered).
- *                      i.e. mapping[0] -> tile 0's region mapping for \p t.
- * \param zeroCS        Compute set to add the operation into.
+ *                      For example, mapping[0] is tile 0's region mapping for
+ *                      tensor \p t.
+ * \param zeroCS        The compute set to add the operation to.
  */
 void zero(poplar::Graph &graph, const poplar::Tensor &t,
           const std::vector<std::vector<poplar::Interval>> &mapping,
           poplar::ComputeSet zeroCS);
 
-/** Appends programs to \p prog which zeroes all elements of the tensor \p t
+/** Append programs to program sequence \p prog which zeroes all elements of
+ * tensor \p t
  *
  * \param graph         The graph that the operation will be added to.
  * \param t             The tensor whose elements are to be set to zero.
- * \param prog          Poplar program sequence to append the operation onto.
+ * \param prog          The Poplar program sequence to append the operation to.
  * \param debugContext  Optional debug information.
  */
 void zero(poplar::Graph &graph, const poplar::Tensor &t,
