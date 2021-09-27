427288f1e8d4f72d998aeefdbc8c6a74e8cf43cd
Jaynie Padayachee <jayniep@graphcore.ai>
2021-09-27 10:07:41 +0200

    popnn/Pooling.hpp -- Small fixes from comments and general edit
    Summary: Updated reference to overloaded function
    
    Reviewers: jamiep, mattl, #poplar_team, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved
    
    Reviewed By: jamiep, mattl, #poplar_team, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved
    
    Maniphest Tasks: T39880
    
    Differential Revision: https://phabricator.sourcevertex.net/D52106

--- include/popnn/Pooling.hpp
+++ include/popnn/Pooling.hpp
@@ -13,31 +13,33 @@
 #include <tuple>
 
 namespace popnn {
+/// Functions for pooling operations
 namespace pooling {
 
 struct PoolParams {
-  /// The type of pooling to be performed (for example maximum, sum or average)
+  /// The type of pooling to be performed (for example, maximum, sum or
+  /// average).
   PoolingType poolingType;
   /// The input shape not including the dimensions for batch size `batchSize`
-  /// and number of channels `numChannels`
+  /// and number of channels `numChannels`.
   std::vector<std::size_t> inputFieldShape;
-  /// The shape of the pooling kernel
+  /// The shape of the pooling kernel.
   std::vector<std::size_t> kernelShape;
-  /// The stride per input dimension
+  /// The stride per input dimension.
   std::vector<unsigned> stride;
   /// The lower padding (for values >0) or truncation (for values <0) for each
   /// input dimension specifying the padding/truncation at the start of the
-  /// dimension
+  /// dimension.
   std::vector<int> inputTruncationOrPaddingLower;
   /// The upper padding (for values >0) or truncation (for values <0) for each
   /// input dimension specifying the padding/truncation at the end of the
-  /// dimension
+  /// dimension.
   std::vector<int> inputTruncationOrPaddingUpper;
-  /// The number of channels in the input tensor
+  /// The number of channels in the input tensor.
   std::size_t numChannels;
-  /// The batch size of the input tensor
+  /// The batch size of the input tensor.
   std::size_t batchSize;
-  /// The output type
+  /// The output type.
   poplar::Type dType;
 
   PoolParams(PoolingType poolingType, std::vector<std::size_t> inputFieldShape,
@@ -71,18 +73,18 @@ double getFwdPerfectCycleCount(const poplar::Graph &graph,
 double getBwdPerfectCycleCount(const poplar::Graph &graph,
                                const PoolParams &params);
 
-/** Add a pooling operation to the graph
+/** Add a pooling operation to the graph.
  *
  * This performs a pooling over the spatial dimensions [...].  The shape of
- * the input should be [batchSize x numChannels x ...].
+ * the input should be [`batchSize * numChannels * ...`].
  *
- * \param graph             The operation will be added to this graph
- * \param params            Pooling parameters
- * \param in                Input tensor
- * \param prog              Program sequence to append the operation to
+ * \param graph             The graph that the operation will be added to.
+ * \param params            The pooling parameters.
+ * \param in                The input tensor.
+ * \param prog              The program sequence to append the operation to.
  * \param debugContext      Optional debug information.
- * \param options           Pooling options (not currently used)
- * \return                  A tensor with the results of the pooling operation
+ * \param options           Pooling options (not currently used).
+ * \return                  A tensor with the results of the pooling operation.
  */
 /*[INTERNAL]
  * **Pooling options**
@@ -90,7 +92,7 @@ double getBwdPerfectCycleCount(const poplar::Graph &graph,
  *    * `poolUseIntrospectiveMapping` (true, false) [=true]
  *
  *      If true, take into account the tile mapping of the output tensor (where
- *      it is provided in as an argument) or the input tensor when deciding how
+ *      it is provided as an argument) or the input tensor when deciding how
  *      to map the pooling operation across tiles.
  */
 poplar::Tensor pool(poplar::Graph &graph, const PoolParams &params,
@@ -98,29 +100,30 @@ poplar::Tensor pool(poplar::Graph &graph, const PoolParams &params,
                     const poplar::DebugContext &debugContext = {},
                     const poplar::OptionFlags &options = {});
 
+// clang-format off
+// Need long lines for Doxygen
 /** Calculate the gradient with respect to the input of a pooling operation
  * given the gradient of the output.
  *
- * This can be used for MAX, AVG or SUM pooling
- * Note - recommend the specific function for AVG or SUM pooling, below.
- *
+ * While this function can be used for maximum, average or sum pooling, poolInputGradient(poplar::Graph&, const PoolParams&, const unsigned, const poplar::Tensor&, poplar::program::Sequence&, const poplar::DebugContext&, const poplar::OptionFlags&) is recommended for average or sum pooling.
  *
  * This performs a pooling over the spatial dimensions [...].  The shape of
- * the input should be [batchSize x numChannels x ...].
+ * the input should be [`batchSize * numChannels * ...`].
  *
- * \param graph             The operation will be added to this graph
- * \param params            Pooling parameters
- * \param in                Forward activations tensor input to pooling
- * \param pooled            Output of pooling in the forward pass
- * \param pooledGradient    Gradients to the pooling operation
- * \param useScaledGradient Use scaled gradient if set to true. Otherwise, the
+ * \param graph             The graph the operation will be added to.
+ * \param params            The pooling parameters.
+ * \param in                The forward activations tensor input to pooling.
+ * \param pooled            The output of pooling in the forward pass.
+ * \param pooledGradient    The gradients to the pooling operation.
+ * \param useScaledGradient Use a scaled gradient if set to true. Otherwise, the
  *                          gradient is propagated to all the positions which
- *                          matched pooled value in forward pass.
- * \param prog              Program sequence to append the operation to
+ *                          matched the pooled value in the forward pass.
+ * \param prog              The program sequence to append the operation to.
  * \param debugContext      Optional debug information.
- * \param options           Pooling options. See pool().
- * \return                  A tensor with the results of the pooling operation
+ * \param options           The pooling options. See pool().
+ * \return                  A tensor with the results of the pooling operation.
  */
+// clang-format on
 poplar::Tensor poolInputGradient(poplar::Graph &graph, const PoolParams &params,
                                  const poplar::Tensor &in,
                                  const poplar::Tensor &pooled,
@@ -129,22 +132,23 @@ poplar::Tensor poolInputGradient(poplar::Graph &graph, const PoolParams &params,
                                  poplar::program::Sequence &prog,
                                  const poplar::DebugContext &debugContext = {},
                                  const poplar::OptionFlags &options = {});
+
 /** Calculate the gradient with respect to the input of a pooling operation
  * given the gradient of the output.
  *
- * This should be used for AVG and SUM pooling
+ * This function should be used for average and sum pooling.
  *
- * This performs a pooling over the spatial dimensions [...].  The shape of
- * the output will be [batchSize x numChannels x ...].
+ * This performs a pooling over the spatial dimensions [...].  The shape of the
+ * output will be [` batchSize * numChannels * ...`].
  *
- * \param graph             The operation will be added to this graph
- * \param params            Pooling parameters
- * \param fwdChansPerGroup  Used in creating the output tensor
- * \param pooledGradient    Gradients to the pooling operation
- * \param prog              Program sequence to append the operation to
+ * \param graph             The graph the operation will be added to.
+ * \param params            The pooling parameters.
+ * \param fwdChansPerGroup  Used in creating the output tensor.
+ * \param pooledGradient    The gradients to the pooling operation.
+ * \param prog              The program sequence to append the operation to.
  * \param debugContext      Optional debug information.
- * \param options           Pooling options. See pool().
- * \return                  A tensor with the results of the pooling operation
+ * \param options           The pooling options. See pool().
+ * \return                  A tensor with the results of the pooling operation.
  */
 poplar::Tensor poolInputGradient(poplar::Graph &graph, const PoolParams &params,
                                  const unsigned fwdChansPerGroup,
