a1d1bbd026de0c3cf3ee54f778ea68b9034a162d
Bartosz Bogdanski <bartoszb@graphcore.ai>
2022-01-31 15:51:46 +0100

    Deprecate CollectiveTypes.hpp
    Summary:
    In D58812 we copied CollectiveTypes.hpp and CollectiveTypes.cpp to GCL
    and exposed them in the public API.
    
    The motivation for this change is that GCL becomes not only a communication
    but also a collective library.
    
    Reviewers: #poplar_team, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, jorgec, bartoszb
    
    Reviewed By: #poplar_team, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, jorgec
    
    Subscribers: jorgec
    
    Maniphest Tasks: T52223
    
    Differential Revision: https://phabricator.sourcevertex.net/D58813

--- include/popops/CollectiveTypes.hpp
+++ include/popops/CollectiveTypes.hpp
@@ -8,44 +8,62 @@
 #ifndef popops_CollectiveTypes_hpp
 #define popops_CollectiveTypes_hpp
 
+#include <gccs/CompilerFeatures.hpp>
 #include <poplar/Tensor.hpp>
 #include <popops/Operation.hpp>
 
 namespace popops {
 /**
+ *
  * Supported collective operators.
+ * \deprecated Use gcl::CollectiveOperator instead.
  */
-enum class CollectiveOperator {
-  ADD,
-  MEAN,
-  MUL,
-  MIN,
-  MAX,
-  LOGICAL_AND, ///< Only supports boolean operands.
-  LOGICAL_OR,  ///< Only supports boolean operands.
-  SQUARE_ADD,  ///< Squares each element before applying ADD reduction.
-  LOCAL,       ///< Do nothing and keep the local value.
-};
+enum class GC_DEPRECATED_MSG("Use gcl::CollectiveOperator instead")
+    CollectiveOperator {
+      ADD,
+      MEAN,
+      MUL,
+      MIN,
+      MAX,
+      LOGICAL_AND, ///< Only supports boolean operands.
+      LOGICAL_OR,  ///< Only supports boolean operands.
+      SQUARE_ADD,  ///< Squares each element before applying ADD reduction.
+      LOCAL,       ///< Do nothing and keep the local value.
+    };
 
 /**
  * Parse token from input stream \p is to \p op. Valid input values are the
  * stringified enumerations, for example "ADD" or "MUL".
  * \return The original input stream.
+ * \deprecated This operator overload has been deprecated and will be removed
+ * in a future release.
  */
-std::istream &operator>>(std::istream &is, CollectiveOperator &op);
+GC_DEPRECATED std::istream &operator>>(std::istream &is,
+                                       CollectiveOperator &op);
 
 /**
  * Write \p op to output stream \p os. The value written is the stringified
  * enumeration, for example "ADD" or "MUL".
  * \return The original output stream.
+ * \deprecated This operator overload has been deprecated and will be removed
+ * in a future release.
  */
-std::ostream &operator<<(std::ostream &os, const CollectiveOperator &op);
+GC_DEPRECATED std::ostream &operator<<(std::ostream &os,
+                                       const CollectiveOperator &op);
 
-/** Convert from popops::Operation to popops::CollectiveOperator */
+/**
+ *
+ * Convert from popops::Operation to popops::CollectiveOperator
+ * \deprecated Use gcl::operationToCollectiveOperator instead.
+ */
+GC_DEPRECATED_MSG("Use gcl::operationToCollectiveOperator instead")
 CollectiveOperator operationToCollectiveOperator(const Operation &col);
 
-/// Represents a section of a tensor mapped to an IPU.
-struct Chunk {
+/**
+ * Represents a section of a tensor mapped to an IPU.
+ * \deprecated Use gcl::Chunk instead.
+ */
+struct GC_DEPRECATED_MSG("Use gcl::Chunk instead") Chunk {
   poplar::Tensor tensor;
   /// Ring index (data parallel index).
   unsigned index;
@@ -56,8 +74,11 @@ struct Chunk {
       : tensor(tensor), index(index), offset(offset) {}
 };
 
-/// A vector of Chunk data.
-struct Chunks {
+/**
+ * A vector of Chunk data.
+ * \deprecated Use gcl::Chunks instead.
+ */
+struct GC_DEPRECATED_MSG("Use gcl::Chunks instead") Chunks {
   /// Used to undo shuffles introduced by scatter.
   poplar::Tensor originalInput;
   /// Chunks produced by the scatter step.
