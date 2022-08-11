f420ebdf1443417d5f8a99f53245d25adfa66ee8
Matt Law <mattl@graphcore.ai>
2022-08-11 17:04:00 +0100

    Backout D71197 because it caused a compile-time regression in poptorch benchmarks.
    Summary: This reverts commit 9a25bad5899c.
    
    Test Plan: revert-hammer
    
    Reviewers:
    
    Subscribers:

--- lib/poplin/ConvOptions.hpp
+++ lib/poplin/ConvOptions.hpp
@@ -70,7 +70,7 @@ public:
   // disable Stochastic Rounding for AMP vertices
   bool disableSRForAMPVertices = false;
   // Allow the planner to try candidates with tile-level expand dim transforms.
-  bool enableTileLevelExpandDims = true;
+  bool enableTileLevelExpandDims = false;
   void parseConvOptions(const poplar::OptionFlags &options);
 
 private:
